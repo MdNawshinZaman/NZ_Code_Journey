1e85db60fe7c194d5083e2982e86defdb5ba25af
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-11-27 15:15:26 +0000

    Sun, Nov 27, 2022, 3:15 PM +00:00

--- Code15/buggy.c
+++ Code15/buggy.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-
+#include <cs50.h>
 int main (void)
 {
-    for(int i = 0; i </*=*/ 3; i++)
+    for(int i = 0; i <= 3; i++)
     {
-        //printf("i is %i\n",i);
         printf("#\n");
     }
 }
