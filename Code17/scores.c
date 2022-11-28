47252ed4f17910da21f1d8a1815ce2d4f92b1a57
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-11-28 15:20:43 +0000

    Mon, Nov 28, 2022, 3:20 PM +00:00

--- Code17/scores.c
+++ Code17/scores.c
@@ -6,7 +6,9 @@ int main (void)
     int sc2 = 73;
     int sc3 = 33;
 
-    printf("Average : %f\n", (sc1 + sc2 + sc3)/3.0); // In this code, I have took 3 int as input. But my output will be a float value
+    printf("Average : %f\n", (sc1 + sc2 + sc3)/3.0);
+    // In this code, I have took 3 int as input. But my output will be a float value
     // For this, i have used %f. but still it was not working because at first I had devided the sumesion with 3 that is it self an int.
-    // 
+    // to solve this problem I have devided the Average with 3.0
+    // this .0 converts my ans to a floating value.
 }
