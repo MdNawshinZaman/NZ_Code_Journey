a7a3df1ceee7cf599ef8e14524a204fdf20b673d
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-11-28 14:32:58 +0000

    Mon, Nov 28, 2022, 2:32 PM +00:00

---
+++ Code16/buggy_1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int get_negative_int (void);
+
+int main (void)
+{
+    int i = get_negative_int();
+    printf("%i\n",i);
+}
+
+int get_negative_int (void) // here get_negative_int is a fuction which will return an int value but won't take any.
+{
+    int n;
+    do
+    {
+        n = get_int("Please input a negative integer: ");
+    }
+    while (n < 0)
+    return n;
+}
