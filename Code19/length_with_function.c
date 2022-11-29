e8e2a1b23f40797c6cc6c8acb3ad8e9e51f73691
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-11-29 15:59:30 +0000

    Tue, Nov 29, 2022, 3:59 PM +00:00

--- Code19/length_with_function.c
+++ Code19/length_with_function.c
@@ -8,6 +8,6 @@ int main(void)
     int length = strlen(name); //"strlen" is a function that can measure the length of a string.
                                //This function comes with a library called <string.h>
                                // this library comes with C itself
-    printf("")
+    printf("%i \n",length);
 
 }
