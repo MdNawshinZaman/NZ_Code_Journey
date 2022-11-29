a2952f7ab9cf66bdce3999115ddcfa8cad5e831c
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-11-29 15:13:53 +0000

    Tue, Nov 29, 2022, 3:13 PM +00:00

--- Code19/length1.c
+++ Code19/length1.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <cs50.h>
-//doing the 
+//doing the same work, but now I am going to create a separate function for the while loop work
+int string_length (string s);
 int main (void)
 {
     string name = get_string("Name: ");
+    int length = string_length (name);
+    printf("%i\n",length);
+}
 
+int string_length (string s)
+{
+    int i = 0;
+    while (s[i] != 0)
+    {
+        i++;
+    }
+    return i;
 }
