0d7dc2d332def202b25434332e772b58c42b1d0d
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-11-29 12:34:35 +0000

    Tue, Nov 29, 2022, 12:34 PM +00:00

--- Code18 (using char)/hi.c
+++ Code18 (using char)/hi.c
@@ -7,5 +7,6 @@ int main (void)
     char c3 = '!';
 
     //printf("%C%C%C\n", c1,c2,c3);
-    printf("%i %i %i\n",c1,c2,c3); // this line will print the ASCII values of 'H' 'I' and '!'
+    printf("%i %i %i\n", c1
+    , c2, c3); // this line will print the ASCII values of 'H' 'I' and '!'
 }
