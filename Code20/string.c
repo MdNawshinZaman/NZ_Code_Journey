0b2ba1de8b26d7b4c2f9dc14e465956c9b5d9fe8
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-11-29 16:24:42 +0000

    Tue, Nov 29, 2022, 4:24 PM +00:00

--- Code20/string.c
+++ Code20/string.c
@@ -4,7 +4,13 @@
 
 int main (void)
 {
-    string s = get_string("Input: ");
+    string s = get_string("Input:  ");
     // Now objective is to print the collected string without using %s
-    printf("Output",);
+    printf("Output: ");
+
+    for (int i = 0; i < strlen(s); i++)
+    {
+        printf("%c",s[i]);
+    }
+    printf("\n");
 }
