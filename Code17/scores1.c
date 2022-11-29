a794f1801617f9a769500373248ce1ae8f2ca2c8
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-11-29 12:06:57 +0000

    Tue, Nov 29, 2022, 12:06 PM +00:00

--- Code17/scores1.c
+++ Code17/scores1.c
@@ -7,5 +7,5 @@ int main (void)
     scores[1] = 73; //[1]-- index one
     scores[2] = 33;
 
-    printf("Average: %f \n",(scores[0]+scores[1]+scores[2])/3.0);
+    printf("Average: %f \n",(scores[0]+scores[1]+scores[2])/3.0); // watch "scores.c" to see deference
 }
