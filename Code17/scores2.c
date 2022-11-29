54ff05ac9bd2ad9524b36d098bf8ab49b9f0ac70
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-11-29 12:11:11 +0000

    Tue, Nov 29, 2022, 12:11 PM +00:00

---
+++ Code17/scores2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <cs50.h>
+int main (void)
+{
+    int scores [3];
+    
+
+    printf("Average: %f \n",(scores[0]+scores[1]+scores[2])/3.0); // watch "scores.c" to see deference
+}
