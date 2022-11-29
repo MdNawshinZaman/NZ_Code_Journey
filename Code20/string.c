805618501c610df9a201ce40519e5c9068dd88f0
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-11-29 16:19:21 +0000

    Tue, Nov 29, 2022, 4:19 PM +00:00

---
+++ Code20/string.c
@@ -0,0 +1,10 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main (void)
+{
+    string s = get_string("Name: ");
+    // Now objective is to print the collected string without using %s
+    printf("String is",);
+}
