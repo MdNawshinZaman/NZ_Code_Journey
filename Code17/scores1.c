3c38d4dbdc1ea31121b14d083fdf7b63f3f0771a
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-11-29 12:08:53 +0000

    Tue, Nov 29, 2022, 12:08 PM +00:00

--- Code17/scores1.c
+++ Code17/scores1.c
@@ -3,9 +3,9 @@
 int main (void)
 {
     int scores [3];
-    scores[0] = 72; //[0]-- index zero
-    scores[1] = 73; //[1]-- index one
-    scores[2] = 33;
+    scores[0] = get_int ("Score-1: "); //[0]-- index zero
+    scores[1] = get_int ("Score-2: "); //[1]-- index one
+    scores[2] = get_int ("Score-3: ");
 
     printf("Average: %f \n",(scores[0]+scores[1]+scores[2])/3.0); // watch "scores.c" to see deference
 }
