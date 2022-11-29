ac44922a08ffffffbe23141a72e24d92c5729697
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-11-29 15:19:28 +0000

    Tue, Nov 29, 2022, 3:19 PM +00:00

--- Code19/length1.c
+++ Code19/length1.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <cs50.h>
 //doing the same work, but now I am going to create a separate function for the while loop work
+
 int string_length (string s);
+
 int main (void)
 {
     string name = get_string("Name: ");
