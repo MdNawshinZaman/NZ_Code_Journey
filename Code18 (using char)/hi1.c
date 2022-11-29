36a8893c35316d6fe9ee72ce72709efa61ed6150
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-11-29 12:48:44 +0000

    Tue, Nov 29, 2022, 12:48 PM +00:00

--- Code18 (using char)/hi1.c
+++ Code18 (using char)/hi1.c
@@ -6,6 +6,7 @@ int main (void)
    string s = "HI!\n"; //string is implemented as an array of charecters
    //printf("%s", s);
 
-    printf("%i %i %i\n", s[0], s[1], s[2]); // here s[0] refers to index 0 ehich is H, same with s[1] and s[2] that refers to I and !
+    printf("%i %i %i %i\n", s[0], s[1], s[2], s[3]); // here s[0] refers to index 0 ehich is H, same with s[1] and s[2] that refers to I and !
+                                                    // buy using the s[3], we are aqtually seing the Nul Character
 
 }
