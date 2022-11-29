3429c7c47c893e64a6e55de089419302bdd0792a
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-11-29 14:59:12 +0000

    Tue, Nov 29, 2022, 2:59 PM +00:00

--- Code19/length.c
+++ Code19/length.c
@@ -6,7 +6,7 @@ int main (void)
     string name = get_string("Name: ");
 
     int i = 0;
-    while (name[i] != '\0')
+    while (name[i] != '\0') // here '\0' refers to Nul character
     {
         i++;
     }
