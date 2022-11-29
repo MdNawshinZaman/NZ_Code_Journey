18baf6046befdb40ee275b6732ba2b79c4b8ab49
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-11-29 12:56:11 +0000

    Tue, Nov 29, 2022, 12:56 PM +00:00

---
+++ Code18 (using char)/hi2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main (void)
+{
+    string s = "HI!";
+    string t = "BYE!";
+
+    //printf("%s\n",s);
+    printf("%i %i %i %i \n", s[0], s[1], s[2], s[3]);
+
+    //printf("%s\n",t);
+    printf("%i %i %i %i %i %i\n", t[0], t[1], t[2], t[3], t[4], t[5]);
+}
