8dfe21614600ab8b17ffe2fbc14f6c7fb5088104
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-11-29 12:58:07 +0000

    Tue, Nov 29, 2022, 12:58 PM +00:00

--- Code18 (using char)/hi2.c
+++ Code18 (using char)/hi2.c
@@ -10,5 +10,5 @@ int main (void)
     printf("%i %i %i %i \n", s[0], s[1], s[2], s[3]);
 
     //printf("%s\n",t);
-    printf("%i %i %i %i %i %i\n", t[0], t[1], t[2], t[3], t[4], t[5]);
+    printf("%i %i %i %i %i \n", t[0], t[1], t[2], t[3], t[4]);
 }
