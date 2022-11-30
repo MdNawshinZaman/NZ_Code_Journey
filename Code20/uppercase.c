ffdc3aee7cefda1f1cf2c87e0129e4e64906fb7d
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-11-30 06:25:13 +0000

    Wed, Nov 30, 2022, 6:25 AM +00:00

--- Code20/uppercase.c
+++ Code20/uppercase.c
@@ -8,7 +8,7 @@ int main(void)
     string s = get_string("Before: ");
     printf("After:  ");
 
-    for(int i = 0, n = strlen(s); i < n; ++)
+    for(int i = 0, n = strlen(s); i < n; i++)
     {
         if(s[i] >= 'a' && s[i] <='z')
         {
