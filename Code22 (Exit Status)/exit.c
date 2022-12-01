9b842c85097b279b6be0cbfcfd565a2b512494b7
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-12-01 09:10:50 +0000

    Thu, Dec 1, 2022, 9:10 AM +00:00

---
+++ Code22 (Exit Status)/exit.c
@@ -0,0 +1,6 @@
+#include <cs50.h>
+#include <stdio.h>
+int main (int argc, string argv[]) //here argc = argument count  // string argv[] = argument vector, this is an array of strings not a single string.
+{
+    
+}
