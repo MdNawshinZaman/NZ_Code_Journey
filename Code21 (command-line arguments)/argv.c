28a61d0501928e50aef8f9dffdd61257f82309a5
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-12-01 08:55:46 +0000

    Thu, Dec 1, 2022, 8:55 AM +00:00

--- Code21 (command-line arguments)/argv.c
+++ Code21 (command-line arguments)/argv.c
@@ -15,5 +15,5 @@ So if we pass a value to a program, value of argc would be 2 (one for argument a
 #include <stdio.h>
 int main (int argc, string argv[]) //here argc = argument count  // string argv[] = argument vector, this is an array of strings not a single string.
 {
-    printf("Hello, %s.\n", argv[0]);
+    printf("Hello, %s.\n", argv[1]);
 }
