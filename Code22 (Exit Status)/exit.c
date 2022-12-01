3c059d92add7019e833a81058827884787bcafa8
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-12-01 09:22:36 +0000

    Thu, Dec 1, 2022, 9:22 AM +00:00

--- Code22 (Exit Status)/exit.c
+++ Code22 (Exit Status)/exit.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 int main (int argc, string argv[]) //here argc = argument count  // string argv[] = argument vector, this is an array of strings not a single string.
 {
-    if (argc != 0)
+    if (argc != 2)
     {
         printf("Command-line argument is missing!\n");
-        return 11111;
+        return 1;
+
     }
-    printf ("Hello, %s.", argv[1]);
+    printf ("Hello, %s.\n", argv[1]);
     return 0; // main() is always returning 0 secreatly to the computer if the program execution was successful.
+
 }
