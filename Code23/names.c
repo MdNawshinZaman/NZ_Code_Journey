5967b502e3e56f27672c1c88db7fc38bacaae7cc
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-12-07 16:27:24 +0000

    Wed, Dec 7, 2022, 4:27 PM +00:00

--- Code23/names.c
+++ Code23/names.c
@@ -4,4 +4,15 @@
 int main(void)
 {
     string names[]={}
+
+    for (int i = 0; i < 7; i++)
+    { 
+        if (numbers[i] == 9)
+        {
+            printf("Number found in position %i.\n", i);
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
 }
