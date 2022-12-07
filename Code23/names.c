b74f406c92ef0ab68ec5ed0221c727c624bb8df8
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-12-07 16:30:39 +0000

    Wed, Dec 7, 2022, 4:30 PM +00:00

--- Code23/names.c
+++ Code23/names.c
@@ -1,9 +1,9 @@
 #include <cs50.h>
 #include <stdio.h>
-#include <strign.h>
+#include <string.h>
 int main(void)
 {
-    string names[]={"Bill", "Charlie", "Fred", "George", "Ginny", "Percy", "Ron"}
+    string names[]={"Bill", "Charlie", "Fred", "George", "Ginny", "Percy", "Ron"};
 
     for (int i = 0; i < 7; i++)
     {
