5a783dbee077eb91cd84d9c18d7e8418b15d98f1
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-12-08 12:59:02 +0000

    Thu, Dec 8, 2022, 12:59 PM +00:00

--- Code24/advance-phonebook.c
+++ Code24/advance-phonebook.c
@@ -7,19 +7,27 @@
 
 typedef struct
 {
-    string = people;
-    string = number;
+    string name;
+    string number;
 }
+person;
 
 int main(void)
 {
-    
+    person people[];
+
+    people[0].name= "Carter"; // Here the dot between name and people is a new syntext
+                              //the dot is telling my code keep the tryped data inside the variable named "name"
+    people[0].number= "+1-617-495-1000" //for this line, the dot is telling my code keep the tryped data inside the variable named "number"
+
+    people[1].name= "David";
+    people[1].number= "+1-949-468-2750";
 
     for(int i = 0; i < 2; i++)
     {
-        if (strcmp(names[i], "David") == 0)
+        if (people[i].name, "David") == 0)
         {
-            printf("The number of %s is (%s)\n",names[i], numbers[i]);
+            printf("The number of %s is (%s)\n",people[i].name, people[i].number);
             return 0;
         }
     }
