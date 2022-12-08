346cd725f7229e1ef78309ce5f1560208db6fd59
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-12-08 12:20:04 +0000

    Thu, Dec 8, 2022, 12:20 PM +00:00

---
+++ Code24/advance-phonebook.c
@@ -0,0 +1,22 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+
+
+int main(void)
+{
+    string names[] = {"Carter", "David"};
+    string numbers[] = {"+1-617-495-1000", "+1-949-468-2750"};
+
+    for(int i = 0; i < 2; i++)
+    {
+        if (strcmp(names[i], "David") == 0)
+        {
+            printf("The number of %s is (%s)\n",names[i], numbers[i]);
+            return 0;
+        }
+    }
+    printf("Name not found.\n");
+    return 1;
+}
