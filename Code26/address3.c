2addca9e145eb57a51320e6dec9c2738dd6d222b
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-12-09 15:40:22 +0000

    Fri, Dec 9, 2022, 3:40 PM +00:00

--- Code26/address3.c
+++ Code26/address3.c
@@ -1,1 +1,11 @@
-#inlude
+#include <stdio.h>
+
+int main(void)
+{
+    char *s = "HI!";
+    printf("%p\n", s);
+    printf("%p\n", s[0]);
+    printf("%p\n", s[1]);
+    printf("%p\n", s[2]);
+    printf("%p\n", s[3]);
+}
