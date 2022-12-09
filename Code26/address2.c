3a0a9a234b1df99399f246436d4254315cff2ff1
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-12-09 15:33:24 +0000

    Fri, Dec 9, 2022, 3:33 PM +00:00

--- Code26/address2.c
+++ Code26/address2.c
@@ -1,9 +1,9 @@
-#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    string s = "HI!"; // this line is working as char *s = "HI!";
+    // string s = "HI!"; // this line is working as char *s = "HI!";
+    char *s = "HI!";
     char *p = &s[0];
 
     printf ("%p\n", s);
