e9a62d3f998f1f3471a82bb3e9285c5fffe2d96c
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-12-09 04:20:54 +0000

    Fri, Dec 9, 2022, 4:20 AM +00:00

--- test.c
+++ test.c
@@ -4,17 +4,25 @@ int main(){
     int age;
     printf("Enter your age:\n");
     scanf("%d",&age);
-    if(age<2){
+    if(age<2)
+    {
         printf("Infant\n");
-    }else if(age >=2 && age <10){
-        printf("Child\n");
-    }else if(age >=10 && age <20){
-        printf("Teenage\n");
-    }else if(age >=20 && age <30){
-        printf("Adult\n");
-    }else if("age >=30")
-    {printf("old\n");
     }
-
+    else if(age >=2 && age <10)
+    {
+        printf("Child\n");
+    }
+    else if(age >=10 && age <20)
+    {
+        printf("Teenage\n");
+    }
+    else if(age >=20 && age <30)
+    {
+        printf("Adult\n");
+    }
+    else if("age >=30")
+    {
+        printf("old\n");
+    }
     return 0;
 }
