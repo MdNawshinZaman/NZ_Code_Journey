bcb50eb965f583f41f08bb5803379d599f25d3b5
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-12-09 15:29:02 +0000

    Fri, Dec 9, 2022, 3:29 PM +00:00

--- Code26/address1.c
+++ Code26/address1.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-    string s = "HI!"; // this line works as --char *s = "HI!";--
+    string s = "HI!"; // this line works as --char *s = "HI!";-- // see the proof in address2.c
     char c = s[0];
     char *p = &c;
     printf("%p\n",s); // seeing the address of s
