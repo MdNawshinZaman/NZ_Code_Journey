9f49ee43f8e3884b69f94f06aabd97bdb87ca513
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-12-09 12:45:53 +0000

    Fri, Dec 9, 2022, 12:45 PM +00:00

--- Code26/address.c
+++ Code26/address.c
@@ -4,5 +4,7 @@
  int main(void)
  {
     int n = 50;
-    printf("%i\n",n);
+    int *p = &n; /* here the single &n refers the memory address on the value of n. the *(star) tells my computer that
+                 I am storing an address type integer in my new  variable called p */
+    printf("%p\n",p); // %p is a special formate code for printing address in C.
  }
