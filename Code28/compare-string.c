7ec0ff2e8abbcdfc349d344b0c98d58edbaf18e4
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-12-11 15:26:52 +0000

    Sun, Dec 11, 2022, 3:26 PM +00:00

--- Code28/compare-string.c
+++ Code28/compare-string.c
@@ -10,7 +10,7 @@ int main(void)
     char *s = get_string("s: ");
     char *t = get_string("t: ");
 
-    if (strcmp(s, t) == 0) // 'strcmp()'
+    if (strcmp(s, t) == 0) // 'strcmp() is the function built-in string.h library to compare 2 strings'
     {
         printf("Same\n"); // this part of the code never works when we use only 'string' instead of 'char *'
     }
