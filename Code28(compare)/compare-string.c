1c9042a1a51571eacd0fe4b2280028e8cb1bb95e
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-12-11 15:39:30 +0000

    Sun, Dec 11, 2022, 3:39 PM +00:00

--- Code28(compare)/compare-string.c
+++ Code28(compare)/compare-string.c
@@ -10,14 +10,19 @@ int main(void)
     char *s = get_string("s: ");
     char *t = get_string("t: ");
 
+    printf("\n");
+
     //printing those inputs
     printf("%s\n", s); // here %s knows that, it has to start printing from the first character (addressed by char *) and do the rest until it finds the NUL(\0) character
     printf("%s\n", t);
+    printf("\n");
 
     //printing the addresses of those inputs
     printf("%p\n", s);
     printf("%p\n", t);
+    printf("\n");
 
+    //checking if those inputs are saem or different.
     if (strcmp(s, t) == 0) // 'strcmp() is the function built-in string.h library to compare two strings
     {
         printf("Same\n"); // this part of the code never works when we use only 'string' instead of 'char *'
