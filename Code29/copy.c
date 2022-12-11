442cbb4d8486cd6687c089912a67441261f984fa
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-12-11 16:12:55 +0000

    Sun, Dec 11, 2022, 4:12 PM +00:00

--- Code29/copy.c
+++ Code29/copy.c
@@ -1,10 +1,11 @@
 #include <cs50.h>
+#include <ctype.h> // this lirary contains ''
 #include <stdio.h>
 #include <string.h>
 
 int main(void)
 {
-     string s = get-string("s: ");
+     string s = get_string("s: ");
 
      string t = s;
 
