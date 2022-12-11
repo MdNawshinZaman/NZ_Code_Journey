828897d256f29d1175f7a312c13df3d5e40a378c
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-12-11 15:21:42 +0000

    Sun, Dec 11, 2022, 3:21 PM +00:00

--- Code28/compare-string.c
+++ Code28/compare-string.c
@@ -3,12 +3,15 @@
 
 int main(void)
 {
-    string s = get_string("s: ");
-    string t = get_string("t: ");
+    //string s = get_string("s: "); // get_string alway return the first character address of the strign
+    //string t = get_string("t: ");
+
+    char *s = get_string("s: ");
+    char *t = get_string("t: ");
 
     if (s == t)
     {
-        printf("Same\n"); 
+        printf("Same\n"); // this par of the code never works when we use only 'string' instead of 'char *'
     }
     else
     {
