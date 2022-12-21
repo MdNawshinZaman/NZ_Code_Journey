09dd2c89fea7ac353cab58cfb8099b23300982c7
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-12-21 17:16:10 +0000

    Wed, Dec 21, 2022, 5:16 PM +00:00

--- Problem-solving/test.c
+++ Problem-solving/test.c
@@ -6,9 +6,9 @@ int main(void)
     double b, p, R;
     printf("Enter the Base: ");
     scanf("%lf", &b);
-    printf("Enter the power: ");
+    printf("Enter the exponent: ");
     scanf("%lf", &p);
     R = pow(b, p);
-    printf("Ans: %lf\n", R);
+    printf("Ans of power: %lf\n", R);
     return 0;
 }
