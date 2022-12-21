74e2b4a21a34808c8f352485e4e81262e6156be2
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-12-21 17:15:06 +0000

    Wed, Dec 21, 2022, 5:15 PM +00:00

--- Problem-solving/test.c
+++ Problem-solving/test.c
@@ -1,22 +1,14 @@
 #include<stdio.h>
 #include<math.h>
-int main(){
 
-double base,expo,power = 1 ;
-
-double i;
-
-printf("Enter Base : ");
-scanf("%lf",&base);
-
-printf("Enter Expo : ");
-scanf("%lf",&expo);
-
-for(i = 1;i <= expo ; i++){
-
-    power = power * base ;
-
-}
-printf("Power is %lf",power);
-return 0;
+int main(void)
+{
+    double b, p, R;
+    printf("Enter the Base: ");
+    scanf("%lf", &b);
+    printf("Enter the power: ");
+    scanf("%lf", &p);
+    R = pow(b, p);
+    printf("Ans: %lf\n", R);
+    return 0;
 }
