5194c9a2d4fde4535d0d4d9e3fadc941be12dd6c
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-12-21 15:39:05 +0000

    Wed, Dec 21, 2022, 3:39 PM +00:00

--- Problem-solving/p-20.c
+++ Problem-solving/p-20.c
@@ -4,6 +4,11 @@
 int main(void)
 {
     double b, p, R;
-    printf("Enter the Base and power: ");
-    scanf("%lf %lf", &b, &p);
+    printf("Enter the Base: ");
+    scanf("%lf", &b);
+    printf("Enter the power: ");
+    scanf("%lf", &p);
+    R = pow(b, p);
+    printf("Ans: %lf\n", R);
+    return 0;
 }
