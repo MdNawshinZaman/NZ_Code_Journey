2e9dc53be395165ff51c597b62a9d7dcd1651349
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-12-21 17:19:46 +0000

    Wed, Dec 21, 2022, 5:19 PM +00:00

--- Problem-solving/p-21.c
+++ Problem-solving/p-21.c
@@ -6,7 +6,7 @@ int main(void)
      double b, p, R = 1; // if the value of R is not defined as 1, you will get the ans 0
     printf("Enter the Base: ");
     scanf("%lf", &b);
-    printf("Enter a non fractional power: "); // this code can't 
+    printf("Enter a non fractional power: "); // this code can't handle fractional exponent
     scanf("%lf", &p);
 
     for (int i = 1; i <= p; i++)
