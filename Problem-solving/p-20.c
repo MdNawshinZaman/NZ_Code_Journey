87a7a57507c910a06da2dc7bfc11f98d65068e06
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-12-21 15:49:25 +0000

    Wed, Dec 21, 2022, 3:49 PM +00:00

--- Problem-solving/p-20.c
+++ Problem-solving/p-20.c
@@ -13,8 +13,23 @@
     return 0;
 }*/
 
-
+double NZ (double base, double power);
 int main(void)
 {
-    
+    double b, p, R;
+    printf("Enter the Base: ");
+    scanf("%lf", &b);
+    printf("Enter the power: ");
+    scanf("%lf", &p);
+
+    R = NZ(b, p);
+
+    printf("Your Ans is: %lf\n", R);
+}
+
+
+
+double NZ (double base, double power)
+{
+    return pow(base, power);
 }
