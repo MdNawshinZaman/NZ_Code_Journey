120bb037905e7cc770f52418796e3a8efc1b4171
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-12-22 13:16:40 +0000

    Thu, Dec 22, 2022, 1:16 PM +00:00

--- Problem-solving/p-1.c
+++ Problem-solving/p-1.c
@@ -5,10 +5,12 @@ int main(void)
     //char *n = "Md. Nawshin Zaman";
     //char *N = "Bangladeshi";
 
+    char n,N;
+
     printf("Enter your name: ");
-    scanf("%s",&char *n);
+    scanf("%s",&*n);
     printf("Enter your nationality: ");
-    scanf("%s",&char *N);
+    scanf("%s",&*N);
 
     printf("Hello world, i am a student of CSE. Here is my identity: \n Name: %s \n Nationality: %s \n",n, N);
 }
