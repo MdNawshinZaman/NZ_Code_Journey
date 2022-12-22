09d59517ea07117f79c3935fdff55885ecdff3d7
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-12-22 13:05:04 +0000

    Thu, Dec 22, 2022, 1:05 PM +00:00

--- Problem-solving/p-1.c
+++ Problem-solving/p-1.c
@@ -4,5 +4,5 @@ int main(void)
 {
     char *n = "Md. Nawshin Zaman";
 
-    printf("Hello world, i am a student of CSE. Here is my identity: \n Name: %c",n);
+    printf("Hello world, i am a student of CSE. Here is my identity: \n Name: %s",n);
 }
