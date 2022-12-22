f7b0160e764574fd98e0adadf405790785cbf8b8
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-12-22 14:59:04 +0000

    Thu, Dec 22, 2022, 2:59 PM +00:00

--- Problem-solving/p-1.c
+++ Problem-solving/p-1.c
@@ -1,3 +1,5 @@
+// Improved Version
+// This Code asks the name and the nationality of the user before showing output.
 #include <stdio.h>
 
 int main(void)
@@ -5,13 +7,13 @@ int main(void)
     //char *n = "Md. Nawshin Zaman";
     //char *N = "Bangladeshi";
 
-    char name[100];
-    char nationality[100];
+    char name[30];
+    char nationality[30];
 
     printf("Enter your name: ");
     gets(name);
     printf("Enter your nationality: ");
     gets(nationality);
-
-    printf("Hello world, i am a student of CSE. Here is my identity: \n Name: %s \n Nationality: %s \n",name, nationality);
+    printf("\n");
+    printf("Hello world, I am a student of CSE. Here is my identity: \nName: %s \nNationality: %s \n",name, nationality);
 }
