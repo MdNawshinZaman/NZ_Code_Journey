4e7197ec42eadda43fe4ada3755a55eb48ea7116
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-12-23 04:34:13 +0000

    Fri, Dec 23, 2022, 4:34 AM +00:00

--- Code3-4/calculator.c
+++ Code3-4/calculator.c
@@ -3,8 +3,14 @@
 
 int main(void)
 {
+    // Commenting is used to design the code in a more readable way!!
+    // prompt user for x
     int x = get_int("x: ");
+
+    // prompt user for y
     int y = get_int("y: ");
+
+    // perform addition
     int z = x + y;
     printf("%i\n",z);
 }
