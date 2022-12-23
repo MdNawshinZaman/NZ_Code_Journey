0845e5bf44d7862ea9e61e78baf00ea31c62bcd1
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-12-23 04:42:31 +0000

    Fri, Dec 23, 2022, 4:42 AM +00:00

--- Code5/if-else.c
+++ Code5/if-else.c
@@ -3,5 +3,7 @@
 
 int main(void)
 {
-    
+    const int MINE = 2; // here the 'const' function will never let the value of 'MINE' to be changed.
+                        // as this is a constant variable, it's recommended to use all Capital letters to remember it.
+    int 
 }
