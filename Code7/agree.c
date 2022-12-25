c6002255d33fdd5d5c65e89f074feb9f223d6ac8
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-12-25 05:31:23 +0000

    Sun, Dec 25, 2022, 5:31 AM +00:00

--- Code7/agree.c
+++ Code7/agree.c
@@ -8,6 +8,7 @@ int main(void)
 
     // check weather user agreed
     // if you are dealing with single characters also known as 'char', you will have to use single quotes ('') instead of Double Quotes ("")
+    start:
     if (c == 'y' || c == 'Y')
     {
         printf("Agreed.\n");
@@ -18,7 +19,6 @@ int main(void)
     }
     else
     {
-        printf("User Input is Wrong");
-        goto line 7
+        goto start;
     }
 }
