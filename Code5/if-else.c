0b2e7df1332e935634a9b5130595193dfca01c4a
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-12-25 03:35:45 +0000

    Sun, Dec 25, 2022, 3:35 AM +00:00

--- Code5/if-else.c
+++ Code5/if-else.c
@@ -5,5 +5,19 @@ int main(void)
 {
     const int MINE = 2; // here the 'const' function will never let the value of 'MINE' to be changed.
                         // as this is a constant variable, it's recommended to use all Capital letters to remember it.
-    int 
+    int points = get_int ("How many points did you loose?\n");
+
+    if (points < MINE)
+    {
+        printf("You have loose less points than me!\n");
+    }
+    else if (points > MINE)
+    {
+        printf("You hava loose more points than me!\n");
+    }
+    else
+    {
+        printf("You hava loose same number of points as me!\n");
+    }
+    return 0;
 }
