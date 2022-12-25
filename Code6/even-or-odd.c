753453a9c9d3e98e72a0897799498246cfdef35a
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-12-25 05:21:22 +0000

    Sun, Dec 25, 2022, 5:21 AM +00:00

--- Code6/even-or-odd.c
+++ Code6/even-or-odd.c
@@ -3,6 +3,15 @@
 
 int main(void)
 {
-    int n = get_iny("Enter the number you want to cheak: ");
-    
+    int n = get_int("Enter the number you want to cheak: ");
+
+    if (n%2 == 0)
+    {
+        printf("%i is an even number.\n", n);
+    }
+    else
+    {
+        printf("%i is an odd number.\n", n);
+    }
+    return 0;
 }
