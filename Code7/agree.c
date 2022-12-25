1b81a67d6fd0f837d238c10e064ae96a7953c92f
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2022-12-25 05:27:08 +0000

    Sun, Dec 25, 2022, 5:27 AM +00:00

---
+++ Code7/agree.c
@@ -0,0 +1,24 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // promt use to agree
+    char c = get_char("Do you agree? ");
+
+    // check weather user agreed
+    // if you are dealing with single characters also known as 'char', you will have to use single quotes ('') instead of Double Quotes ("")
+    if (c == 'y' || c == 'Y')
+    {
+        printf("Agreed.\n");
+    }
+    else if (c == 'n' || c == 'N')
+    {
+        printf("Not Agreed.\n");
+    }
+    else
+    {
+        printf("User Input is Wrong");
+        goto line 7
+    }
+}
