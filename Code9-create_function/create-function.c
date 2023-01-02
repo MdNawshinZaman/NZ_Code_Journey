722dc074a4209d1142fc0c8c0cb71a2445fc7c56
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-02 15:17:15 +0000

    Mon, Jan 2, 2023, 3:17 PM +00:00

--- Code9-create_function/create-function.c
+++ Code9-create_function/create-function.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 
 //lets create a new function
+void meow (void); //here the first 'void' is called the return value, 'meow' is the name of my function,
+                 //and the 2nd '(void)' is called the arguments (the taking value) to that function
 
-void main(void) //here the first 'void' is called the return value, 'meow' is the name of my function, and the 2nd '(void)' is called the arguments to that function
+int main(void)
 {
 /* when I moved the newly made function called 'meow' at the bottom after my main function,
 there is a tiny error appearing, but when I typed 'void main (void)' at the top after #include,
