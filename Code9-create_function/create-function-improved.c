44732ffcb0e25b8f9c9c143e2dd8ee2ba32307a3
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-02 15:54:55 +0000

    Mon, Jan 2, 2023, 3:54 PM +00:00

--- Code9-create_function/create-function-improved.c
+++ Code9-create_function/create-function-improved.c
@@ -8,8 +8,20 @@ void meow (int n)
     }
 }
 
+void hi(int h);
+
 int main(void)
 {
+    hi(2);
     meow(3);
 }
 
+void hi(int h)
+{
+    int a = 0;
+    while (a < h)
+    {
+        printf("Hi.\n");
+        a++;
+    }
+}
