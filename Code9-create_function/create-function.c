5c3ae1f85a15fe9bec7a480ace59c41f6bc64bd0
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-02 15:15:19 +0000

    Mon, Jan 2, 2023, 3:15 PM +00:00

--- Code9-create_function/create-function.c
+++ Code9-create_function/create-function.c
@@ -4,6 +4,11 @@
 
 void main(void) //here the first 'void' is called the return value, 'meow' is the name of my function, and the 2nd '(void)' is called the arguments to that function
 {
-
+/* when I moved the newly made function called 'meow' at the bottom after my main function,
+there is a tiny error appearing, but when I typed 'void main (void)' at the top after #include,
+my program knows that there is a function called 'meow'.
+That means, I have declared the that there is a Hand made function named "void meow (void)"
+As a result, the program search through the whole code for
+'meow' function before executing and as a result, the error is gone :) */
 
 }
