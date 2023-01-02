1518b5f0276acae3f0be728ce6ee2522b7d7cb20
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-02 13:15:17 +0000

    Mon, Jan 2, 2023, 1:15 PM +00:00

--- Code6/even-or-odd.c
+++ Code6/even-or-odd.c
@@ -5,7 +5,7 @@ int main(void)
 {
     int n = get_int("Enter the number you want to cheak: ");
 
-    if (n%2 == 0)
+    if (n%2 == 0) //n কে ২ দিয়ে ভাগ করে  ভাগশেষ যদি শুন্য হয়
     {
         printf("%i is an even number.\n", n);
     }
