f328e95c99bf554da81f604253a37c167719b8f8
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-02 14:21:35 +0000

    Mon, Jan 2, 2023, 2:21 PM +00:00

--- Code8/meow.c
+++ Code8/meow.c
@@ -3,11 +3,19 @@
 
 int main(void)
 {
-    //starting while loop
+    /*starting while loop
 
     while (1)
     {
         printf("Meow. \n");
+    }*/ //this part will run forever
+
+    int counter = 0;
+    while(counter < 5)
+    {
+        printf("meow.\n");
+        counter++;  // "counte = counter + 1" , "counter += 1" , "counter++" all are same
     }
     return 0;
+
 }
