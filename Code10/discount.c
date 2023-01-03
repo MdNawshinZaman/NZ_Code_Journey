179c5c1f60f89319dcdd44037f8bc1d30aaf05b7
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-03 12:57:26 +0000

    Tue, Jan 3, 2023, 12:57 PM +00:00

--- Code10/discount.c
+++ Code10/discount.c
@@ -11,16 +11,26 @@ int main (void)
     float sale = regular * .85;
     printf ("Sale price: %.2f\n", sale);
 }*/
-
+float discount (float price);
 int main(void)
 {
     char pr = '%';
-    float regular = get_float("Regular price: You are getting 15%c off.\nEnter the ammount: ");
+    float regular = get_float("Regular price: You are getting 15%c off.\nEnter the ammount: ",pr);
     float sale = discount (regular);
-    printf("Sale price: %.2f", sale);
+    printf("Sale price: %.2f\n", sale);
 }
+
+/*float discount (float price)
+{
+    float sale = .85 * price;
+    return sale; // here "return" is used for getting the value from the function  named "discount"
+}*/
+
+//this is the first method to make my own function and get value from it.
+//But there is an another and more effective way to do this.
+// Doing that below.
 
 float discount (float price)
 {
-    
+    return .85 * price;
 }
