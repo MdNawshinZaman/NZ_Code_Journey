3a08f62d043db1a0a351a6fc6059d81f2a93c12f
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-03 12:40:52 +0000

    Tue, Jan 3, 2023, 12:40 PM +00:00

--- Code10/discount.c
+++ Code10/discount.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main(void)
+
+int main (void)
 {
-    
+    char ch = '%';
+    float regular = get_float ("Regular price: you are getting 15%c off.\n",ch);
+    float sale = regular * .85;
+    printf ("Sale price: %.2f", sale);
 }
