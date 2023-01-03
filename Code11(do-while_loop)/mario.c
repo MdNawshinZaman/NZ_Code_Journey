33b72e495726b6cba300386f86e411057f7f44f6
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-03 13:16:18 +0000

    Tue, Jan 3, 2023, 1:16 PM +00:00

--- Code11(do-while_loop)/mario.c
+++ Code11(do-while_loop)/mario.c
@@ -6,5 +6,18 @@
 int main(void)
 {
     int n;
-    
+    do
+    {
+        n = get_int("Size: ");
+    }
+    while (n<1);
+    /*By using do-while loop, I can set a logic criteria.
+    For Example, by using do-while loop in this code, I have easily set a Criteria that,
+    if the user input a value is less than 1, then my code won't take the value instead, it
+    will run the do-while loop and ask for a value again.
+
+    If I had used "if()" to set my criteria and the input value was less than 1,
+    my code would just finished without doing any thing. But now I can keep putting my value
+    until I get my desired result!!
+
 }
