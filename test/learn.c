670d39796bebee288edd1f14476d7b6487931ce9
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-18 14:57:21 +0000

    Wed, Jan 18, 2023, 2:57 PM +00:00

--- test/learn.c
+++ test/learn.c
@@ -1,7 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
+//writing a program to give 15% discount to my customer
+
 int main(void)
 {
-    
+    char pr = '%';
+    float regular = get_float("Welcome to the shop!\nYou will get 15%c off.\nPlease enter your price: ",pr);
+    float sale = regular*(100-(15/100));
+    printf("Sale price: %f\n",sale);
+    return 0;
 }
