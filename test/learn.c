b46f4ff6e3cfe8ab00915b0fe815d0ea725ba34a
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-18 14:51:09 +0000

    Wed, Jan 18, 2023, 2:51 PM +00:00

--- test/learn.c
+++ test/learn.c
@@ -3,13 +3,5 @@
 
 int main(void)
 {
-
-}
-
-
-#include <stdio.h>
-int main ()
-{
-    printf ("Assalamu Alikukm\nName: Rafiur Rahman Zihan\nUniversity: Daffodil International University\nStudent I'D: 0242310005101857\n");
-    return 0;
+    
 }
