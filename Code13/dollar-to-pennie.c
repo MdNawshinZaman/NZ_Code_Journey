7292552e32983a65810986395969f51972313d50
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-19 03:38:25 +0000

    Thu, Jan 19, 2023, 3:38 AM +00:00

--- Code13/dollar-to-pennie.c
+++ Code13/dollar-to-pennie.c
@@ -6,8 +6,9 @@ int main(void)
 {
     float dollar;
     dollar = get_float("Enter your ammount in dollar: ");
-    int pennies = dollar * 100;
+    int pennies = round(dollar * 100); //here without the round() fuction, my code was probably saving of 4.19999..... refer to 4.20
+                                       // As a result, this code can't convert 4.20$ to pennies without the round() function!!!
 
-    printf("%.3f dollar is equal to %i pennies.\n",dollar, pennies);
+    printf("%.2f dollar is equal to %i pennies.\n",dollar, pennies);
     return 0;
 }
