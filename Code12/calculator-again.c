3990ff39b0c4d424990155990bfda038acf72fee
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-19 03:21:47 +0000

    Thu, Jan 19, 2023, 3:21 AM +00:00

--- Code12/calculator-again.c
+++ Code12/calculator-again.c
@@ -5,7 +5,9 @@ int main(void)
 {
     int x = get_int ("x: ");
     int y = get_int ("y: ");
-    float z = x/y;
+float z = (float) x/ (float) y; //এখানে x and y এর সামনে () এর মধ্যে float লেখার মাধ্যমে এটা বুঝায় যে,
+                                //আমার user input গুলো int value হলেও output value গুলো float type এরও হতে পারে।
+                                // () এর ভিতরে float না লিখলে, ভাহ করার পর ভাগফলে যদি কোন floating value থাকতো, তবে তা হারিয়ে যেতো।
 
     printf("value of z is: %.3f\n", z);
     return 0;
