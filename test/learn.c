fd554c5264e9fee9c6e64288443df632adc9d4b5
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-19 15:09:18 +0000

    Thu, Jan 19, 2023, 3:09 PM +00:00

--- test/learn.c
+++ test/learn.c
@@ -2,12 +2,5 @@
 #include<cs50.h>
 
 int main(void){
-    string s = "HI!\n";
-    string t = "BYE!\n";
-
-    printf("%s",s);
-    printf("%i %i %i %i\n", s[0], s[1], s[2], s[3]);
-
-    printf("%s",t);
-    printf("%i %i %i %i %i \n", t[0], t[1], t[2], t[3], t[4]);
+    
 }
