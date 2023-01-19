802c30d97e088ba06a3947842b22f9e5a9d72bce
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-19 03:48:34 +0000

    Thu, Jan 19, 2023, 3:48 AM +00:00

--- Code14/arrays.c
+++ Code14/arrays.c
@@ -7,4 +7,4 @@ int main(void)
     printf("Hello, %s\n",name);
 }
 //this program does not run in CodeBlocks. it will run in cs50.io
-//
+//"clang -o arrays arrays.c -lcs50" this is the command I used to compile this code in cs50 IDE
