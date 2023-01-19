6dec3b360cf9c342f3ba7095fb641f79f9949d84
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-19 03:56:51 +0000

    Thu, Jan 19, 2023, 3:56 AM +00:00

--- Code14/name_without_cs50.c
+++ Code14/name_without_cs50.c
@@ -1,3 +1,6 @@
+// this code runs in CodeBlocks but does't run in cs50.io
+//Most probably the gets() function is out dated now because of its security threats
+
 #include<stdio.h>
 
 int main(void)
@@ -5,5 +8,5 @@ int main(void)
     char s1[50];
     printf("Please enter your name: ");
     gets (s1);
-    printf("Welcome abroad, %s. Hope we will pass a great time together!");
+    printf("Welcome abroad, %s. Hope we will pass a great time together!", s1);
 }
