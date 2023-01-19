39f6d395356c2cc678dbe545442dba566a02cf78
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-19 16:29:40 +0000

    Thu, Jan 19, 2023, 4:29 PM +00:00

--- test/learn.c
+++ test/learn.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
 #include<cs50.h>
 
-int string_length(s)
+int string_length(string n){
+    int i = 0;
+    while(n[i] != '\0'){
+        i++;
+    }
+    return i;
+}
+
 
 int main(void){
-    string name = get_string("Name: ")
+    string name = get_string("Name: ");
     int length = string_legnth(name);
     printf("%i\n",length);
 }
