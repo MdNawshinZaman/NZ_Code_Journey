e5f194c85b6eabd5b89ecc6aa85c293d1b79db71
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-19 02:26:09 +0000

    Thu, Jan 19, 2023, 2:26 AM +00:00

--- test/learn.c
+++ test/learn.c
@@ -5,9 +5,5 @@
 
 int main(void)
 {
-    char pr = '%';
-    float regular = get_float("Welcome to the shop!\nYou will get 15%c off.\nPlease enter your price: ",pr);
-    float sale = regular *(100-15)/100;
-    printf("Sale price: %.2f\n",sale);
-    return 0;
+   float 
 }
