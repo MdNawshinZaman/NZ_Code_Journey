6b96c1519b894614d55ca497fdf20f4ec82fa654
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-19 11:19:09 +0000

    Thu, Jan 19, 2023, 11:19 AM +00:00

--- test/learn.c
+++ test/learn.c
@@ -1,28 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void)
-{
-    int n;
-    /*do
-    {
-        n = get_int("Enter the size: ");
-    }
-    while (n<1);*/
-
-    while(1)
-    {
-        n = get_int("Enter the size: ");
-        if(n>1)
-        {
-            break;
-        }
-    }
-
-    for(int i=0; i<n; i++)
-    {
-        printf("?");
-    }
-    printf("\n");
-    return 0;
+int main(void){
+    for
 }
