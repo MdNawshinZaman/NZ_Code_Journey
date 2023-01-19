6e55a96b88a4e0dbb7e9e2bbacf119712c8ecb5c
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-19 03:53:21 +0000

    Thu, Jan 19, 2023, 3:53 AM +00:00

---
+++ Code14/name_without_cs50.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(void)
+{
+    char s1[50];
+    printf("Please enter your name: ");
+    gets (s1);
+    printf("Welcome abroad, %s. Hope we will pass a great time together!");
+}
