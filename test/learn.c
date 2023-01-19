caa4c4282e1e4b5e4b585c36f56a12d6bc36aaaf
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-19 12:58:50 +0000

    Thu, Jan 19, 2023, 12:58 PM +00:00

--- test/learn.c
+++ test/learn.c
@@ -2,11 +2,5 @@
 #include <stdio.h>
 int get_negative_int(void);
 int main(void){
-    int sc1, sc2, sc3;
-
-    sc1 = 72;
-    sc2 = 73;
-    sc3 = 33;
-
-    printf("Averege: %.1f\n", (sc1+sc2+sc3)/3.0);
+    
 }
