ba508ea8d7ccf79f2e4e6dfc85db043d6b68273e
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-19 13:04:30 +0000

    Thu, Jan 19, 2023, 1:04 PM +00:00

--- test/learn.c
+++ test/learn.c
@@ -2,5 +2,13 @@
 #include <stdio.h>
 int get_negative_int(void);
 int main(void){
-    
+    int n = get_int("How many numbers? \n");
+
+    int scores[n];
+    for(int i = 0; i < n; i++)
+    {
+        scores[i] = get_int("Enter numebr: ");
+    }
+
+    printf("Avarage of the first 3 input is %f\n", (scores[0]+scores[1]+scores[2])/3.0);
 }
