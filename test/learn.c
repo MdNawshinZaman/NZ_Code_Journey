117564d591418d289b8143a7480063b822fa1b47
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-21 08:15:32 +0000

    Sat, Jan 21, 2023, 8:15 AM +00:00

--- test/learn.c
+++ test/learn.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-
+#include <math.h>
 int main() {
 
-    int code1, code2, unit1, unit2;
-    float price1, price2, total;
-
-    scanf("%d %d %f", &code1, &unit1, &price1);
-    scanf("%d %d %f", &code2, &unit2, &price2);
-
-    total = (unit1*price1)+(unit2*price2);
-    printf("VALOR A PAGAR: R$ %.2f\n",total);
+    double pi=3.14159, r, vol;
+    scanf("%lf",&r);
+    vol = (4/3.0)*pi*pow(r,3);
+    printf("VOLUME = %.3lf\n",vol);
 
     return 0;
 }
