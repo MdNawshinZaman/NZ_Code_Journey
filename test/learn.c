31d35cf6a57c726035a062bf2b1f155d47e737dc
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-21 03:00:06 +0000

    Sat, Jan 21, 2023, 3:00 AM +00:00

--- test/learn.c
+++ test/learn.c
@@ -1,8 +1,16 @@
 #include<cs50.h>
 #include<stdio.h>
+#include<string.h>
 #include<ctype.h>
 
 int main(void)
 {
-    string s = get_string("")
+    string s = get_string("Before: ");
+    printf("After: ");
+
+    for(int i = 0, j = strlen(s); i < j; i++)
+    {
+        if(islower(s[i]) != 0)
+        
+    }
 }
