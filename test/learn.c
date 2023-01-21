69b56c6f679e987e68334b3789a63f417cdf810a
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-21 08:29:02 +0000

    Sat, Jan 21, 2023, 8:29 AM +00:00

--- test/learn.c
+++ test/learn.c
@@ -5,11 +5,11 @@ int main() {
     double A, B, C, pi=3.14159;
     scanf("%lf %lf %lf",&A, &B, &C);
 
-    printf("TRIANGULO: %lf\n", .5*A*C);
-    printf("CIRCULO: %lf\n",pi*pow(C,2));
-    printf("TRAPEZIO: %lf\n", C*(A+B)/2.0);
-    printf("QUADRADO: %lf\n", pow(B,2));
-    printf("RETANGULO: %lf\n", A*B);
+    printf("TRIANGULO: %.3lf\n", .5*A*C);
+    printf("CIRCULO: %.3lf\n",pi*pow(C,2));
+    printf("TRAPEZIO: %.3lf\n", C*(A+B)/2.0);
+    printf("QUADRADO: %.3lf\n", pow(B,2));
+    printf("RETANGULO: %.3lf\n", A*B);
 
     return 0;
 }
