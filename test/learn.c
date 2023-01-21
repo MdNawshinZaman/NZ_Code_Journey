edffbba0aedf0284106e3d2c789a4cdc5e425970
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-21 10:09:07 +0000

    Sat, Jan 21, 2023, 10:09 AM +00:00

--- test/learn.c
+++ test/learn.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
+#include <math.h>
 int main() {
 
-    int X;
-    float Y, avgcon;
-    scanf("%d %f", &X, &Y);
-    avgcon = X/Y;
-    printf("%.3f km/l\n", avgcon);
-
+    double x1, y1, x2, y2, d;
+    scanf("%lf %lf %lf %lf", &x1, &y1, &x2, &y2);
+    d = sqrt(pow(x2-x1, 2) + pow(y2-y1, 2));
+    printf("%.4lf\n", d);
     return 0;
 }
