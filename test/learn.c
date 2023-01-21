317fa7a85a37cd1101ab3ef9aff1dc33ac36e573
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-21 11:15:15 +0000

    Sat, Jan 21, 2023, 11:15 AM +00:00

--- test/learn.c
+++ test/learn.c
@@ -2,9 +2,13 @@
 
 int main() {
 
-    int d, t;
-    scanf("%d",&d);
-    t = d*2.0;
-    printf("%d minutos\n",t);
+    int t, speed;
+    double d, fuel;
+
+    scanf("%d %d", &t, &speed);
+    d = t * speed;
+    fuel = d/12.0;
+
+    printf("%.3lf\n",fuel);
     return 0;
 }
