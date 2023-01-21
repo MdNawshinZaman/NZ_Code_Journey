bbd1237ced65ec02c25d078ec7d85db2774ddefe
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-21 11:39:24 +0000

    Sat, Jan 21, 2023, 11:39 AM +00:00

--- test/learn.c
+++ test/learn.c
@@ -23,7 +23,13 @@ int main() {
 
     g= (n - a*100 - b*50 - c*20 -d*10 - e*5 - f*2)/1;
 
-    printf("%d %d %d %d %d %d %d\n", a,b,c,d,e,f,g);
+    printf("%d nota(s) de R$ 100,00 \n",a);
+    printf("%d nota(s) de R$ 50,00 \n",b);
+    printf("%d nota(s) de R$ 20,00 \n",c);
+    printf("%d nota(s) de R$ 10,00 \n",d);
+    printf("%d nota(s) de R$ 5,00 \n",e);
+    printf("%d nota(s) de R$ 2,00 \n",f);
+    printf("%d nota(s) de R$ 1,00 \n",g);
 
     return 0;
 }
