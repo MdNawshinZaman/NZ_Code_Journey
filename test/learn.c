5230e71058541d12b1865e8be4dab79739734f1b
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-21 12:21:06 +0000

    Sat, Jan 21, 2023, 12:21 PM +00:00

--- test/learn.c
+++ test/learn.c
@@ -4,23 +4,12 @@ int main() {
 
     int n, a, b, c, d, e, f, g;
     scanf("%d",&n);
-
-    //total n = 576
-    a = n/100; //5 of 100$
-
-    //remains 76
-    b = (n- a*100)/50; //1 of 50$
-
-    //remains 26
-    c = (n- a*100 - b*50)/20; // 1 of 20$
-
-    //remains 6
+    a = n/100;
+    b = (n- a*100)/50;
+    c = (n- a*100 - b*50)/20;
     d = (n - a*100 - b*50 - c*20)/10;
-
     e = (n - a*100 - b*50 - c*20 -d*10)/5;
-
     f = (n - a*100 - b*50 - c*20 -d*10 - e*5)/2;
-
     g= (n - a*100 - b*50 - c*20 -d*10 - e*5 - f*2)/1;
 
     printf("%d nota(s) de R$ 100,00 \n",a);
