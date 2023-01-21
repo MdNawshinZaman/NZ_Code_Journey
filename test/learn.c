c4fc8bddb7435ed83538c053763bba8cd318c2fc
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-21 16:43:44 +0000

    Sat, Jan 21, 2023, 4:43 PM +00:00

--- test/learn.c
+++ test/learn.c
@@ -7,11 +7,12 @@ int main() {
     scanf("%d",&N);
 
     for(int i = 0; i < 3; i++){
-        time[i] = N % 60; 
+        time[i] = N % 60;
         N = N - (N % 60);
     }
 
-    for(int j = 2; j >= 0; j--){
+    int j;
+    for(j = 2; j >= 0; j--){
         printf("%d",time[j]);
         if(j > 0){
             printf(":");
