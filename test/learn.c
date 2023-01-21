67a78fa075589fb4f3b3b94317a6d6bb3a02a082
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-21 11:36:22 +0000

    Sat, Jan 21, 2023, 11:36 AM +00:00

--- test/learn.c
+++ test/learn.c
@@ -2,13 +2,28 @@
 
 int main() {
 
-    int t, speed;
-    double d, fuel;
+    int n, a, b, c, d, e, f, g;
+    scanf("%d",&n);
 
-    scanf("%d %d", &t, &speed);
-    d = t * speed;
-    fuel = d/12.0;
+    //total n = 576
+    a = n/100; //5 of 100$
 
-    printf("%.3lf\n",fuel);
+    //remains 76
+    b = (n- a*100)/50; //1 of 50$
+
+    //remains 26
+    c = (n- a*100 - b*50)/20; // 1 of 20$
+
+    //remains 6
+    d = (n - a*100 - b*50 - c*20)/10;
+
+    e = (n - a*100 - b*50 - c*20 -d*10)/5;
+
+    f = (n - a*100 - b*50 - c*20 -d*10 - e*5)/2;
+
+    g= (n - a*100 - b*50 - c*20 -d*10 - e*5 - f*2)/1;
+
+    printf("%d %d %d %d %d %d %d\n", a,b,c,d,e,f,g);
+
     return 0;
 }
