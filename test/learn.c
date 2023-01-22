742d2b9c4bea0f617adade61c3f10097a8357937
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-22 16:17:52 +0000

    Sun, Jan 22, 2023, 4:17 PM +00:00

--- test/learn.c
+++ test/learn.c
@@ -1,12 +1,24 @@
 #include<stdio.h>
+#include<math.h>
 
 int main(void){
 
-    double a, b, c;
+    double a, b, c, d, r1, r2;
     scanf("%lf %lf %lf", &a, &b, &c);
 
-    
-    if (a = 0)
+    d = pow(b, 2)-4 * a * c;
+
+    if (a == 0 || d < 0)
+    {
+        printf("Impossivel calcular;");
+    }
+    else{
+    r1 = (-b + sqrt(d))/ 2*a ;
+    r2 = (-b - sqrt(d))/ 2*a ;
+
+    printf("R1 = %.5lf\n",r1);
+    printf("R2 = %.5lf\n",r2);
+    }
 
     return 0;
 }
