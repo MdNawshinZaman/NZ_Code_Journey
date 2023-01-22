88a6093b2e299ca12cffd0d23a3061f3584655c3
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-22 12:42:48 +0000

    Sun, Jan 22, 2023, 12:42 PM +00:00

--- test/learn.c
+++ test/learn.c
@@ -1,17 +1,5 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-
-    int days, y, m, d;
-    scanf("%d", &days);
-
-    y = days/365;
-    days = days - (y*365);
-    m = days/30;
-    days = days-(m*30);
-    d = days;
-
-    printf("%d ano(s)\n%d mes(es)\n%d dia(s)\n", y, m, d);
-
-    return 0;
+int main(void){
+    
 }
