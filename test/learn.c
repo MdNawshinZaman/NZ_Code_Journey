a9e0cf750b4e1dc4c3e0dbda876c8b7d011495b5
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-22 12:52:03 +0000

    Sun, Jan 22, 2023, 12:52 PM +00:00

--- test/learn.c
+++ test/learn.c
@@ -4,5 +4,9 @@ int main(void){
     double N;
     scanf("%lf",&N);
 
-    int
+    int note[6] = {100, 50, 20, 10, 5, 2};
+    float coin[6] = {1.00, 0.50, 0.25, 0.10, 0.05, 0.01};
+
+    int i;
+    for(i = 0; )
 }
