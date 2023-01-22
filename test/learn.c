a314d71ca7c1df1714144c1d1c60377e73066f8d
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-22 13:00:47 +0000

    Sun, Jan 22, 2023, 1:00 PM +00:00

--- test/learn.c
+++ test/learn.c
@@ -7,6 +7,18 @@ int main(void){
     int note[6] = {100, 50, 20, 10, 5, 2};
     float coin[6] = {1.00, 0.50, 0.25, 0.10, 0.05, 0.01};
 
-    int i;
-    for(i = 0; )
+    int nota[6], moeda[6];
+    for(int i = 0; i < 6; i++)
+    {
+        nota[i] = N / note[i];
+        N = N % note[i];
+    }
+    printf("%.2lf",N);
+    /*for(int j = 0; j < 6; j++)
+    {
+        moeda[i] = N / coin[i];
+        N = N % coin[i]
+    }*/
+
+    return 0;
 }
