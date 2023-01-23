7e88671bd0bd0938bf126efaab999eba81f8664f
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-23 05:27:09 +0000

    Mon, Jan 23, 2023, 5:27 AM +00:00

--- test/learn.c
+++ test/learn.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
 int main(void){
-    int x, y;
+    int x, y, total;
     scanf("%d %d", &x, &y); // x=product code, y= quantity of the product
 
     if(x==1)
     {
-        
+        total = 
     }
 
 }
