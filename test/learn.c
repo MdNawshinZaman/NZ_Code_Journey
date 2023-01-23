5f12c268f3ca75b67f92a47a5cad78a615cf6172
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-23 06:07:14 +0000

    Mon, Jan 23, 2023, 6:07 AM +00:00

--- test/learn.c
+++ test/learn.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 
 int main(void){
-    float n1, n2, n3, n4;
+    int a=2, b=3, c=4, d=1;
+    float n1, n2, n3, n4, avg;
     scanf("%f %f %f %f", &n1, &n2, &n3, &n4);
 
-    
+    avg = (n1*a + n2*b + n3*c + n4*d)/()
 
 }
