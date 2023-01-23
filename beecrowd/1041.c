634f9501226e0eff1fe852ac23ae13f6e09b0799
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-23 16:44:36 +0000

    Mon, Jan 23, 2023, 4:44 PM +00:00

--- beecrowd/1041.c
+++ beecrowd/1041.c
@@ -5,8 +5,18 @@ int main(void){
     float a,b;
     scanf("%f %f", &a, &b);
 
-    if(x==0 && y==0)
-        printf("Origem")
+    if(x==0 && y==0){
+        printf("Origem\n");
+    }
+    else if(x>0 && y==0){
+        printf("Eixo X\n");
+    }
+    else if(y>0 && x==0){
+        printf("Eixo Y\n");
+    }
+    else if(x>0 && y==0){
+        printf("Eixo X\n");
+    }
 
     return 0;
 }
