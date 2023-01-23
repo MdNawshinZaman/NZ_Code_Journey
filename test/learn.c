ddddf66174ecb5909f0feaaa5ee01fed6f1fbbd1
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-23 04:54:20 +0000

    Mon, Jan 23, 2023, 4:54 AM +00:00

---
+++ test/learn.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void){
+    float a;
+    scanf("%f",&a);
+
+    if(a>=0 && a<=25){
+        print("Intervalo [0,25]\n");
+    }
+    else if(a>=0 && a<=25){
+        print("Intervalo [0,25]\n");
+    }
+}
