6c73123e72654ef6e8b1dc01f324f230a7261027
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-23 05:33:07 +0000

    Mon, Jan 23, 2023, 5:33 AM +00:00

--- test/learn.c
+++ test/learn.c
@@ -1,12 +1,34 @@
 #include<stdio.h>
 
 int main(void){
-    int x, y, total;
+    int x, y;
+    float total;
     scanf("%d %d", &x, &y); // x=product code, y= quantity of the product
 
     if(x==1)
     {
-        total = 
+        total = y * 4.00;
+        printf("Total: R$ %.2f\n",total);
+    }
+    else if(x==2)
+    {
+        total = y * 4.50;
+        printf("Total: R$ %.2f\n",total);
+    }
+    else if(x==3)
+    {
+        total = y * 5.00;
+        printf("Total: R$ %.2f\n",total);
+    }
+    else if(x==4)
+    {
+        total = y * 2.00;
+        printf("Total: R$ %.2f\n",total);
+    }
+    else if(x==5)
+    {
+        total = y * 1.50;
+        printf("Total: R$ %.2f\n",total);
     }
 
 }
