86c1f452d074078ceefc9ec9594f4fdec5541ef0
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-23 16:46:21 +0000

    Mon, Jan 23, 2023, 4:46 PM +00:00

--- beecrowd/1041.c
+++ beecrowd/1041.c
@@ -14,8 +14,17 @@ int main(void){
     else if(y>0 && x==0){
         printf("Eixo Y\n");
     }
-    else if(x>0 && y==0){
-        printf("Eixo X\n");
+    else if(x>0 && y>0){
+        printf("Q1\n");
+    }
+    else if(x<0 && y>0){
+        printf("Q2\n");
+    }
+    else if(x<0 && y<0){
+        printf("Q3\n");
+    }
+    else if(x>0 && y<0){
+        printf("Q4\n");
     }
 
     return 0;
