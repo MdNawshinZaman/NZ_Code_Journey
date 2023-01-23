2b677718d2d9ccec0e0684a18f35307882354a59
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-23 13:25:10 +0000

    Mon, Jan 23, 2023, 1:25 PM +00:00

--- beecrowd/1040.c
+++ beecrowd/1040.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
 int main(void){
-    
+    float a, b, c, d, avg;
+    scanf("%f%f%f%f", &a, &b, &c, &d);
+
+    a = a * 2;
+    b = b * 3;
+    c *= 4;
+    d *= 1;
+
+    avg = (a+b+c+d)/10;
+
+    printf("%f",c);
+    printf("%f",avg);
+
+
+    return 0;
 }
