16897c0ad9f4d603a16cb4fb78cb425822d45e0a
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-24 08:29:52 +0000

    Tue, Jan 24, 2023, 8:29 AM +00:00

--- beecrowd/1042.c
+++ beecrowd/1042.c
@@ -1,8 +1,15 @@
 #include<stdio.h>
 
 int main(void){
-    int a, b, c;
+    int a, b, c, A, B, C;
     scanf("%d %d %d", &a, &b, &c);
 
-    
+     A = (a + b + abs(a-b))/2;
+     A = (A + c + abs(A-c))/2;
+
+     B = (a + b + abs(a-b))/2;
+     A = (A + c + abs(A-c))/2;
+
+     A = (a + b + abs(a-b))/2;
+     A = (A + c + abs(A-c))/2;
 }
