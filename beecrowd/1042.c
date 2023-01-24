a474e47e221807bf1e715da7f1849f25bbfbab8b
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-24 13:31:15 +0000

    Tue, Jan 24, 2023, 1:31 PM +00:00

--- beecrowd/1042.c
+++ beecrowd/1042.c
@@ -1,16 +1,20 @@
 #include<stdio.h>
 
 int main(void){
-    int a, b, c, temp=0;
-    scanf("%d %d %d", &a, &b, &c);
+    int A, B, C, a, b, c, temp=0;
+    scanf("%d %d %d", &A, &B, &C);
 
-
+    a=A;
+    b=B;
+    c=C;
 
     if(a>b){temp=a; a=b; b=temp;}
     if(a>c){temp=a; a=c; c=temp;}
     if(b>c){temp=b; b=c; c=temp;}
 
-    
+    // I will get a<b<c
+
+    printf("%d\n%d\n%d\n\n%d\n%d\n%d\n", a,b,c,A,B,C);
 
     return 0;
 }
