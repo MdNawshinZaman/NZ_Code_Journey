b11da70f9955f1ff63320dced6d77b1c8ae76005
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-24 11:39:00 +0000

    Tue, Jan 24, 2023, 11:39 AM +00:00

--- beecrowd/1042.c
+++ beecrowd/1042.c
@@ -7,8 +7,8 @@ int main(void){
 
     // input_2 21 7 -14
 
-    if(a>b){temp=a; a=b; b=c;}    //temp=21, a=7, b=-14, c=
-    if(temp>b){a=temp; a=b; b=c;}    //temp=, a=21, b=, c=
+    if(a>b){temp=a; a=b; b=c; c=temp;}    //temp=21, a=7, b=-14, c=21
+    if(temp>b){temp=a; a=c; c=temp;}    //temp=7, a=21, b=-14, c=7
     if(b>c){temp=a; a=b; b=c:}    //temp=21, a=7, b=-14, c=21
 
     return 0;
