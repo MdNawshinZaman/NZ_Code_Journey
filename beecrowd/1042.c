e71c5c12bf75708d8ed25c057a0c1f9635bba0ee
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-24 11:19:01 +0000

    Tue, Jan 24, 2023, 11:19 AM +00:00

--- beecrowd/1042.c
+++ beecrowd/1042.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
 
 int main(void){
-    int a, b, c, temp;
+    int a, b, c, temp=0;
     scanf("%d %d %d", &a, &b, &c);
 
-    if(a>b){}
+    //input 7 21 -14
+
+    if(a>b){temp=a; a=b; b=c; c=temp;} // temp=0, a=7, b=24, c=-14
+    if(a>b){temp=a; a=b; b=c; c=temp;} //
+    if(a>b){temp=a; a=b; b=c; c=temp;}
 
     return 0;
 }
