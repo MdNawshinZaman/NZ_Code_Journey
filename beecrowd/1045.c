11fb8104fbd082c4ec3438700139cd33b93f99b9
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-25 08:49:48 +0000

    Wed, Jan 25, 2023, 2:49 PM +06:00

--- beecrowd/1045.c
+++ beecrowd/1045.c
@@ -15,8 +15,8 @@ int main(void){
 
     if(a >= b+c){printf("NAO FORMA TRIANGULO\n");}
     if(pow(a,2) == (pow(b,2)+pow(c,2))){printf("TRIANGULO RETANGULO\n");}
-    if(pow(a,2) > (pow(b,2)+pow(c,2))){printf("TRIANGULO OBTUSANGULO\n");}
-    if(pow(a,2) < (pow(b,2)+pow(c,2))){printf("TRIANGULO ACUTANGULO\n");}
+    if(pow(a,2) > (pow(b,2) + pow(c,2))){printf("TRIANGULO OBTUSANGULO\n");}
+    if(pow(a,2) < (pow(b,2) + pow(c,2))){printf("TRIANGULO ACUTANGULO\n");}
     if(a == b == c){printf("TRIANGULO EQUILATERO\n");}
     if(a==b!=c || a==c!=b || a!=b==c){printf("TRIANGULO ISOSCELES\n");}
 
