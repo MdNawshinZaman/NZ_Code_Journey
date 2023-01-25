aa6b3292c12cb6ce47ef3c27fca1a029c226ad71
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-25 12:21:10 +0000

    Wed, Jan 25, 2023, 6:21 PM +06:00

--- beecrowd/1045.c
+++ beecrowd/1045.c
@@ -7,20 +7,23 @@ int main(void){
     scanf("%lf %lf %lf", &a, &b, &c);
 
     //obj: a<b<c
-    if(a>b){temp=a; a=b; b=temp;}
-    if(a>c){temp=a; a=c; c=temp;}
-    if(b>c){temp=b; b=c; c=temp;}
+    if(a<b){temp=a; a=b; b=temp;}
+    if(a<c){temp=a; a=c; c=temp;}
+    if(b<c){temp=b; b=c; c=temp;}
 
     //printf("%lf %lf %lf\n", a, b, c);
 
     if(a >= b+c){printf("NAO FORMA TRIANGULO\n");}
-    if(pow(a,2) == (pow(b,2)+pow(c,2))){printf("TRIANGULO RETANGULO\n");}
-    if(pow(a,2) > (pow(b,2) + pow(c,2))){printf("TRIANGULO OBTUSANGULO\n");}
-    if(pow(a,2) < (pow(b,2) + pow(c,2))){printf("TRIANGULO ACUTANGULO\n");}
-    if(a == b == c){printf("TRIANGULO EQUILATERO\n");}
-    if(a==b!=c || a==c!=b || a!=b==c){printf("TRIANGULO ISOSCELES\n");}
-
+    else{
+        if(pow(a,2) == (pow(b,2)+pow(c,2))){printf("TRIANGULO RETANGULO\n");}
+        if(pow(a,2) > (pow(b,2) + pow(c,2))){printf("TRIANGULO OBTUSANGULO\n");}
+        if(pow(a,2) < (pow(b,2) + pow(c,2))){printf("TRIANGULO ACUTANGULO\n");}
+        if(a == b && b == c){printf("TRIANGULO EQUILATERO\n");}
+        if((a == b && a !=c) || (a == c && a !=b) || (b == c && b !=a)){printf("TRIANGULO ISOSCELES\n");}
+    }
+    
     return 0;
 }
 
 //this program taught me the differance of "if" and "else if" :)
+//this also taught me the use of || and &&
