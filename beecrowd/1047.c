be6f5b2e791fb56f891e5268960ae24cbe174003
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-25 13:30:56 +0000

    Wed, Jan 25, 2023, 7:30 PM +06:00

--- beecrowd/1047.c
+++ beecrowd/1047.c
@@ -2,11 +2,20 @@
 
 int main(void){
     int h1, m1, h2, m2;
-    float t1, t2;
+    float t1, t2, time;
     scanf("%d %d %d %d", &h1, &m1, &h2, &m2);
 
-    t1 = h1 + 
+    t1 = h1 + (m1/60.0);
+    t2 = h2 + (m2/60.0);
 
-    if()
+    if(t2-t1<=0){
+        t1 = 1440 - t1;
+        time = t1 + t2;
+    }
+    else{
+        time = t2 - t1;
+    }
+
+    printf("%f\n",time);
 
 }
