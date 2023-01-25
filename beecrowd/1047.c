2374672a61fc9e395ee924c447de8a21dba4db25
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-25 13:25:44 +0000

    Wed, Jan 25, 2023, 7:25 PM +06:00

--- beecrowd/1047.c
+++ beecrowd/1047.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 
 int main(void){
-    int h1, m1, h2, m2, t1, t2;
+    int h1, m1, h2, m2;
+    float t1, t2;
     scanf("%d %d %d %d", &h1, &m1, &h2, &m2);
 
-    t1 = 
+    t1 = h1 + 
 
     if()
 
