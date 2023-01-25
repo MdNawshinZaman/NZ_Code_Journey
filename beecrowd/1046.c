d92688f4c84c2de571246b16883e09dff0f2619d
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-25 13:14:49 +0000

    Wed, Jan 25, 2023, 7:14 PM +06:00

--- beecrowd/1046.c
+++ beecrowd/1046.c
@@ -5,18 +5,16 @@ int main(void){
     int a, b, t1, t2, time;
     scanf("%d %d", &a, &b);
 
-    if(b-a<0){
+    if(b-a<=0){
         t1 = 24-a;
         t2 = b;
-        time = t1+t2
+        time = t1+t2;
     }
     else{
-        
+        time = b - a;
     }
 
-    /*t1 = 24-a;
-    t2 = b;
-    time = t1+t2;
+    printf("O JOGO DUROU %d HORA(S)\n",time);
 
-    printf("O JOGO DUROU %d HORA(S)\n",time);*/
+    return 0;
 }
