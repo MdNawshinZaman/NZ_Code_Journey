c8d2da55fb7936a9b4ab11dd7d8b233916c3327b
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-25 08:38:54 +0000

    Wed, Jan 25, 2023, 2:38 PM +06:00

--- beecrowd/1045.c
+++ beecrowd/1045.c
@@ -14,11 +14,11 @@ int main(void){
     //printf("%lf %lf %lf\n", a, b, c);
 
     if(a >= b+c){printf("NAO FORMA TRIANGULO\n");}
-    else if(pow(a,2) == pow(b,2)+pow(c,2)){printf("TRIANGULO RETANGULO\n");}
-    else if(pow(a,2) > pow(b,2)+pow(c,2)){printf("TRIANGULO OBTUSANGULO\n");}
-    else if(pow(a,2) < pow(b,2)+pow(c,2)){printf("TRIANGULO ACUTANGULO\n");}
-    else if(a == b == c){printf("TRIANGULO EQUILATERO\n");}
-    else if(a==b!=c || a==c!=b || a!=b==c){printf("TRIANGULO ISOSCELES\n");}
+    if(pow(a,2) == pow(b,2)+pow(c,2)){printf("TRIANGULO RETANGULO\n");}
+    if(pow(a,2) > pow(b,2)+pow(c,2)){printf("TRIANGULO OBTUSANGULO\n");}
+    if(pow(a,2) < pow(b,2)+pow(c,2)){printf("TRIANGULO ACUTANGULO\n");}
+    if(a == b == c){printf("TRIANGULO EQUILATERO\n");}
+    if(a==b!=c || a==c!=b || a!=b==c){printf("TRIANGULO ISOSCELES\n");}
 
     return 0;
 }
