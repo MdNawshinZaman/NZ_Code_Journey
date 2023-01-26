e3ccc34cac5cd2bd9bb7215d365c2aecbef2cc79
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-26 11:29:52 +0000

    Thu, Jan 26, 2023, 5:29 PM +06:00

--- beecrowd/1049.c
+++ beecrowd/1049.c
@@ -5,9 +5,59 @@ int main(void){
     char n1[20], n2[20], n3[20];
     scanf("%s %s %s",&*n1, &*n2, &*n3);
 
-    printf("%s\n",n1);
-    printf("%s\n",n2);
-    printf("%s\n",n3);
+    if(strcmp(n1,"vertebrado") == 0)
+    {
+        if(strcmp(n2,"ave") == 0)
+        {
+            if(strcmp(n3,"carnivoro") == 0)
+            {
+                printf("aguia\n");
+            }
+            else
+            {
+                printf("pomba\n");
+            }
+        }
+        else
+        {
+            if(strcmp(n3,"onivoro") == 0)
+            {
+                printf("homem\n");
+            }
+            else
+            {
+                printf("vaca\n");
+            }
+        }
+    }
+    else
+    {
+        if(strcmp(n2,"inseto") == 0)
+        {
+            if(strcmp(n3,"hematofago") == 0)
+            {
+                printf("pulga\n");
+            }
+            else
+            {
+                printf("lagarta\n");
+            }
+        }
+        else
+        {
+            if(strcmp(n3,"hematofago") == 0)
+            {
+                printf("sanguessuga\n");
+            }
+            else
+            {
+                printf("minhoca\n");
+            }
+        }
+    }
+    //printf("%s\n",n1);
+    //printf("%s\n",n2);
+    //printf("%s\n",n3);
 
     return 0;
 }
