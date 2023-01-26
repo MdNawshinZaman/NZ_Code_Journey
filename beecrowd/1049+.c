7ab23dadbc9fb4aecd065ee7cc3df913cb5efd7e
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-26 11:47:41 +0000

    Thu, Jan 26, 2023, 5:47 PM +06:00

--- beecrowd/1049+.c
+++ beecrowd/1049+.c
@@ -6,8 +6,8 @@ int main(void){
     char n1[20], n2[20], n3[20];
     scanf("%s %s %s",&*n1, &*n2, &*n3);
 
-    strcmp(n1,"vertebrado")==0 ? (strcmp(n2,"ave")==0? (strcmp(n3,"carnivoro")==0? printf("aguia\n")):(printf("pomba\n"));) \
-    : (strcmp(n2,"ave")==0? (strcmp(n3,"carnivoro")==0? printf("aguia\n")):(printf("pomba\n")););
+    strcmp(n1,"vertebrado")==0 ? (strcmp(n2,"ave")==0? (strcmp(n3,"carnivoro")==0? printf("aguia\n")):(printf("pomba\n"));) :()\
+    ();
 
     return 0;
 }
