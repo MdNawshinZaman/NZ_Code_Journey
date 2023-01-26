71d7a2577352c7be4cd9db7be2bb100cce939e9b
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-26 01:34:43 +0000

    Thu, Jan 26, 2023, 7:34 AM +06:00

--- beecrowd/1049.c
+++ beecrowd/1049.c
@@ -3,11 +3,11 @@
 int main(void){
 
     char n1[20], n2[20], n3[20];
-    scanf("%s %s %s",&n1[20], &n2[20], &n3[20]);
+    scanf("%s %s %s",&n1, &n2, &n3);
 
-    printf("%c\n",n1[20]);
-    printf("%c\n",n2[20]);
-    printf("%c\n",n3[20]);
+    printf("%s\n",n1);
+    printf("%s\n",n2);
+    printf("%s\n",n3);
 
     return 0;
 }
