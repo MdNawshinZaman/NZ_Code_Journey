c602dda18eeaefa66807a95e79bdaf8d9d4d43b2
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-27 02:53:44 +0000

    Fri, Jan 27, 2023, 8:53 AM +06:00

--- beecrowd/1051.c
+++ beecrowd/1051.c
@@ -5,13 +5,7 @@ int main(void)
     float sal, n, s, m ,l, tax;
     scanf("%f",&sal);
 
-    //4520.00
-
-    n = sal - 2000.00; // 2520.00
-    sal = sal - n; // 2520.00
-
-//1000, 1500, 20
-
+    sal>2000? 
 
 
     tax = (n*0)+(s*0.08)+(m*0.18)+(l*0.28);
