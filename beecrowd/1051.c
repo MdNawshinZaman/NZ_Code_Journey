488d7af5d07a101f47b4a627a14577a0afaa6acb
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-27 03:11:38 +0000

    Fri, Jan 27, 2023, 9:11 AM +06:00

--- beecrowd/1051.c
+++ beecrowd/1051.c
@@ -2,15 +2,21 @@
 
 int main(void)
 {
-    float sal, n, s, m ,l, tax;
+    float sal, n, s, m ,l, tax, small, medium, large;
     scanf("%f",&sal);
 
     sal>2000? goto tax : goto tax_free;
 
 tax:
-    
+    n = sal - 2000.00;
+    s = sal - 3000.00;
+    m =
 
-    tax = (n*0)+(s*0.08)+(m*0.18)+(l*0.28);
+
+    n > 1000? small = 1000 * 0.08 : small = s *0.08;
+
+
+    tax = (s*0.08)+(m*0.18)+(l*0.28);
     printf("R$ %.2f\n", tax);
 
 tax_free:
