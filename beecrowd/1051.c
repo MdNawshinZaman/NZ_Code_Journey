3cdbc65dda52bc6e757136296b1562501d0e158e
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-27 03:19:32 +0000

    Fri, Jan 27, 2023, 9:19 AM +06:00

--- beecrowd/1051.c
+++ beecrowd/1051.c
@@ -6,17 +6,17 @@ int main(void)
     scanf("%f",&sal);
 
     sal>2000? goto tax : goto tax_free;
-//
+//3002.00
 tax:
-    s = sal - 2000.00; //
-    m = sal - 3000.00; //
-    l = sal - 4500.00; //
+    s = sal - 2000.00; //1002.0
+    m = sal - 3000.00; //2.00
+    l = sal - 4500.00; //-1498
 
 
 
-    s > 1000? small = 1000 * 0.08 : small = s *0.08; //
-    m > 1500? medium = 1500 * 0.18 : medium = m * 0.18;
-    large = l * 0.28;
+    s > 1000? small = 1000 * 0.08 : small = s *0.08; //80
+    m > 1500? medium = 1500 * 0.18 : medium = m * 0.18; //
+    l>0? large = l * 0.28 : large = 0;
 
     tax = small + medium + large;
 
