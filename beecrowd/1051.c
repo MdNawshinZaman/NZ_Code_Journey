cdcb818c4f41fe6ec8959960c191e93a709b44a3
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-27 03:05:42 +0000

    Fri, Jan 27, 2023, 9:05 AM +06:00

--- beecrowd/1051.c
+++ beecrowd/1051.c
@@ -8,13 +8,12 @@ int main(void)
     sal>2000? goto tax : goto tax_free;
 
 tax:
+    
 
     tax = (n*0)+(s*0.08)+(m*0.18)+(l*0.28);
-
     printf("R$ %.2f\n", tax);
 
 tax_free:
-
-
+    printf("Isento\n");
     return 0;
 }
