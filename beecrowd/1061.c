7138ab596495fcc373ba4ef0332391fcbd35bd09
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-27 11:23:57 +0000

    Fri, Jan 27, 2023, 5:23 PM +06:00

--- beecrowd/1061.c
+++ beecrowd/1061.c
@@ -1,6 +1,15 @@
 /*
    1. event starts and ends in april.
    2. main objective is to calculate the event time.
-   
 
+    a. consider that the minimum duration is 1 minute.
 */
+
+#include<stdio.h>
+
+int main(void){
+
+    
+
+    return 0;
+}
