34bf3f8b1cee3a6b49aac06e743de93f1189cfe6
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-27 11:11:55 +0000

    Fri, Jan 27, 2023, 5:11 PM +06:00

--- beecrowd/1060.c
+++ beecrowd/1060.c
@@ -2,7 +2,19 @@
 
 int main(void){
 
-    
+    float a[6];
+    for(int i=0, i<6, i++){
+        scanf("%f", &a[i]);
+    }
+
+    for(int j=0, k=0; j<6; j++){
+
+        if(a[j]>0){
+            k++;
+        }
+    }
+
+    printf("&d valores positivos\n", k);
 
     return 0;
 }
