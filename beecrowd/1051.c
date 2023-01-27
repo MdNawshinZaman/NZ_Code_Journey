907c862a30e9180fbaf8cbf43d4c3cfbbe2eaf50
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-27 03:25:34 +0000

    Fri, Jan 27, 2023, 9:25 AM +06:00

--- beecrowd/1051.c
+++ beecrowd/1051.c
@@ -5,29 +5,27 @@ int main(void)
     float sal, n, s, m ,l, tax, small, medium, large;
     scanf("%f",&sal);
 
-    sal>2000? goto tax : goto tax_free;
-//3002.00
-tax:
-    s = sal - 2000.00; //1002.0
-    m = sal - 3000.00; //2.00
-    l = sal - 4500.00; //-1498
+    //sal>2000? goto tax : goto tax_free;
 
+    if(sal > 2000){
 
+        //3002.00
+        s = sal - 2000.00; //1002.0
+        m = sal - 3000.00; //2.00
+        l = sal - 4500.00; //-1498
 
-    s > 1000? small = 1000 * 0.08 : small = s *0.08; //80
-    m > 1500? medium = 1500 * 0.18 : medium = m * 0.18; //0.36
-    l>0? large = l * 0.28 : large = 0; //0.00
+        s > 1000? (small = 1000 * 0.08) : (small = s *0.08); //80
+        m > 1500? (medium = 1500 * 0.18) : (medium = m * 0.18); //0.36
+        l>0? (large = l * 0.28) : (large = 0); //0.00
 
-    tax = small + medium + large;
+        tax = small + medium + large;
 
-    printf("R$ %.2f\n", tax);
-    goto end;
+        printf("R$ %.2f\n", tax);
+    }
 
-tax_free:
-    printf("Isento\n");
+    else{
+        printf("Isento\n");
+    }
     return 0;
 
-end:
-
-    return 0;
 }
