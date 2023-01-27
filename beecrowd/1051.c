93daa1f9bf76675bbf75f045ff693275213eb691
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-27 03:32:35 +0000

    Fri, Jan 27, 2023, 9:32 AM +06:00

--- beecrowd/1051.c
+++ beecrowd/1051.c
@@ -5,16 +5,16 @@ int main(void)
     float sal, n, s, m ,l, tax, small, medium, large;
     scanf("%f",&sal);
 
-    if(sal > 2000){
-
-        //3002.00
+    if(sal > 2000)
+    {
+      //3002.00
         s = sal - 2000; //1002.0
         m = sal - 3000; //2.00
         l = sal - 4500; //-1498
 
         s > 1000? (small = 1000 * 0.08) : (small = s *0.08); //80
         m > 1500? (medium = 1500 * 0.18) : (medium = m * 0.18); //0.36
-        l>0? (large = l * 0.28) : (large = 0); //0.00
+        l > 0? (large = l * 0.28) : (large = 0); //0.00
 
         tax = small + medium + large;
 
