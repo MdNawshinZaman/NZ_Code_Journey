6724c88f01a684e30548eb4f0d08a106734eade3
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-28 16:34:02 +0000

    Sat, Jan 28, 2023, 10:34 PM +06:00

--- beecrowd/1067.c
+++ beecrowd/1067.c
@@ -4,9 +4,12 @@ int main(void)
 
     int n = 1;
 
-    
+    printf("%d\n",n);
 
-    printf("%d\n", );
+    for(int i = 0; i < ; i++ )
+    {
+        printf("%d\n", n+2 );
+    }
 
     return 0;
 }
