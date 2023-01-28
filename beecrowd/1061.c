1fd7b2441b0a15e2d7d4e34ce4872a689acadcae
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-28 01:51:26 +0000

    Sat, Jan 28, 2023, 7:51 AM +06:00

--- beecrowd/1061.c
+++ beecrowd/1061.c
@@ -10,10 +10,11 @@
 int main(void){
 
     double day1, h1, m1 , s1, day2, h2, m2 ,s2, tot_sec;
-    char in1[100], in2[100], in3[100], in4[100]
+    char in1[100], in2[100], in3[100], in4[100];
 
-    scanf("%s %s %s %s", &in1, &in2, &in3, &in4);
-    
+    scanf("%s %s %s %s", &*in1, &*in2, &*in3, &*in4);
+
+    printf("%s\n%s\n%s\n%s\n",in1, in2, in3, in4);
 
     return 0;
 }
