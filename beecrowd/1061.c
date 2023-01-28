c93ab23256bb6c55268af8929b5c0c54d1f18133
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-28 04:23:59 +0000

    Sat, Jan 28, 2023, 10:23 AM +06:00

--- beecrowd/1061.c
+++ beecrowd/1061.c
@@ -17,8 +17,7 @@
 
 int main(void){
 
-    int day1=0, h1, m1 , s1, day2=0, h2, m2 ,s2, t_day, t_h, t_m, t_s;
-    double w_second;
+    int day1=0, h1, m1 , s1, day2=0, h2, m2 ,s2, t_day, t_h, t_m, t_s, w_second;
     char del[10], x[2];
 
     scanf("%s %d %d %s %d %s %d %s %d %d %s %d %s %d", del, &day1, &h1, x, &m1, x, &s1, del, &day2, &h2, x, &m2, x, &s2);
@@ -29,14 +28,14 @@ int main(void){
     t_m = (60-m1-1)+m2; //60
     t_s = (60-s1)+s2; //60
 
-    printf("%d\n", t_day);
-    printf("%d\n", t_h);
-    printf("%d\n", t_m);
-    printf("%d\n", t_s);
+    //printf("%d\n", t_day);
+    //printf("%d\n", t_h);
+    //printf("%d\n", t_m);
+    //printf("%d\n", t_s);
 
     w_second = (t_day * 24 *60 *60) + (t_h * 60 * 60) + (t_m * 60) + t_s;
 
-    //printf("\n\n%lf\n\n",w_second);
+    printf("\n\n%d\n\n",w_second);
 
 
 
