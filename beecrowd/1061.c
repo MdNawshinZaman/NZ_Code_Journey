2076637773e140ea84a1ac3bd4d13b5ef3a88b68
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-28 01:47:43 +0000

    Sat, Jan 28, 2023, 7:47 AM +06:00

--- beecrowd/1061.c
+++ beecrowd/1061.c
@@ -9,9 +9,10 @@
 
 int main(void){
 
-    double day1, h1, m1 , s1, day2, h2, m2 ,s2, t_s;
+    double day1, h1, m1 , s1, day2, h2, m2 ,s2, tot_sec;
+    char in1[100], in2[100], in3[100], in4[100]
 
-
+    
 
     return 0;
 }
