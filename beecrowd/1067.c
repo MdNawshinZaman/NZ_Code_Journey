b5dc42f856730b2c96437c0a36b2de34de876787
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-28 16:47:35 +0000

    Sat, Jan 28, 2023, 10:47 PM +06:00

--- beecrowd/1067.c
+++ beecrowd/1067.c
@@ -1,15 +1,19 @@
 #include<stdio.h>
 int main(void)
 {
-
+    int x = 11;
+    float y;
     int n = 1;
 
     printf("%d\n",n);
 
-    for(int i = 0; i < ; i++ )
+    y = round(x /2.0);
+    printf("%f\n", y);
+
+    /*for(int i = 0; i < ; i++ )
     {
         printf("%d\n", n+2 );
-    }
+    }*/
 
     return 0;
 }
