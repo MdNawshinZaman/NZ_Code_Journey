f921ae98a77ba4e7de1e431bbb4e317b678f862c
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-28 02:51:21 +0000

    Sat, Jan 28, 2023, 8:51 AM +06:00

--- beecrowd/1061.c
+++ beecrowd/1061.c
@@ -13,8 +13,7 @@ int main(void){
     char del[10], x[2];
 
     scanf("%s %d %d %s %d %s %d %s %d %d %s %d %s %d", del, &day1, &h1, x, &m1, x, &s1, del, &day2, &h2, x, &m2, x, &s2);
-
-    printf("\n\n%d\n\n", day2);
+    //printf("\n\n%d\n\n", day2);
 
     return 0;
 }
