c61f5e694bfbd37dba734a8a0a68965d93409a78
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-29 05:25:33 +0000

    Sun, Jan 29, 2023, 11:25 AM +06:00

--- beecrowd/1071.c
+++ beecrowd/1071.c
@@ -3,10 +3,23 @@
 int main(void)
 {
 
-   int x, y;
+   int x, y, temp, sum;
    scanf("%d %d", &x, &y);
 
-   
+   if(x<y){temp = x; x = y; y = temp;} //now x is the highest value and y is the lowest
+
+   if(y%2 == 0){sum = y + 2; }
+   eles{sum = y + 1;}
+   //now sum has the first value from where it would start summation.
+
+   for(int i=0; i<x; i++)
+   {
+
+        y = y+2;
+   }
+   sum = ();
+
+   printf("%d\n",sum);
 
     return 0;
 }
