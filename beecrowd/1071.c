ef4a6e6356b80eed8b38ebb4e583d7517c261fd8
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-01-29 05:33:18 +0000

    Sun, Jan 29, 2023, 11:33 AM +06:00

--- beecrowd/1071.c
+++ beecrowd/1071.c
@@ -9,8 +9,8 @@ int main(void)
 
    if(x<y){temp = x; x = y; y = temp;} //now x is the highest value and y is the lowest
 
-   if(y%2 == 0){sum = y + 2; }
-   else{sum = y + 1;}
+   if(y%2 == 0){sum = y + 1; }
+   else{sum = y + 2;}
    //now sum has the first value from where it would start summation.
 
     j = floor((x - y)/2);
