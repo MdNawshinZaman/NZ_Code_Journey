fad6a00495f3bc35f5b2d0c0a5f6a95add248c59
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-02-25 03:15:35 +0000

    Sat, Feb 25, 2023, 9:15 AM +06:00

--- test/learn.c
+++ test/learn.c
@@ -1,50 +1,40 @@
 #include<stdio.h>
 
-int mamainin(void){
-  mainmaintt a=2, b=3, c=4, d=1;
-    float n1, n2, n3, n4,n5, avg;
-    scanf("%f %f %f %f", &n1, &n2, &n3, &n4);
-
-
-
-    avg = (n1*a + n2*b + n3*c + n4*d)/(a+b+c+d);
-
-    printf("Media: %.1f\n",avg);
-
-    if(avg >= 7.0){
-        printf("Aluno em exame.\n");
-    }
-    else if(avg < 5.0){
-        printf("Aluno reprovado.\n");
-    }
-    else if(avg >= 5.0 && avg <= 6.9)
+int main()
+{
+    int price = 250, paid = 0, change, unit; // 1 unit = 2+1 pizza
+    while(1)
     {
-        printf("Aluno em exame.\n");
+        printf("\nEnter the ammount you paid: ");
+        scanf("%d", &paid);
+        if(paid == EOF){break;}
+
+        change = 0;
+        unit = 0;
+
+        if(paid > 250)
+        {
+            if(paid < 500)
+            {
+                change = paid - price;
+                printf("You are getting 1 pizza's, sir!\n");
+            }
+            else if(paid >= 500)
+            {
+                unit = paid / 500;
+                printf("You are getting %d pizza's, sir!\n", (unit*3));
+                price = unit * 500;
+                change = paid - price;
+            }
+        }
+        else
+        {
+            printf("You are getting no pizza!\n");
+            change = paid;
+        }
+
+        printf("Your Change is: %d$\n", change);
+
     }
-
-    if(avg >= 5.0)
-    {
-        scanf("%f",&n5);
-        printf("Nota do exame: %.1f\n",n5);
-
-
-    float avg_f;
-
-    avg_f = (avg+n5)/2;
-
-    if(avg_f >= 5.0)
-    {
-        printf("Aluno aprovado.\n");
-    }
-    else
-    {
-        printf("Aluno reprovado.\n");
-    }
-
-    printf("Media final: %.1f\n",avg_f);
-
-    }
-
     return 0;
-
 }
