dad862b238d535f6fef037b071a2d4583a4c22f4
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-02-25 09:32:00 +0000

    Sat, Feb 25, 2023, 3:32 PM +06:00

--- beecrowd/1789.c
+++ beecrowd/1789.c
@@ -5,14 +5,17 @@ int main(void)
     int l;
     while(scanf("%d", &l) != EOF)
     {
-        int speed[l];
+        int speed[l], level = 0;
+
 
         for(int i = 0; i < l; i++)
         {
             scanf("%d", &speed[i]);
+
+            if(speed[i] < 10){level = i}
         }
 
-        
+
 
     }
 
