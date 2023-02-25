507c883905f33cb77c70784c42499a2629afddc5
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-02-25 09:16:15 +0000

    Sat, Feb 25, 2023, 3:16 PM +06:00

--- beecrowd/1858.c
+++ beecrowd/1858.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-    int n, num = 0;
+    int n, num = 0, smallest, position;
     scanf("%d", &n);
     int hit[n];
 
@@ -12,7 +12,20 @@ int main(void)
         scanf("%d", &hit[num]);
         num++;
     }
-    
+
+    smallest = hit[0];
+    position = 0;
+
+    for(int i = 1; i < n; i++)
+    {
+        if(hit[i] < smallest)
+        {
+            smallest = hit[i];
+            position = i;
+        }
+    }
+
+    printf("%d\n", position+1);
 
     return 0;
 }
