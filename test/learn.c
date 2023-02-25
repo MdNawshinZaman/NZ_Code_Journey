62361c254fe3cef2f5eea2152f2b085c4b08ae24
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-02-25 03:52:58 +0000

    Sat, Feb 25, 2023, 9:52 AM +06:00

--- test/learn.c
+++ test/learn.c
@@ -2,12 +2,13 @@
 
 int main()
 {
-    double price = 250, paid, change, unit; // 1 unit = 2+1 pizza
+    double price = 250, paid, change, unit, get; // 1 unit = 2+1 pizza
     while(1)
     {
         change = 0;
         unit = 0;
         paid = 0;
+        get = 0;
         printf("\nEnter the ammount you paid: ");
         scanf("%lf", &paid);
         if(paid == EOF){break;}
@@ -17,18 +18,18 @@ int main()
             if(paid < 500)
             {
                 change = paid - price;
-                printf("You are getting 1 pizza's, sir!\n");
+                get = 1;
             }
             else if(paid >= 500)
             {
                 unit = paid / 500;
-                printf("You are getting %.0lf pizza's, sir!\n", (unit*3));
+                get = (unit * 3);
                 price = unit * 500;
                 change = paid - price;
             }
             if(change >= 250)
             {
-                
+
             }
         }
         else
