67adc77fef01002b49b9b6eb04c7872b27d498e5
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-02-25 09:38:05 +0000

    Sat, Feb 25, 2023, 3:38 PM +06:00

--- beecrowd/1789.c
+++ beecrowd/1789.c
@@ -5,18 +5,32 @@ int main(void)
     int l;
     while(scanf("%d", &l) != EOF)
     {
-        int speed[l], level = 0;
+        int speed[l];
 
 
         for(int i = 0; i < l; i++)
         {
             scanf("%d", &speed[i]);
-
-            if(speed[i] < 10){level = i}
         }
 
+        int sp = speed[0], level = 0;
+        for(int j = 0; j < l; j++)
+        {
+            if(sp < speed[j]){sp = speed[j];}
+        }
 
-
+        if(sp < 10)
+        {
+            printf("1\n");
+        }
+        else if(sp >= 10 && sp < 20)
+        {
+            printf("2\n");
+        }
+        else if(sp > 20)
+        {
+            printf("3\n");
+        }
     }
 
     return 0;
