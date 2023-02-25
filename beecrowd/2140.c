49037acce9565dd697ece39b6e57cf9d557e3404
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-02-25 03:21:37 +0000

    Sat, Feb 25, 2023, 9:21 AM +06:00

--- beecrowd/2140.c
+++ beecrowd/2140.c
@@ -1,1 +1,19 @@
-#include<>
+#include<stdio.h>
+
+int main(void)
+{
+    int bills[] = {2, 5, 10, 20, 50, 100};
+    int price, paid, change;
+    scanf("%d %d", &price, &paid);
+
+    change = paid - price;
+
+    int change_note[6];
+    for(int i = 5; i >= 0; i--)
+    {
+
+    }
+
+
+    return 0;
+}
