8abe0c1eda8430aca70ceab632977b70a8a65d48
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-02-25 05:48:57 +0000

    Sat, Feb 25, 2023, 11:48 AM +06:00

--- beecrowd/1858.c
+++ beecrowd/1858.c
@@ -2,6 +2,16 @@
 
 int main(void)
 {
+    int n, num = 0;
+    scanf("%d", &n);
+    int hit[n];
+
+
+    while(num < n)
+    {
+        scanf("%d", &hit[num]);
+        num++;
+    }
     
 
     return 0;
