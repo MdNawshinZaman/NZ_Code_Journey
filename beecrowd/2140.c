feb8aeadd89de62c91983947ee3301fc238fc5fd
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-02-25 03:36:43 +0000

    Sat, Feb 25, 2023, 9:36 AM +06:00

--- beecrowd/2140.c
+++ beecrowd/2140.c
@@ -4,27 +4,28 @@ int main(void)
 {
     int bills[] = {2, 5, 10, 20, 50, 100};
     int price, paid, change, sum = 0;
-    scanf("%d %d", &price, &paid);
+    while
+        scanf("%d %d", &price, &paid);
 
-    change = paid - price;
+        change = paid - price;
 
-    int change_note[6];
-    for(int i = 5; i >= 0; i--)
-    {
-        change_note[i] = paid / bills[i];
-        paid -= change_note[i] * bills[i];
+        int change_note[6];
+        for(int i = 5; i >= 0; i--)
+        {
+            change_note[i] = paid / bills[i];
+            paid -= change_note[i] * bills[i];
 
-        sum += change_note[i];
-    }
+            sum += change_note[i];
+        }
 
-    if(sum <= 2)
-    {
-        printf("possible\n");
-    }
-    else
-    {
-        printf("impossible\n");
-    }
+        if(sum <= 2)
+        {
+            printf("possible\n");
+        }
+        else
+        {
+            printf("impossible\n");
+        }
 
 
 
