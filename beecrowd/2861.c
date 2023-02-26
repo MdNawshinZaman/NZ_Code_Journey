63ba401f826f82554b6607b72655054fafdf430d
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-02-26 03:24:30 +0000

    Sun, Feb 26, 2023, 9:24 AM +06:00

--- beecrowd/2861.c
+++ beecrowd/2861.c
@@ -5,9 +5,11 @@ int main(void)
     int n;
     scanf("%d", &n);
 
+    char que[100];
     for(int i = 0; i < n; i++)
     {
-        gets
+        gets(que);
+        printf("gzuz\n");
     }
 
     return 0;
