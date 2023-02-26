ab39f65ddf93ccd96ccffa8b18b24e3ca9900a08
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-02-26 02:29:28 +0000

    Sun, Feb 26, 2023, 8:29 AM +06:00

--- beecrowd/1973.c
+++ beecrowd/1973.c
@@ -1,7 +1,12 @@
 /*
 Phudo code;
 1. first firm named Star 1 and the name contuniues serially
-2. 
+2. he steals 1 sheep from each farm if there are any
+then,
+3. moves to i+1 th farm depending on whether the number of sheep in Star i was, respectively, odd
+    or
+    moves to i-1 th farm depending on whether the number of sheep in Star i was, respectively, even
+
 
 */
 #include<stdio.h>
