647afdb3fb597692b777a455ebeba2c2384a6869
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-02-26 02:53:30 +0000

    Sun, Feb 26, 2023, 8:53 AM +06:00

--- beecrowd/1973.c
+++ beecrowd/1973.c
@@ -27,7 +27,7 @@ int main(void)
 
     for(int j = 0; j < n;)
     {
-        if(sheep[j] > 0){non_stolen += (sheep[j] - 1); attacked++;}
+        if(sheep[j] > 0){non_stolen += (sheep[j] -= 1); attacked++;}
         if(sheep[j] % 2 != 0){j += 1;}
         else if((sheep[j] % 2) == 0){j -= 1;}
 
