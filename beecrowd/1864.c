bc5ec4d738ccccc436501abc6c2f36a987d5b405
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-02-26 11:37:40 +0000

    Sun, Feb 26, 2023, 5:37 PM +06:00

--- beecrowd/1864.c
+++ beecrowd/1864.c
@@ -3,10 +3,16 @@
 
 int main(void)
 {
+    char line[34]={"LIFE IS NOT A PROBLEM TO BE SOLVED"};
+
     int a;
     scanf("%d", &a);
 
-
+    for(int i = 0; i < a; i++)
+    {
+        printf("%c", line[i]);
+    }
+    printf("\n");
 
     return 0;
 }
