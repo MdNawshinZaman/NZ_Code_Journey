8d95b0ef233b607c359f9560e8c22f7067c7bfaa
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-02-26 02:35:11 +0000

    Sun, Feb 26, 2023, 8:35 AM +06:00

--- beecrowd/1973.c
+++ beecrowd/1973.c
@@ -7,13 +7,25 @@ then,
     or
     moves to i-1 th farm depending on whether the number of sheep in Star i was, respectively, even
 4. if there is not stars left for him to go, he stops
-5. 
+5. The mad brother starts his Star Trek in Star 1, stealing a sheep from his own farm.
 
 */
 #include<stdio.h>
 
 int main(void)
 {
+    int n;
+    scanf("%d", &n);
+
+    int sheep[n];
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &sheep[i]);
+    }
+
+    int attacked = 0; non_stolen = 0;
+
+    
 
 
     return 0;
