8e9650d4e09f825244585e7fbfea38b858518f16
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-02-27 14:58:47 +0000

    Mon, Feb 27, 2023, 8:58 PM +06:00

--- codeforce/dp1.c
+++ codeforce/dp1.c
@@ -9,7 +9,13 @@ int main(void)
     for(int i = 0; i < t; i++)
     {
         scanf("%d %d", &n, &m);
-        
+
+        int num[m];
+        for(int j = 0; j < m; j++)
+        {
+            scanf("%d", &num[j]);
+        }
+
     }
 
     return 0;
