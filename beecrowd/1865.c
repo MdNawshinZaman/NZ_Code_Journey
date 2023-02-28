f2f7b7b01bfe0f8dc15f90ecf287d8c061619c49
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-02-28 16:19:15 +0000

    Tue, Feb 28, 2023, 10:19 PM +06:00

--- beecrowd/1865.c
+++ beecrowd/1865.c
@@ -2,7 +2,17 @@
 
 int main(void)
 {
-    
+    int n;
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+    {
+        char name[50];
+        int force = 0;
+        scanf("%s %d", name, &force);
+
+        
+    }
 
     return 0;
 }
