cf43fcd1ef456c6333ed1f42e6877ab86a5ef25f
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-01 13:34:20 +0000

    Wed, Mar 1, 2023, 7:34 PM +06:00

--- beecrowd/1129.c
+++ beecrowd/1129.c
@@ -16,10 +16,18 @@ int main(void)
 
         for(int i = 0; i < n; i++)
         {
-            int a = 0; b = 0, c = 0, d = 0, e = 0;
+            int a = 0, b = 0, c = 0, d = 0, e = 0;
             scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
 
-            
+            //<= 127 is black, >= 127 is white
+
+            a = (a <= 127)? 1 : 0;
+            b = (b <= 127)? 1 : 0;
+            c = (c <= 127)? 1 : 0;
+            d = (d <= 127)? 1 : 0;
+            e = (e <= 127)? 1 : 0;
+
+            if()
         }
     }
 
