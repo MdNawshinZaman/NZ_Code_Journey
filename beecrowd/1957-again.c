9d732b6833fa8f631c05b7d416b750b08c1f7d78
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-02 16:32:14 +0000

    Thu, Mar 2, 2023, 10:32 PM +06:00

--- beecrowd/1957-again.c
+++ beecrowd/1957-again.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main(void)
 {
@@ -7,51 +7,61 @@ int main(void)
 
     int digit_num = 1;
 
-    for(;;)
+    for (;;)
     {
         v = v / 10;
-        if(v > 0){digit_num++;}
-        if(v == 0){break;}
+        if (v > 0)
+        {
+            digit_num++;
+        }
+        if (v == 0)
+        {
+            break;
+        }
     }
-    //printf("%d\n", digit_num);
+    // printf("%d\n", digit_num);
 
     char num[digit_num];
 
     int hex_digit, i = 0;
-    for(;;)
+    for (;;)
     {
         hex_digit = v % 16;
         v = v / 16;
-        switch(hex_digit)
+        switch (hex_digit)
         {
-            case 10:
-                num[i] = 'A';
-                break;
-            case 11:
-                num[i] = 'B';
-                break;
-            case 12:
-                num[i] = 'C';
-                break;
-            case 13:
-                num[i] = 'D';
-                break;
-            case 14:
-                num[i] = 'E';
-                break;
-            case 15:
-                num[i] = 'F';
-                break;
-            default:
-                num[i] = hex_digit;
-                break;
+        case 10:
+            num[i] = 'A';
+            break;
+        case 11:
+            num[i] = 'B';
+            break;
+        case 12:
+            num[i] = 'C';
+            break;
+        case 13:
+            num[i] = 'D';
+            break;
+        case 14:
+            num[i] = 'E';
+            break;
+        case 15:
+            num[i] = 'F';
+            break;
+        default:
+            num[i] = hex_digit;
+            break;
         }
-        if(v == 0){break;}
+        if (v == 0)
+        {
+            break;
+        }
     }
 
-    for(int j = digit_num - 1; j > 0; j--)
+    for (int j = digit_num - 1; j > 0; j--)
     {
         printf("%c", num[j]);
     }
+    printf("\n");
     return 0;
 }
