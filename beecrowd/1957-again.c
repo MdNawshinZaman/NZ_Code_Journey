36535e5282a85a1002d9d34456edb9611b0f6f8c
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-02 16:18:30 +0000

    Thu, Mar 2, 2023, 10:18 PM +06:00

--- beecrowd/1957-again.c
+++ beecrowd/1957-again.c
@@ -14,7 +14,37 @@ int main(void)
     }
     //printf("%d\n", digit_num);
 
-    
+    int hex_digit;
+    for(;;)
+    {
+        hex_digit = v % 16;
+        switch(hex_digit)
+        {
+            case 10:
+                printf("A");
+                break;
+            case 11:
+                printf("B");
+                break;
+            case 12:
+                printf("C");
+                break;
+            case 13:
+                printf("D");
+                break;
+            case 14:
+                printf("E");
+                break;
+            case 15:
+                printf("F");
+                break;
+            default:
+                printf("%d", hex_digit);
+                break;
+        }
+    }
+
+
 
     return 0;
 }
