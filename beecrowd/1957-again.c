270122612b98a1a0a0a68779734dfd96221bc5fe
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-02 15:40:56 +0000

    Thu, Mar 2, 2023, 9:40 PM +06:00

---
+++ beecrowd/1957-again.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(void)
+{
+    unsigned long long int v;
+    scanf("%llu", &v);
+
+    int digit_num = 0;
+    for(;;)
+    {
+        v = v / 10;
+        if(v > 0){digit_num++;}
+        if(v == 0){break;}
+    }
+
+    printf("%d\n", digit_num);
+
+    return 0;
+}
