a10f4f29605ab56588787233e80f46ab861688f9
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-03 01:58:51 +0000

    Fri, Mar 3, 2023, 7:58 AM +06:00

--- beecrowd/1957-again.c
+++ beecrowd/1957-again.c
@@ -21,37 +21,16 @@ int main(void)
     }
     // printf("%d\n", digit_num);
 
-    char num[digit_num];
+    int num[digit_num];
 
     int hex_digit, i = 0;
     for (;;)
     {
         hex_digit = v % 16;
         v = v / 16;
-        switch (hex_digit)
-        {
-        case 10:
-            num[i] = 'A';
-            break;
-        case 11:
-            num[i] = 'B';
-            break;
-        case 12:
-            num[i] = 'C';
-            break;
-        case 13:
-            num[i] = 'D';
-            break;
-        case 14:
-            num[i] = 'E';
-            break;
-        case 15:
-            num[i] = 'F';
-            break;
-        //default:
-          //  num[i] = hex_digit;
-           // break;
-        }
+
+        num[i] = hex_digit;
+
         i++;
         if (v == 0)
         {
@@ -61,7 +40,13 @@ int main(void)
 
     for (int j = digit_num - 1; j > 0; j--)
     {
-        printf("%c", num[j]);
+        if(num[j] == 10){printf("A");}
+        else if(num[j] == 11){printf("B");}
+        else if(num[j] == 12){printf("C");}
+        else if(num[j] == 13){printf("D");}
+        else if(num[j] == 14){printf("E");}
+        else if(num[j] == 15){printf("F");}
+        else {printf("%d", num[j]);}
     }
     printf("\n");
     return 0;
