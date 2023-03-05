e455fcb911ca88fd59a2b4d6c47e7e6fb8b46af0
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-05 13:31:03 +0000

    Sun, Mar 5, 2023, 7:31 PM +06:00

--- beecrowd/2840.c
+++ beecrowd/2840.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
+#include<math.h>
 #define PI 3.1415
 #define int long long int
+
 int main(void)
 {
     int R, L;
+    double volume;
     scanf("%lld %lld", &R, &L);
 
-    
+    volume = ((float)4/3) * PI * pow(R , 3);
 
     return 0;
 }
