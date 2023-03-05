25ab27f0bfb894c2d25aba00d9ef748777dc64ee
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-05 02:33:46 +0000

    Sun, Mar 5, 2023, 8:33 AM +06:00

--- beecrowd/2456.c
+++ beecrowd/2456.c
@@ -15,7 +15,7 @@ int main(void)
         card1 = card[1];
         for (int j = 2; j < 5; j++)
         {
-            if(card1 !< card[j]){a++;}//if a == 0 then ascending
+            if(card1 !< card[j]){a++;d++;}//if a == 0 then ascending
         }
     }
     else if (card1 > card[1])
@@ -23,11 +23,13 @@ int main(void)
         card1 = card[1];
         for (int j = 2; j < 5; j++)
         {
-            if(card1 !> card[j]){d++;}//if d == 0 then descending
+            if(card1 !> card[j]){a++;d++;}//if d == 0 then descending
         }
     }
 
-    if(a == 0 )
+    if(a == 0){printf("C\n");}
+    else if(d == 0){printf("D\n");}
+    else{printf("N\n");}
 
     return 0;
 }
