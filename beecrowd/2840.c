103eec72982e23ace9f7ee473141be3da3341bf3
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-05 13:34:57 +0000

    Sun, Mar 5, 2023, 7:34 PM +06:00

--- beecrowd/2840.c
+++ beecrowd/2840.c
@@ -5,11 +5,14 @@
 
 int main(void)
 {
-    int R, L;
+    int R, L, balloon_num;
     double volume;
     scanf("%lld %lld", &R, &L);
 
     volume = ((float)4/3) * PI * pow(R , 3);
+    balloon_num = floor(L / volume);
+
+    printf("%d", balloon_num);
 
     return 0;
 }
