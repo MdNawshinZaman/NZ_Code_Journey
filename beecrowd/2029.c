305cc2ce742a9fc8cb4babcf88cba8959db37be3
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-07 13:28:17 +0000

    Tue, Mar 7, 2023, 7:28 PM +06:00

--- beecrowd/2029.c
+++ beecrowd/2029.c
@@ -1,16 +1,21 @@
+/*
+1. Volune = pi * pow(r , 2) * h;
+2. area_entrance = pi * pow(r , 2);
+
+*/
 #include <stdio.h>
 #include <math.h>
 #define pi 3.14
 
 int main(void)
 {
-    double v, d, r, h;
+    double v, d, r, h, area_entrance;
     while(scanf("%lf %lf", &v, &d) != EOF)
     {
         r = d / 2.0;
         h = v/(pi * pow(r , 2));
-
-        printf("ALTURA = %.2lf\nAREA = %.2lf", h, r);
+        area_entrance = pi * pow(r , 2);
+        printf("ALTURA = %.2lf\nAREA = %.2lf\n", h, area_entrance);
     }
 
     return 0;
