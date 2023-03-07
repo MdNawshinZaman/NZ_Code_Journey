57e09d988cc0a49de8db4c4add44a792c625c1f2
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-07 17:08:38 +0000

    Tue, Mar 7, 2023, 11:08 PM +06:00

--- beecrowd/1973.c
+++ beecrowd/1973.c
@@ -14,20 +14,21 @@ then,
 
 int main(void)
 {
-    unsigned int n;
-    scanf("%u", &n);
+    #define int unsigned long long int
+    int n;
+    scanf("%llu", &n);
 
-    unsigned int sheep[n], ref[n];
+    int sheep[n], ref[n];
     //taking sheep value and an identical copy
-    for (unsigned int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        scanf("%u", &sheep[i]);
+        scanf("%llu", &sheep[i]);
         ref[i] = sheep[i];
     }
 
-    unsigned int attacked = 0, non_stolen = 0;
+    int attacked = 0, non_stolen = 0;
 
-    for (unsigned int j = 0;;)
+    for (int j = 0;;)
     {
         //odd condision
         if (sheep[j] % 2 != 0)
@@ -51,7 +52,7 @@ int main(void)
     }
 
     // checking the number of attacked stars
-    for (unsigned int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         if (sheep[i] != ref[i])
         {
@@ -60,12 +61,12 @@ int main(void)
     }
 
     // checking the number of remaining sheeps
-    for (unsigned int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         non_stolen += sheep[i];
     }
 
-    printf("%u %u\n", attacked, non_stolen);
+    printf("%llu %llu\n", attacked, non_stolen);
 
     return 0;
 }
