7132194d29962671da9ee7de82e5a1c50819e652
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-07 11:58:52 +0000

    Tue, Mar 7, 2023, 5:58 PM +06:00

--- beecrowd/2518.c
+++ beecrowd/2518.c
@@ -1,12 +1,18 @@
 #include<stdio.h>
+#include<math.h>
 
 int main(void)
 {
     int n, h, c, l;
 
+    double x, area;
+
     while(scanf("%d %d %d %d", &n, &h, &c, &l) != EOF)
     {
-        
+        x = sqrt(pow(c , 2) + pow(h , 2));
+        area = x * l;
+
+        printf("%.4lf\n", area);
     }
 
 
