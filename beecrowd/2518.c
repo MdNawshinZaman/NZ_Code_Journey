121a30e99a7faaa90eb2f020ed70e27d2e7b069f
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-07 12:04:05 +0000

    Tue, Mar 7, 2023, 6:04 PM +06:00

--- beecrowd/2518.c
+++ beecrowd/2518.c
@@ -1,22 +1,18 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
     int n, h, c, l;
-
     double x, area;
 
-    while(scanf("%d %d %d %d", &n, &h, &c, &l) != EOF)
+    while (scanf("%d %d %d %d", &n, &h, &c, &l) != EOF)
     {
-        x = sqrt(pow(c , 2) + pow(h , 2));
-        area = n * ((x/100.0) * (l/100.0));//in square meter
-
+        x = sqrt(pow(c, 2) + pow(h, 2));
+        area = n * ((x / 100.0) * (l / 100.0)); // area of the whole ramp in square meters
 
         printf("%.4lf\n", area);
     }
-
-
 
     return 0;
 }
