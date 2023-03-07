f11af67a78162c9f45cf10a52ce0ee890afb7ce4
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-07 13:20:22 +0000

    Tue, Mar 7, 2023, 7:20 PM +06:00

--- beecrowd/2029.c
+++ beecrowd/2029.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#define pi 3.14
 
 int main(void)
 {
-    double v, d;
+    double v, d, r, h;
+    scanf("%lf %lf", &v, &d);
 
-    
+    r = d / 2.0;
+    h = v/(pi)
+
 
     return 0;
 }
