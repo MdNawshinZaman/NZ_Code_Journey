54175dac2b789019f37b68cfc672bfe3dcf67545
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-08 04:11:03 +0000

    Wed, Mar 8, 2023, 10:11 AM +06:00

--- beecrowd/1318.c
+++ beecrowd/1318.c
@@ -9,15 +9,27 @@ int main(void)
 
         if((n == 0) && (m == 0)){break;}
 
-        int people[m];
+        int people[m], sorted[m];
         for(int i = 0; i < m; i++)
         {
             scanf("%d", &people[i]);
         }
 
+        sorted[0] = people[0];
         for(int i = 0; i < m; i++)
         {
-            for(int j = 0; j <  )
+            for(int j = i+1; j < m; j++)
+            {
+                if(sorted[i] > people[j])
+                {
+                    sorted[i] = people[j];
+                }
+            }
+        }
+
+        for(int i = 0; i < m, i++)
+        {
+            printf("%d \n", sorted[i]);
         }
 
     }
