4af9a975582d72949ab2394bd1006da5261a8486
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-09 05:41:43 +0000

    Thu, Mar 9, 2023, 11:41 AM +06:00

--- beecrowd/1318.c
+++ beecrowd/1318.c
@@ -38,23 +38,18 @@ int main(void)
         printf("\n");
 
         //check how many unique digits
-        int check_u = people[0], unique_n = 1;
-        for(int i = 1; i < m; i++)
+        int check_u, unique_n = 1, unique[m];
+        for(int i = 0; i < m; i++)
         {
-            if(check_u < people[i])
+            check_u = people[i];
+            unique[i] = check_u;
+            if(check_u < people[i+1])
             {
                 unique_n++;
                 check_u = people[i];
             }
         }
         //printf("%d\n", unique_n);
-
-        //take all unique numbers in a array
-        int unique[unique_n];
-        for(int i = 0; i < unique_n; i++)
-        {
-            unique[i] = 
-        }
 
         /*int test, num =  0, count = 0;
         for(int i = 0; i < m; i++)
