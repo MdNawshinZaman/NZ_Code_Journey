b797cc4b0f0d4293deae25547a7fc3d164d23711
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-09 06:02:58 +0000

    Thu, Mar 9, 2023, 12:02 PM +06:00

--- beecrowd/1318.c
+++ beecrowd/1318.c
@@ -54,17 +54,18 @@ int main(void)
         }
         //printf("%d\n", unique_n);
 
-         // checking the unique values
+        /*// checking the unique values
         for(int i = 0; i < unique_n; i++)
         {
             printf("%d ", unique[i]);
         }
         printf("\n");
-        
+        */
 
-       int count_i = 0, count_f = 0, z = 0;
-       for(int i = 0; i < m; i++)
-       {
+
+        int count_i = 0, count_f = 0, z = 0;
+        for(int i = 0; i < m; i++)
+        {
             if(people[i] == unique[z])
             {
                 count_i++;
