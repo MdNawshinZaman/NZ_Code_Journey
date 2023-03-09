9b62136af4be49e090670db2357e33399a7a90d4
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-09 05:16:36 +0000

    Thu, Mar 9, 2023, 11:16 AM +06:00

--- beecrowd/1318.c
+++ beecrowd/1318.c
@@ -9,69 +9,12 @@ int main(void)
 
         if((n == 0) && (m == 0)){break;}
 
-        int people[m], temp;
+        int people[m];
         for(int i = 0; i < m; i++)
         {
             scanf("%d", &people[i]);
         }
 
-        for(int i = 0; i < m; i++)
-        {
-            for(int j = i+1; j < m; j++)
-            {
-                if(people[i] > people[j])
-                {
-                    temp = people[i];
-                    people[i] = people[j];
-                    people[j] = temp;
-                }
-            }
-        }
-
-        for(int i = 0; i < m; i++)
-        {
-            printf("%d ", people[i]);
-        }
-        printf("\n");
-
-        int unique_p[m];
-        unique_p[0] = people[0];
-
-        int x = 1;
-        for(int i = 0; i < m-1; i++)
-        {
-            if(people[i] < people[i+1])
-            {
-                unique_p[x] = people[i];
-                x++;
-            }
-        }
-
-        for(int i = 0; i < x; i++)
-        {
-            printf("%d ", unique_p[i]);
-        }
-
-        /*int count = 0;
-        for(int i = 0; i < m-1; i++)
-        {
-            if(people[i] < people[i+1])
-            {
-                count++;
-            }
-        }*/
-
-        /*int nonCopy = 1, num = people[0];
-        for(int i = 1; i < m; i++)
-        {
-            if(num < people[i])
-            {
-                nonCopy++;
-                num = people[i];
-            }
-        }*/
-
-        //printf("%d\n", );
 
     }
 
