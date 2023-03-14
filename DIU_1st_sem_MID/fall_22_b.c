a45c6384e694f5c44438b4f6c421d4ab1256f17c
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-14 03:36:45 +0000

    Tue, Mar 14, 2023, 9:36 AM +06:00

--- DIU_1st_sem_MID/fall_22_b.c
+++ DIU_1st_sem_MID/fall_22_b.c
@@ -5,7 +5,7 @@ int main()
     int a = 5, b = 7, c = 4, d = 8, e = 9;
 
     //Equation 1
-    //e -= a + (++c / d) * (e & 8) - (d | b);
+    e -= a + (++c / d) * (e & 8) - (d | b);
 
     //Equation 2
     a *= a = b? (c * d) && e : (c * d) % b ; // ? true : false
