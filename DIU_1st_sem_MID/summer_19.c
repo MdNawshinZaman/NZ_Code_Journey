486b82b175c013794e47e86b2d873e209c531a47
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-14 02:50:38 +0000

    Tue, Mar 14, 2023, 8:50 AM +06:00

--- DIU_1st_sem_MID/summer_19.c
+++ DIU_1st_sem_MID/summer_19.c
@@ -2,16 +2,7 @@
 
 int main()
 {
-    int a = 5, b = 7, c = 4, d = 8, e = 9;
-
-    //Equation 1
-    e -= a + (++c / d) * (e & 8) - (d | b);
-
-    //Equation 2
-    a *= a = b? (c * d) && e : (c * d) % b;
-
-
-    printf(" a = %d\n b = %d\n c = %d\n d = %d\n e = %d\n", a, b, c, d, e);
+    
 
     return 0;
 }
