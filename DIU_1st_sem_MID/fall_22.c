0795757e81abadbaf4317d6f64c097c885421eb4
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-14 02:34:17 +0000

    Tue, Mar 14, 2023, 8:34 AM +06:00

--- DIU_1st_sem_MID/fall_22.c
+++ DIU_1st_sem_MID/fall_22.c
@@ -2,11 +2,15 @@
 
 int main(void)
 {
-    int a = 5, b = 7, c = 4, d = 8; e = 9;
+    int a = 5, b = 7, c = 4, d = 8, e = 9;
 
 
     //Equation 1
-    a += a * b / c
+    a += a * b / c % d++ + a | b;
 
+    //Equation 2
+    b *= (a/b) >= c + d + e++ -e;
+
+    printf(" a = %d\n b = %d\n c = %d\n d = %d\n e = %d\n", a, b, c, d, e);
     return 0;
 }
