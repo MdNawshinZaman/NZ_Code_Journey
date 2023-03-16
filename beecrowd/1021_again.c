59e2d935daa18d935d3fa2094afc993d4d51d327
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-16 15:14:30 +0000

    Thu, Mar 16, 2023, 9:14 PM +06:00

--- beecrowd/1021_again.c
+++ beecrowd/1021_again.c
@@ -5,7 +5,14 @@ int main(void)
     float n;
     scanf("%f", &n);
 
-    
+    float notes[] = {100, 50, 20, 10, 5, 2}, coins[] = {1, 0.50, 0.10, 0.05, 0.01};
+    float notas[6], moedas[6];
+
+    for(int i = 0; i < 6; i++)
+    {
+        notas[i] = n / notes[]
+    }
+
 
     return 0;
 }
