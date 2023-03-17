8e13fd2897577fcc761c629a06d4b1affccb648f
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-17 01:52:58 +0000

    Fri, Mar 17, 2023, 7:52 AM +06:00

--- beecrowd/2456_again.c
+++ beecrowd/2456_again.c
@@ -8,12 +8,12 @@ int main(void)
         scanf("%d", &cards[i]);
     }
 
-    int comp, c = 1, d = 1, n = 1;
+    int comp, c, d, n;
     comp = card[0];
 
     if(comp < card[1])
     {
-        c = 1
+        c = 1;
         d = 0;
         n = 0;
         for(int i = 1; i < 5; i++)
@@ -31,6 +31,7 @@ int main(void)
     else if(comp > card[1])
     {
         c = 0;
+        d = 1;
         n = 0;
         for(int i = 1; i < 5; i++)
         {
