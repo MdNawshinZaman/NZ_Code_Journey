5d37e23d74c868b25feb3d616502026936719343
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-18 03:00:18 +0000

    Sat, Mar 18, 2023, 9:00 AM +06:00

--- beecrowd/1071_again.c
+++ beecrowd/1071_again.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-    int x, y, temp;
+    int x, y, temp, sum = 0;
     scanf("%d %d", &x, &y);
 
     if(x > y)
@@ -12,7 +12,15 @@ int main(void)
         y = temp;
     }
 
-    for(int i = x; i <)
+    for(int i = x+1; i < y; i++)
+    {
+        if(i % 2 != 0)
+        {
+            sum += i;
+        }
+    }
+
+    printf("%d\n", sum);
 
     return 0;
 }
