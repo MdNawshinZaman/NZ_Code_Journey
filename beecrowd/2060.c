78a37fab77ab6d37d8d44b295ebd39c17f3fa8a6
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-18 03:23:47 +0000

    Sat, Mar 18, 2023, 9:23 AM +06:00

--- beecrowd/2060.c
+++ beecrowd/2060.c
@@ -2,7 +2,28 @@
 
 int main(void)
 {
-    
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int count, z = 0;
+    for(int i = 2; i <= 5; i++)
+    {
+        count = 0;
+        for(int j = 0; j < n; j++)
+        {
+            if(arr[j] % i == 0)
+            {
+                count++;
+            }
+        }
+        printf("%d Multiplo(s) de %d\n", count, arr[z]);
+        z++;
+    }
 
     return 0;
 }
