1407aa86256f87366e2c36a288204ce3d019e630
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-18 06:04:54 +0000

    Sat, Mar 18, 2023, 12:04 PM +06:00

--- beecrowd/1089.c
+++ beecrowd/1089.c
@@ -5,7 +5,15 @@ int main(void)
     int n;
     while(scanf("%d", &n) != 0)
     {
+        int peaks, in[n];
+        for(int i = 0; i < n; i++)
+        {
+            scanf("%d", &in[i]);
+        }
+        //I have value in 0 to n-1 index.
+
         
+
     }
 
     return 0;
