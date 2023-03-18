9af0b6aa352a120a99f207768e755c61368f8c49
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-18 02:56:17 +0000

    Sat, Mar 18, 2023, 8:56 AM +06:00

---
+++ beecrowd/1071_again.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int x, y, temp;
+    scanf("%d %d", &x, &y);
+
+    if(x > y)
+    {
+        temp = x;
+        x = y;
+        y = temp;
+    }
+
+    for
+
+    return 0;
+}
