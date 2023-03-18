f38f4d3caf0f61e58002182ae585f47e0b3e4101
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-18 03:06:08 +0000

    Sat, Mar 18, 2023, 9:06 AM +06:00

--- beecrowd/1133.c
+++ beecrowd/1133.c
@@ -13,8 +13,13 @@ int main(void)
     }
     //now x is less than y
 
-    
-
+    for(int i = x + 1; i < y; i++)
+    {
+        if((i % 5 == 2) || (i % 5 == 3))
+        {
+            printf("%d\n", i);
+        }
+    }
 
     return 0;
 }
