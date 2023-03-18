e869c93a5bd4e15b45e731b42b7450d81287fd3e
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-18 06:12:38 +0000

    Sat, Mar 18, 2023, 12:12 PM +06:00

--- beecrowd/1089.c
+++ beecrowd/1089.c
@@ -13,7 +13,21 @@ int main(void)
         //I have value in 0 to n-1 index.
 
         int copy[n+2];
-        copy[0] = in[]
+        copy[0] = in[n-1];
+        copy[n+1] = in[0];
+
+        for(int i = 1, j = 0; i <= n; i++, j++)
+        {
+            copy[i] = in[j];
+        }
+
+        for(int i = 0; i < n+2; i++)
+        {
+            printf("%d ", copy[i]);
+        }
+        printf("\n");
+
+
 
     }
 
