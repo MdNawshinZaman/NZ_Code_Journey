dc48a3c88bfa40013c05a7518a32c75bd14ac7c1
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-18 13:50:41 +0000

    Sat, Mar 18, 2023, 7:50 PM +06:00

--- beecrowd/1118.c
+++ beecrowd/1118.c
@@ -2,7 +2,24 @@
 
 int main(void)
 {
-    
+    double a, b, media;
+
+    while(1)
+    {
+        do
+        {
+            scanf("%d", &a);
+            i
+        }while((a < 0) || (a > 10));
+
+        do
+        {
+            scanf("%d", &b);
+        }while((b < 0) || (b > 10));
+
+        media = (a + b) / 2.0;
+        printf("%d")
+    }
 
     return 0;
 }
