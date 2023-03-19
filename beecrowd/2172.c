9cb9a06378315268a74b5c00d1d275a5e31f22f9
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-19 05:38:35 +0000

    Sun, Mar 19, 2023, 11:38 AM +06:00

--- beecrowd/2172.c
+++ beecrowd/2172.c
@@ -2,7 +2,12 @@
 
 int main(void)
 {
-    
+    #define int unsigned long long
+    int x, m, e;
+    scanf("%llu %llu", &x, &m);
+    if((x == 0) && (m == 0)){break;}
+
+    printf("%llu", (x * m));
 
     return 0;
 }
