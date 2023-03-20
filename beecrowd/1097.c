fb7b146f85a4372b5ca1d3505816123b8fba5344
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-20 12:02:24 +0000

    Mon, Mar 20, 2023, 6:02 PM +06:00

--- beecrowd/1097.c
+++ beecrowd/1097.c
@@ -1,3 +1,18 @@
 #include<stdio.h>
 
-int maniu 
+int mani(void)
+{
+    for(int i = 1; i <= 9; i++)
+    {
+        int k = 7;
+        for(int j = k; j >= k-2; j--)
+        {
+            printf("I=%d J=%d\n", i, k);
+        }
+
+        k += 2;
+
+    }
+
+    return 0;
+}
