8ea5378fb6678768ea9d40ed146219eab027a60d
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-20 13:04:12 +0000

    Mon, Mar 20, 2023, 7:04 PM +06:00

--- beecrowd/1134.c
+++ beecrowd/1134.c
@@ -2,7 +2,26 @@
 
 int main(void)
 {
-    
+    int n, a = 0, g = 0, d = 0;
+    do{
+        scanf("%u", &n);
+
+        if(n == 4){break;}
+        switch(n)
+        {
+            case 1:
+                a++;
+                break;
+            case 2:
+                g++;
+                break;
+            case 3:
+                d++;
+                break;
+        }
+    }while((n < 1) || (n > 4));
+
+    printf("MUITO OBRIGADO\nAlcool: %u\nGasolina: %u\nDiesel: %u\n", a, g, d);
 
     return 0;
 }
