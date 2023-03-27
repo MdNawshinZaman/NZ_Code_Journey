d349a7aee16657bdc73fdaaa38fb7c50b87f226d
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-27 04:03:30 +0000

    Mon, Mar 27, 2023, 10:03 AM +06:00

---
+++ DIU_1st_sem_MID/b-3-2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    int num = 10;
+    for(int i = 1; i < num; i++)
+        for(int j = 1; j < num; j++)
+            printf("j = %d\ti = %d\n", i = j * 10, j);
+
+    return 0;
+}
