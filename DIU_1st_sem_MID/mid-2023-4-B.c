4ef6b7c8763df7908a8ab3386ca67a72eec323f5
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-30 09:44:03 +0000

    Thu, Mar 30, 2023, 3:44 PM +06:00

--- DIU_1st_sem_MID/mid-2023-4-B.c
+++ DIU_1st_sem_MID/mid-2023-4-B.c
@@ -2,7 +2,27 @@
 
 int main(void)
 {
-    char food[4][10] = {"Soup", "Noodles", ""}
+    char food[4][10] = {"Soup", "Noodles", "Pasta", "Pizza"};
+    int weight[4], c_weight[4], temp;
+
+    for(int i = 0; i < 4; i++)
+    {
+        scanf("%d", &weight);
+        c_weight[i] = weight[i];
+    }
+
+    for(int i = 0; i < 4; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            if(weight[j] < weight[j+1]){temp = weight[j]; weight[j] = weight[j+1]; weight[j+1] = temp;}
+        }
+    }
+
+    for(int i = 0; i < 4; i++)
+    {
+        printf("%d ",weight[i])
+    }
 
     return 0;
 }
