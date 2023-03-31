cabf05cc224cf731826d76a50071572fc388be2c
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-31 00:37:27 +0000

    Fri, Mar 31, 2023, 6:37 AM +06:00

--- DIU_1st_sem_MID/mid-2023-4-C.c
+++ DIU_1st_sem_MID/mid-2023-4-C.c
@@ -2,7 +2,19 @@
 
 int main(void)
 {
-    
+    int n;
+    scanf("%d", n);
+
+    for(int i = 0; i < n; i++)
+    {
+        int m;
+        scanf("%d", &m);
+
+        for(int j = 0; j < m; j++)
+        {
+            
+        }
+    }
 
     return 0;
 }
