16c6fe1eacb5be742375a505f2ee293864e9a793
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-03-31 00:45:19 +0000

    Fri, Mar 31, 2023, 6:45 AM +06:00

--- DIU_1st_sem_MID/mid-2023-4-C.c
+++ DIU_1st_sem_MID/mid-2023-4-C.c
@@ -7,7 +7,7 @@ int main(void)
 
     for(int i = 0; i < n; i++)
     {
-        int m, num, big_2_digit_prime = 0, is_prime = 0;
+        int m, num, big_2_digit_prime = 0, is_prime = 1;
         scanf("%d", &m);
 
         for(int j = 0; j < m; j++)
