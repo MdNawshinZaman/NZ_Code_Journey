8c049f79378da034e2d7d9410d027a0cd3cf32a0
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-04-01 04:34:45 +0000

    Sat, Apr 1, 2023, 10:34 AM +06:00

--- cs50-2023/2-compare.c
+++ cs50-2023/2-compare.c
@@ -10,6 +10,14 @@ int main(void)
     {
         printf("x is less than y\n");
     }
+    else if (x > y)
+    {
+        printf("x is greater than y\n");
+    }
+    else
+    {
+        printf("x is equal to y\n");
+    }
 
     return 0;
 }
