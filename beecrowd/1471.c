b9aaff4c958fe46953386934a8d52d6a48f985ff
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-04-14 01:39:58 +0000

    Fri, Apr 14, 2023, 7:39 AM +06:00

--- beecrowd/1471.c
+++ beecrowd/1471.c
@@ -3,9 +3,22 @@
 int main(void)
 {
     int n, r;
-    while(scanf("%d %d", &n, &r) != EOF)
+    while (scanf("%d %d", &n, &r) != EOF)
     {
-        
+        int volunteers[r];
+
+        for (int i = 0; i < r; i++)
+        {
+            scanf("%d", &volunteers[i]);
+        }
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 0; j < r; J++)
+            {
+                
+            }
+        }
     }
 
     return 0;
