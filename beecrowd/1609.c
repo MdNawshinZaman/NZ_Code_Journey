b4bb1a2b1534b00bad967cebd9e254b995a6ae50
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-04-14 02:13:48 +0000

    Fri, Apr 14, 2023, 8:13 AM +06:00

--- beecrowd/1609.c
+++ beecrowd/1609.c
@@ -5,7 +5,7 @@ int main(void)
     int t, n;
     sacnf("%d", &t);
 
-    for (int i = 0; i < t; i++)
+    for (int z = 0; z < t; z++)
     {
         scanf("%d", &n);
         int sheep[n];
@@ -14,7 +14,32 @@ int main(void)
         {
             sacnf("%d", &sheep[i]);
         }
-        
+
+        int temp;
+        for(int i = 0; i < n; i++)
+        {
+            for(int i = 0, m = n - 1; i < m; i++)
+            {
+                if (sheep[i] > sheep[i + 1])
+                {
+                    temp = sheep[i];
+                    sheep[i] = sheep[i + 1];
+                    sheep[i + 1] = temp;
+                }
+            }
+        }
+
+        int unique = sheep[0], count = 1;
+        for(int i = 1; i < n; i++)
+        {
+            if (unique != sheep[i])
+            {
+                count++;
+                unique = sheep[i];
+            }
+        }
+
+
     }
 
     return 0;
