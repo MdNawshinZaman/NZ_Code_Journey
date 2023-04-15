4ab4f7d886e03cf03410c7d8309bdc4c15252c07
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-04-15 06:17:19 +0000

    Sat, Apr 15, 2023, 12:17 PM +06:00

--- beecrowd/1533.c
+++ beecrowd/1533.c
@@ -8,7 +8,15 @@ int main(void)
         scanf("%d", &n);
         if(n == 0){break;}
 
-        
+        int arr[n], arr_comp[n];
+
+        for(int i = 0; i < n; i++)
+        {
+            scanf("%d",&arr[i]);
+            arr_comp[i] = arr[i];
+        }
+
+        for(int i = 0; i )
     }
 
     return 0;
