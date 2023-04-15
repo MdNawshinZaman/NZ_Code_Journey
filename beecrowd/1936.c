82933981207b060815970169f7724c786fca7b35
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-04-15 05:19:55 +0000

    Sat, Apr 15, 2023, 11:19 AM +06:00

--- beecrowd/1936.c
+++ beecrowd/1936.c
@@ -2,15 +2,23 @@
 
 int main(void)
 {
-    int n;
+    int n, count = 0;
     scanf("%d", &n);
 
     int factorials[8] = {1, 2, 6, 24, 120, 720, 5040, 40320};
 
     while(n > 0)
     {
-        
+        for(int i = 7; i >= 0; i--)
+        {
+            if(n >= factorials[i])
+            {
+                count++;
+                n -= factorials[i];
+                break;
+            }
+        }
     }
-
+    printf("%d\n", count);
     return 0;
 }
