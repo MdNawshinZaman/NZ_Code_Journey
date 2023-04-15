844855daad0fd52b0d9f9ffdc69ad6be53684f1c
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-04-15 09:35:58 +0000

    Sat, Apr 15, 2023, 3:35 PM +06:00

--- beecrowd/1521.c
+++ beecrowd/1521.c
@@ -7,7 +7,28 @@ int main(void)
         int n;
         scanf("%d",&n);
 
-        
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", &arr[i]);
+        }
+
+        int k, guilty;
+        scanf("%d", &k);
+
+        guilty = k;
+
+        for(int i = 0; i < n; i++)
+        {
+            if(guilty == arr[i])
+            {
+                printf("%d\n", guilty);
+            }
+            else
+            {
+                guilty = arr[i];
+            }
+        }
     }
 
 
