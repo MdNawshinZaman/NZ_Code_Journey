30cbeea86c46e94b494128611e2a86a9d1b34244
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-04-15 06:05:21 +0000

    Sat, Apr 15, 2023, 12:05 PM +06:00

--- test/learn.c
+++ test/learn.c
@@ -3,9 +3,9 @@
 int main(void)
 {
     float a = 2.234888;
-    printf("%.3f\n", a);
+    printf("%.6f\n", a);
 
-    printf("%.3f\n", (int)(a*1000)/1000.0);
+    printf("%.6f\n", (int)(a*1000)/1000.0);
 
     return 0;
 }
