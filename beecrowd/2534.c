41309ccdb226b0e2b8030ff18f9fdb807ec358b3
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-04-15 09:05:58 +0000

    Sat, Apr 15, 2023, 3:05 PM +06:00

--- beecrowd/2534.c
+++ beecrowd/2534.c
@@ -5,7 +5,31 @@ int main(void)
     int n, q;
     whiel(scanf("%d %d", &n, &q) != EOF)
     {
-        
+        int grade[n], position[q];
+
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", &grade[i]);
+        }
+
+        int temp;
+        for (int i = 0; i < n;; i++)
+        {
+            for (int j = 0, m = n - 1 ; j < m; j++)
+            {
+                if (grade[j] < grade[j + 1])
+                {
+                    temp = grade[j];
+                    grade[j] = grade[j + 1];
+                    grade[j + 1] = temp;
+                }
+            }
+        }
+
+        for(int i = 0; i < q; i++)
+        {
+            printf("%d",grade[])
+        }
     }
 
     return 0;
