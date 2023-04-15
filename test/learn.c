32695e50db71df5faaea17e2f67838cb5674bb16
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-04-15 06:06:26 +0000

    Sat, Apr 15, 2023, 12:06 PM +06:00

--- test/learn.c
+++ test/learn.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
-
+#include <stdio.h>
+#include <math.h>
 int main(void)
 {
     float a = 2.234888;
     printf("%.6f\n", a);
 
-    printf("%.6f\n", (int)(a*1000)/1000.0);
+    printf("%.6f\n", ceil(a*1000)/1000.0);
 
     return 0;
 }
