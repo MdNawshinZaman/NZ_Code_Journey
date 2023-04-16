10e97efa2be3108b8e0285d653a4aa160323d2b9
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-04-16 09:05:08 +0000

    Sun, Apr 16, 2023, 3:05 PM +06:00

--- beecrowd/1181.c
+++ beecrowd/1181.c
@@ -2,7 +2,14 @@
 
 int main(void)
 {
-    
+    float M[12][12];
+    for (int i = 0; i < 12; i++)
+    {
+        for (int j = 0; j < 12; j++)
+        {
+            scanf("%d", &M[i][j]);
+        }
+    }
 
     return 0;
 }
