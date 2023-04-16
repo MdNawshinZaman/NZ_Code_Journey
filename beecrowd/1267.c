f27ad5f4293d74c9c2defcd6c883bc8e8e14cc37
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-04-16 10:39:40 +0000

    Sun, Apr 16, 2023, 4:39 PM +06:00

--- beecrowd/1267.c
+++ beecrowd/1267.c
@@ -4,13 +4,23 @@ int main(void)
 {
     int n, d;
     scanf("%d %d",&n, &d);
-    if (m == 0 && n == 0){break;}
+    int arr[d][n];
+    if (m == 0 && n == 0)1{break;}
 
     for (int i = 0, i < d; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            scanf()
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    for (int i = 0; i < d; i++)
+    {
+        int sum = 0;
+        for(int j = 0; j < n; j++)
+        {
+            sum += arr[][]
         }
     }
 
