be1fc7075a5d81e28a0b4f11e9b06b45bd7cb111
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-04-16 09:48:15 +0000

    Sun, Apr 16, 2023, 3:48 PM +06:00

---
+++ beecrowd/1190.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float M[12][12], sum = 0;
+    char c;
+    scanf(" %c",&c);
+
+    for (int i = 0; i < 12; i++)
+    {
+        for (int j = 0; j < 12; j++)
+        {
+            scanf("%f", &M[i][j]);
+        }
+    }
+
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 12 - i; j <= 11 ; j++)
+        {
+            sum += M[i][j];
+        }
+
+    }
+
+    for (int i = 6; i <= 10; i++)
+    {
+        for (int j = 11-i+1 ; j <= 12 ; j++)
+        {
+            sum += M[i][j];
+        }
+
+    }
+
+
+
+    return 0;
+}
