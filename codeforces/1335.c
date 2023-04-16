706f3494d2aae6c6ddfb6ae83d630919576a6926
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-04-16 04:56:03 +0000

    Sun, Apr 16, 2023, 10:56 AM +06:00

--- codeforces/1335.c
+++ codeforces/1335.c
@@ -5,7 +5,15 @@ int main(void)
     int t;
     scanf("%d", &t);
 
-    
+    for (int i = 0; i < t; i++)
+    {
+        int num;
+        int result;
+        scanf("%d", &num);
+
+        result = ceil(num/2.0) - 1;
+
+    }
 
     return 0;
 }
