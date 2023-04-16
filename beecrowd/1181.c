a74ffbd4877f5f269192e4639a1cf4b3bfff960c
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-04-16 09:10:29 +0000

    Sun, Apr 16, 2023, 3:10 PM +06:00

--- beecrowd/1181.c
+++ beecrowd/1181.c
@@ -2,13 +2,33 @@
 
 int main(void)
 {
-    float M[12][12];
+    float M[12][12], sum = 0;
+    int l;
+    char t;
+    scanf("%d %c", &l, &t);
+
     for (int i = 0; i < 12; i++)
     {
         for (int j = 0; j < 12; j++)
         {
-            scanf("%d", &M[i][j]);
+            scanf("%f", &M[i][j]);
         }
+    }
+
+    for (int i = 0; i < 12; i++)
+    {
+        sum += M[l][i];
+    }
+
+    switch(t)
+    {
+        case 'S':
+        printf("%.1f\n", sum);
+        break;
+
+        case 'M':
+        printf("%.1f\n", sum / 12);
+        break;
     }
 
     return 0;
