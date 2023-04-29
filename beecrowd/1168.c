87a37e4960e86a04ada00b2d5a68e081fc5c314d
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-04-29 02:54:05 +0000

    Sat, Apr 29, 2023, 8:54 AM +06:00

--- beecrowd/1168.c
+++ beecrowd/1168.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-    
+    int n;
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+    {
+        char number[101];
+        scanf("%s", number);
+
+
+    }
 
     return 0;
 }
