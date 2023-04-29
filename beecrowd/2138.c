db0ca476787b606425ef8e4f45916b5c29cb3013
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-04-29 03:19:15 +0000

    Sat, Apr 29, 2023, 9:19 AM +06:00

--- beecrowd/2138.c
+++ beecrowd/2138.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
     char number[1002];
     while(scanf("%s", number) != EOF)
     {
-        
+        int len = strlen(number)
     }
 
     return 0;
