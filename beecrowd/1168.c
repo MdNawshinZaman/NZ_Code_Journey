8edaf86bbd82e6444033ee760c5a3671146a857f
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-04-29 03:02:44 +0000

    Sat, Apr 29, 2023, 9:02 AM +06:00

--- beecrowd/1168.c
+++ beecrowd/1168.c
@@ -8,9 +8,49 @@ int main(void)
 
     for(int i = 0; i < n; i++)
     {
-        char number[101];
+        char number[101] = "0";
         scanf("%s", number);
 
+        int len = strlen(number), led = 0;
+
+        for(int j = 0; j < len; j++)
+        {
+            switch(number[j])
+            {
+                case '0':
+                    led += 6;
+                    break;
+                case '1':
+                    led += 2;
+                    break;
+                case '2':
+                    led += 5;
+                    break;
+                case '3':
+                    led += 5;
+                    break;
+                case '4':
+                    led += 4;
+                    break;
+                case '5':
+                    led += 5;
+                    break;
+                case '6':
+                    led += 6;
+                    break;
+                case '7':
+                    led += 3;
+                    break;
+                case '8':
+                    led += 7;
+                    break;
+                case '9':
+                    led += 6;
+                    break;
+            }
+        }
+
+        printf("%d leds\n", led);
 
     }
 
