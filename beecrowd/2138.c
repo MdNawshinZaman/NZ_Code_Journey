39e3f5ba9595a8a76d6304badefb6677d3f70349
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-04-29 03:34:30 +0000

    Sat, Apr 29, 2023, 9:34 AM +06:00

--- beecrowd/2138.c
+++ beecrowd/2138.c
@@ -46,11 +46,11 @@ int main(void)
             }
         }
 
-        for(int i = 0; i < 10; i++)
+        /*for(int i = 0; i < 10; i++)
         {
             printf("%d ", digit_arr[i]);
         }
-        printf("\n");
+        printf("\n");*/
     }
 
     return 0;
