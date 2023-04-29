995d4f0664d8cc7266e7698573a18d2453b531a6
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-04-29 02:34:07 +0000

    Sat, Apr 29, 2023, 8:34 AM +06:00

--- beecrowd/2167.c
+++ beecrowd/2167.c
@@ -5,7 +5,18 @@ int main(void)
     int n;
     scanf("%d", &n);
 
-    
+    int rpm0, rpm_i;
+    scanf("%d", &rpm0);
 
+    int fall = 0;
+    for(int i = 1; i < n; i++)
+    {
+        scanf("%d", &rpm_i);
+
+        if(rpm_i < rpm0)
+        {
+            fall = i + 1;
+        }
+    }
     return 0;
 }
