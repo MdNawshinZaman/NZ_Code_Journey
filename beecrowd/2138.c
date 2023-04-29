706b4e8682389d0cb8847ceba9a32453108e1577
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-04-29 03:24:16 +0000

    Sat, Apr 29, 2023, 9:24 AM +06:00

--- beecrowd/2138.c
+++ beecrowd/2138.c
@@ -11,7 +11,39 @@ int main(void)
         int digit_arr[10];
         for(int i = 0; i < len, i++)
         {
-            
+            switch(number[i])
+            {
+                case '0':
+                    led += 6;
+                    break;
+                case '1':
+                    led += 2;
+                    break;
+                case '2':
+                    led += 5;
+                    break;
+                case '3':
+                    led += 5;
+                    break;
+                case '4':
+                    led += 4;
+                    break;
+                case '5':
+                    led += 5;
+                    break;
+                case '6':
+                    led += 6;
+                    break;
+                case '7':
+                    led += 3;
+                    break;
+                case '8':
+                    led += 7;
+                    break;
+                case '9':
+                    led += 6;
+                    break;
+            }
         }
     }
 
