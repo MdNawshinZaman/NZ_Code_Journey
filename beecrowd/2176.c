7a36df425f0d0152ab1824a5eb5988c86618299c
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-05-01 11:35:00 +0000

    Mon, May 1, 2023, 5:35 PM +06:00

--- beecrowd/2176.c
+++ beecrowd/2176.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-    
+    char s[101];
+    scanf("%s", s);
+
+    int len = strlen(s), num_of_one = 0;
+
+    for(int i = 0; i < len; i++)
+    {
+        if (s[i] == '1')
+        {
+            num_of_one++;
+        }
+    }
+
+    if()
+
+
 
     return 0;
 }
