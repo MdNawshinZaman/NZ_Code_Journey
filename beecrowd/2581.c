299d53b67169e00a75382dc08ad9bc634e9ecfab
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-05-01 11:45:25 +0000

    Mon, May 1, 2023, 5:45 PM +06:00

---
+++ beecrowd/2581.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    while(scanf("%d", &n) != EOF)
+    {
+        
+        for (int i = 0; i < n; i++)
+        {
+            scan
+        }
+    }
+
+    return 0;
+}
