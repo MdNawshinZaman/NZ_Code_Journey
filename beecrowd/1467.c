610c3bfdc334bb7f1427a8cad105a2bd74c29eb4
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-05-13 17:09:33 +0000

    Sat, May 13, 2023, 11:09 PM +06:00

--- beecrowd/1467.c
+++ beecrowd/1467.c
@@ -3,7 +3,7 @@
 int main(void)
 {
     int a, b, c;
-    while(scanf("%d %d %d", &a, &b, &c) != EOF)
+    while (scanf("%d %d %d", &a, &b, &c) != EOF)
     {
         if ((a + b + c == 0) || (a + b + c == 3))
         {
@@ -11,15 +11,33 @@ int main(void)
         }
         else if (a + b + c == 1)
         {
-            if (a == 1){printf("A\n");}
-            else if (b == 1){printf("B\n");}
-            else (c == 1){printf("C\n");}
+            if (a == 1)
+            {
+                printf("A\n");
+            }
+            else if (b == 1)
+            {
+                printf("B\n");
+            }
+            else
+            {
+                printf("C\n");
+            }
         }
         else if (a + b + c == 2)
         {
-            if (a == 0){printf("A\n");}
-            else if (b == 0){printf("B\n");}
-            else (c == 0){printf("C\n");}
+            if (a == 0)
+            {
+                printf("A\n");
+            }
+            else if (b == 0)
+            {
+                printf("B\n");
+            }
+            else
+            {
+                printf("C\n");
+            }
         }
     }
 
