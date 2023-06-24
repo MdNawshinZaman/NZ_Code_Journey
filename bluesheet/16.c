4a6b6dee4c9dd2823bf36d82a2ee18b082bfa1a8
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-06-24 03:11:27 +0000

    Sat, Jun 24, 2023, 9:11 AM +06:00

--- bluesheet/16.c
+++ bluesheet/16.c
@@ -2,6 +2,21 @@
 
 int main(void)
 {
+    int a;
+    char str[22];
+
+    scanf("%d", &a);
+    fflush(stdin);
+    scanf("%s", str);
+
+    int k = 0;
+    int i = 0;
+    while(str[i] != '\0')
+    {
+        k++;
+        i++;
+    }
+
     
 
     return 0;
