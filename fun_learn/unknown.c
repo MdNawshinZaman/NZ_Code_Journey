b57c36e4bbb9088be221f5d3367f06a649f48609
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-06-27 02:02:03 +0000

    Tue, Jun 27, 2023, 8:02 AM +06:00

---
+++ fun_learn/unknown.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main()
+{
+    double avg, phy, math, funda, pps, ppslab, eng;
+
+    printf("phy: ");
+    scanf("%lf", &phy);
+
+    printf("math: ");
+    scanf("%lf", &math);
+
+    printf("funda: ");
+    scanf("%lf", &funda);
+
+    printf("pps: ");
+    scanf("%lf", &pps);
+
+    printf("ppslab: ");
+    scanf("%lf", &ppslab);
+
+    printf("eng: ");
+    scanf("%lf", &eng);
+
+    printf("avg: ");
+    scanf("%lf", &avg);
+
+    ppslab /= 2;
+
+    avg = (avg * 16.5) / 3.0;
+    int n = 0;
+    if (phy == 1)n++;
+    if (math == 1)n++;
+    if (funda == 1)n++;
+    if (pps == 1)n++;
+    if (ppslab == 1)n++;
+    if (eng == 1)n++;
+
+    double unknown = (avg - (phy + math + funda + pps + ppslab + eng)) + n;
+
+    printf("\n%.2lf\n\n", unknown);
+
+    return 0;
+}
