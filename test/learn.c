cd8bf6886b0a33ae45ecf5f8dee0f9af2cc993d2
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-06-30 16:07:25 +0000

    Fri, Jun 30, 2023, 10:07 PM +06:00

--- test/learn.c
+++ test/learn.c
@@ -1,20 +1,48 @@
 #include <stdio.h>
-#include <string.h>
+
 int main(void)
 {
-    int n;
-    scanf("%d", &n);
-    char str[n+1];
+    long long int a;
+    char str[22];
+
+    scanf("%lld", &a);
     scanf("%s", str);
 
-    printf("%s\n", str);
-
-    char c;
-    int i = 0;
-    while(getchar() != '\0')
+    long long int k = 0;
+    long long int i = 0;
+    while (str[i] != '\0')
     {
-        printf("%c", str[i++]);
+        k++;
+        i++;
     }
+
+    long long int factorial = a;
+
+    long long int z = a % k;
+
+    if (a > k)
+    {
+        if (z == 0)
+        {
+            for (long long int j = a - k; j >= k; j -= k)
+            {
+                factorial *= j;
+            }
+        }
+        else
+        {
+            for (long long int j = a - k; j >= z; j -= k)
+            {
+                factorial *= j;
+            }
+        }
+    }
+    else
+    {
+        factorial = 1;
+    }
+
+    printf("%lld\n", factorial);
 
     return 0;
 }
