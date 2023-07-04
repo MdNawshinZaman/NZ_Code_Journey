186b4094db2adff14be495ee217021609564b3de
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-07-04 14:26:44 +0000

    Tue, Jul 4, 2023, 8:26 PM +06:00

--- cs50-2023/week2/scores.c
+++ cs50-2023/week2/scores.c
@@ -3,10 +3,24 @@
 int main(void)
 {
     int s[3];
-    s[0] = 72;
-    s[1] = 73;
-    s[2] = 33;
+    //s[0] = 72;
+    //s[1] = 73;
+    //s[2] = 33;
 
+    /*
+    printf("Score 1:");
+    scanf("%d", &s[0]);
+    printf("Score 2:");
+    scanf("%d", &s[1]);
+    printf("Score 2:");
+    scanf("%d", &s[2]);
+    */
+
+   for(int i = 0; i < 3; i++)
+   {
+        printf("Enter Score%d: ", i + 1);
+        scanf("%d", &s[i]);
+   }
 
 
     printf("Avgerage: %.4lf\n", (s[0] + s[1] + s[2]) / (float)3);
