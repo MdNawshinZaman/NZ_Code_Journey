7e3af6444823bce6be48d4864b7996ddcf0ca965
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-07-04 14:21:34 +0000

    Tue, Jul 4, 2023, 8:21 PM +06:00

--- cs50-2023/week2/scores.c
+++ cs50-2023/week2/scores.c
@@ -3,13 +3,13 @@
 int main(void)
 {
     int s[3];
-    int s[0] = 72;
-    int s[1] = 73;
-    int s[2] = 33;
+    s[0] = 72;
+    s[1] = 73;
+    s[2] = 33;
 
 
 
-    printf("Avgerage: %.4lf\n", (s1 + s2 + s3) / (float)3);
+    printf("Avgerage: %.4lf\n", (s[0] + s[1] + s[2]) / (float)3);
 
     return 0;
 
