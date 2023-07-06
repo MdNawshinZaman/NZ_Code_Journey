cf42ec864b639cc93f3f39257d723266f4b6562a
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-07-06 16:10:15 +0000

    Thu, Jul 6, 2023, 10:10 PM +06:00

--- bluesheet/17.c
+++ bluesheet/17.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
+#include<math.h>
 
 int main(void)
 {
     long arr[200000] = {0};
 
-    for(int i = 2; i*i 200000; i++)
+    for(int i = 2; i < sqrt(200000); i++)
 
     return 0;
 }
