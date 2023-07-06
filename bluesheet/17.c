fb0fe2baa69bae30a0513b1bb720286e0e3d2e08
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-07-06 16:19:17 +0000

    Thu, Jul 6, 2023, 10:19 PM +06:00

--- bluesheet/17.c
+++ bluesheet/17.c
@@ -1,11 +1,36 @@
 #include<stdio.h>
 #include<math.h>
+#define MAX 200000
 
 int main(void)
 {
-    long arr[200000] = {0};
+    long arr[MAX] = {1};
+    int prime[MAX] = {0};
+    int p = 0;
 
-    for(int i = 2; i < sqrt(200000); i++)
+    for(int i = 2; i <= sqrt(MAX); i++)
+    {
+        if (arr[i] == 1)
+        {
+            prime[p] = i;
+            p++;
+
+            for (int j = i*i; j <= MAX; j+=i)
+            {
+                arr[j] = 0;
+            }
+        }
+    }
+
+    int n;
+    scanf("%d", &n);
+
+    int temp;
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &temp);
+        printf("%d\n", prime[temp - 1]);
+    }
 
     return 0;
 }
