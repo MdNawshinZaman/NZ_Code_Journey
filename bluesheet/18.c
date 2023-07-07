c794075a4de1c4731af7041e67b1b363671427f0
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-07-07 04:27:42 +0000

    Fri, Jul 7, 2023, 10:27 AM +06:00

--- bluesheet/18.c
+++ bluesheet/18.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
+int arr[10000000009] = {0};
 
 int main(void)
 {
-    int arr[10000000009] = {0};
+    int x = 0;
+    for(int i = 1; i < 10000000009; i+=x)
+    {
+        arr[i] = 1;
+        x++;
+    }
 
     return 0;
 }
