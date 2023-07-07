1d5ec2f7768e965907c3c8f6cbe35ed8a6eb0b26
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-07-07 04:31:24 +0000

    Fri, Jul 7, 2023, 10:31 AM +06:00

--- bluesheet/18.c
+++ bluesheet/18.c
@@ -4,11 +4,21 @@ int arr[10000000009] = {0};
 int main(void)
 {
     int x = 0;
-    for(int i = 1; i < 10000000009; i+=x)
+    for(long i = 1; i < 10000000009; i+=x)
     {
         arr[i] = 1;
         x++;
     }
 
+    int n;
+    scanf("%d", &n);
+
+    int temp;
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &temp);
+        printf("%d", arr[temp]);
+    }
+    
     return 0;
 }
