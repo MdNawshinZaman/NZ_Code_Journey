479e5e56e27e87cec47f4aa6ab177fb2a7bf5dab
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-07-14 03:48:41 +0000

    Fri, Jul 14, 2023, 9:48 AM +06:00

---
+++ data_structure_and_algorithms/3_selection_sort.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void selection_sort(int arr[], int n);
+{
+    int i, index_min, temp;
+    
+}
+
+int main(void)
+{
+    int arr[] = {654,654,54,6874,6564,65,8,65,546,514,6874,61,654,8,2,4,87,9,8,5,4,7,84,674,847,65,48,65,65489,9844,1,1,85,6,45,3,4,63,4,65};
+    int n = sizeof(arr) / sizeof(int);
+
+    selection_sort(arr, n);
+
+    printf("The sorted array is: ");
+    for(int i = 0; i < n)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n\n");
+
+    return 0;
+}
