74d063c17a39e2371f2b4e2810e63f739d1e7783
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-07-14 03:04:27 +0000

    Fri, Jul 14, 2023, 9:04 AM +06:00

---
+++ data_structure_and_algorithms/linear_search.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/*int linear_search(int a[], int n, int x)
+{
+
+}*/
+
+int main(void)
+{
+    int a[] = {60, 1, 88, 10, 11, 100};
+
+    printf("%lu", sizeof(a) / sizeof(int));
+
+    return 0;
+}
