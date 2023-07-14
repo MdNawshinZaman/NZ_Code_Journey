849ffa50b31cb74d8cfee0f79c83074431267cad
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-07-14 03:15:03 +0000

    Fri, Jul 14, 2023, 9:15 AM +06:00

--- data_structure_and_algorithms/linear_search.c
+++ data_structure_and_algorithms/linear_search.c
@@ -1,15 +1,31 @@
 #include<stdio.h>
 
-/*int linear_search(int a[], int n, int x)
+int linear_search(int a[], int n, int x)
 {
-
-}*/
+    for(int i = 0; i < n; i++)
+    {
+        if(a[i] == x)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
 
 int main(void)
 {
     int a[] = {60, 1, 88, 10, 11, 100};
+    int n = sizeof(a) / sizeof(int);
 
-    printf("%lu", sizeof(a) / sizeof(int));
+    int x;
+    printf("\nEnter the number you want to find from the array: ");
+    scanf("%d", &x);
+
+    int r = linear_search(a, n, x);
+
+    if (r == -1) printf("Number not found!\n\n");
+    else printf("Your number is at %dth position of the array.\n\n", r+1);
+
 
     return 0;
 }
