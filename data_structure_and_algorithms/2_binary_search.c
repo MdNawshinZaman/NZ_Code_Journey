35a1ed7ce1b37b99637ce8630041d1ab16431003
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-07-14 03:33:30 +0000

    Fri, Jul 14, 2023, 9:33 AM +06:00

--- data_structure_and_algorithms/2_binary_search.c
+++ data_structure_and_algorithms/2_binary_search.c
@@ -4,7 +4,24 @@ int binary_search(int arr[], int n, int x)
 {
     int left = 0, right = n - 1, mid;
 
-    mid = 
+    while(left <= right)
+    {
+        mid = (left + right) / 2;
+
+        if (arr[mid] == x)
+        {
+            return mid;
+        }
+        else if (arr[mid] < x)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            
+        }
+
+    }
 
 }
 
