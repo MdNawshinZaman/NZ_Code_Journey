265ce19a527ff7c1b22f6944753a36f5f272391b
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-07-16 03:18:08 +0000

    Sun, Jul 16, 2023, 9:18 AM +06:00

--- diu_data_structure/16-07-2023/1.c
+++ diu_data_structure/16-07-2023/1.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
 
 struct student
+{
+    int id;
+    float cgpa;
+};
 
 int main(void)
 {
-
+    int x;
+    printf("Number of Students: ");
+    scanf("%d", &x); 
+    struct student n[];
 
     return 0;
 }
