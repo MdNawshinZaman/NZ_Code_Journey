edc302442a2c476d8905479aeb713a26ea2993b7
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-07-16 03:22:16 +0000

    Sun, Jul 16, 2023, 9:22 AM +06:00

--- diu_data_structure/16-07-2023/1.c
+++ diu_data_structure/16-07-2023/1.c
@@ -10,8 +10,23 @@ int main(void)
 {
     int x;
     printf("Number of Students: ");
-    scanf("%d", &x); 
-    struct student n[];
+    scanf("%d", &x);
+    struct student n[x];
+
+    for(int i = 0; i < x; i++)
+    {
+        printf("ID: ");
+        scanf("%d", &n[i].id);
+
+        printf("CGPA: ");
+        scanf("%f", &n[i].cgpa);
+    }
+
+    for(int i = 0; i < x; i++)
+    {
+        printf("ID: %d    CGPA: %.2f\n", n[i].id, n[i].cgpa );
+
+    }
 
     return 0;
 }
