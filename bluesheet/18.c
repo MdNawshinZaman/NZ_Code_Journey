730d70fb4f9a0bdaf271d72a27f3bf08e9d91abb
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-07-18 16:19:23 +0000

    Tue, Jul 18, 2023, 10:19 PM +06:00

--- bluesheet/18.c
+++ bluesheet/18.c
@@ -1,24 +1,9 @@
 #include<stdio.h>
-unsigned long long int arr[4294967296] = {0};
+
 
 int main(void)
 {
-    int x = 0;
-    for(unsigned long long int i = 1; i < 4294967296; i+=x)
-    {
-        arr[i] = 1;
-        x++;
-    }
-
-    int n;
-    scanf("%d", &n);
-
-    unsigned long long int temp;
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%lld", &temp);
-        printf("%lld", arr[temp]);
-    }
+    int avm;
 
     return 0;
 }
