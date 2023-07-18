afff94120b25390db67ffb3aaec4f60badc4b767
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-07-18 16:38:28 +0000

    Tue, Jul 18, 2023, 10:38 PM +06:00

--- bluesheet/18.c
+++ bluesheet/18.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
+#include<math.h>
 //(2 power 31) - 1 = 2147483648 - 1 = 2147483647
 
 int main(void)
 {
-    //printf("%lf\n",pow(2, 31));
-    
+    //printf("%lf\n",pow(2, 32));
+
+    int position_of_ones[65540];
+
+    for(int pos_of_one = 1,  i = 0; i <= 2147483647; i+=)
+
     return 0;
 }
