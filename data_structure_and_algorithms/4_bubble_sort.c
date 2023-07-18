7600628f0050590f80970da85431e3ba8a78f6ba
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-07-18 02:43:47 +0000

    Tue, Jul 18, 2023, 8:43 AM +06:00

--- data_structure_and_algorithms/4_bubble_sort.c
+++ data_structure_and_algorithms/4_bubble_sort.c
@@ -5,12 +5,24 @@ int main(void)
     int arr[] = {10, 5, 2, 8, 7};
     int s = sizeof(arr) / sizeof(int);
 
+    int temp;
     for(int i = 0; i < s - 1; i++)
     {
-        for(int j = i; j < s; j++)
+        for(int j = i; j < s - 1; j++)
         {
-            if(arr[j] )
+            if(arr[j] > arr[j + 1])
+            {
+                temp  = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
         }
     }
+
+    for(int i = 0; i < s; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
     return 0;
 }
