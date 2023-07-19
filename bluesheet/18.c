9aed69329556c183b3f4ffe66113ba68e3087ec1
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-07-19 12:11:01 +0000

    Wed, Jul 19, 2023, 6:11 PM +06:00

--- bluesheet/18.c
+++ bluesheet/18.c
@@ -1,8 +1,19 @@
 #include<stdio.h>
 #include<math.h>
 //(2 power 31) - 1 = 2147483648 - 1 = 2147483647
+int binary_search(int *arr, int size, unsigned int a)
+{
+    int min = 0, max = size - 1, mid;
+    while(min <= max)
+    {
+        mid = (min + max) / 2;
 
-int binary_search(int a)
+        if(arr[mid] == a) return 1;
+        else if(a < mid) max = mid - 1;
+        else min = mid + 1;
+    }
+    return 0;
+}
 
 int main(void)
 {
@@ -21,6 +32,8 @@ int main(void)
     //     printf("%d ",position_of_ones[i]);
     // }
     // printf("\n");
+
+    for()
 
     return 0;
 }
