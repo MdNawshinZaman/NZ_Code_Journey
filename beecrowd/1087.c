434f5abe0cf437641d42659ade4cae3c33b7dda2
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-07-22 05:58:11 +0000

    Sat, Jul 22, 2023, 11:58 AM +06:00

--- beecrowd/1087.c
+++ beecrowd/1087.c
@@ -1,8 +1,17 @@
 #include<stdio.h>
+#include<math.h>
 
 int main(void)
 {
-    
+    while(1)
+    {
+        int x1, y1, x2, y2;
+        scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+
+        double d = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+
+        printf("%lf\n", d);
+    }
 
     return 0;
 }
