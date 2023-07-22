7ff644479463df60843c3a7aa5559c79d44331ba
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-07-22 05:43:43 +0000

    Sat, Jul 22, 2023, 11:43 AM +06:00

--- beecrowd/2702.c
+++ beecrowd/2702.c
@@ -5,6 +5,12 @@ int main(void)
     int a, b, c, x, y, z;
     scanf("%d %d %d %d %d %d", &a, &b, &c, &x, &y, &z);
 
-    int sum = ()
+    int sum = 0;
+
+    if(x > a){sum += x - a;}
+    if(y > b){sum += y - b;}
+    if(z > c){sum += z - c;}
+
+    printf("%d\n", sum);
     return 0;
 }
