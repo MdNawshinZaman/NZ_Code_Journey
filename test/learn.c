2567a86fe2e042dfe676aca28be7f9640d2de319
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-07-28 09:58:31 +0000

    Fri, Jul 28, 2023, 3:58 PM +06:00

--- test/learn.c
+++ test/learn.c
@@ -2,17 +2,10 @@
 
 int main(void)
 {
-    char a[107], b[107], c[107];
-    scanf("%s", a);
-    fflush(stdin);
-    scanf("%s", b);
-    fflush(stdin);
-    scanf("%s", c);
 
-    printf("%s%s%s\n", a, b, c);
-    printf("%s%s%s\n", b, c, a);
-    printf("%s%s%s\n", c, a, b);
-    printf("%s%s%s\n", a, b, c);
+    char txt = 'x';
+
+    printf("%d", txt);
 
     return 0;
 }
