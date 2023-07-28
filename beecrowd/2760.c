20f2ae3707656a214c109350c41e0350c004759c
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-07-28 03:20:50 +0000

    Fri, Jul 28, 2023, 9:20 AM +06:00

--- beecrowd/2760.c
+++ beecrowd/2760.c
@@ -2,7 +2,17 @@
 
 int main(void)
 {
-    
+    char a[107], b[107], c[107];
+    scanf("%[^\n]s", a);
+    fflush(stdin);
+    scanf("%[^\n]s", b);
+    fflush(stdin);
+    scanf("%[^\n]s", c);
+
+    printf("%s%s%s\n", a, b, c);
+    printf("%s%s%s\n", b, c, a);
+    printf("%s%s%s\n", c, a, b);
+    printf("%s%s%s\n", a, b, c);
 
     return 0;
 }
