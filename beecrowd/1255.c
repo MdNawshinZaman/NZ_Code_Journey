be8c1c353ac8a0ef037d14d1a8ebc39106ee104a
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-07-28 05:25:58 +0000

    Fri, Jul 28, 2023, 11:25 AM +06:00

--- beecrowd/1255.c
+++ beecrowd/1255.c
@@ -8,15 +8,19 @@ int main(void)
 
     for(int i = 0; i < n; i++)
     {
-        char freq[200] = '\0';
-        scanf("%[^\n]s", freq);
-        freq[strlen(freq)] = '\0';
+        char freq[200] = '\0', text[209] = '\0';
+        scanf("%[^\n]s", text);
+        freq[strlen(text)] = '\0';
 
         int j = 0;
         while(freq[j] != '\0')
         {
-            freq[]
+            freq[text[j]]++;
         }
+
+        int max = 0;
+
+        
     }
 
     return 0;
