219925e401ec43dcaa290fd287fbff99cbba2281
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-07-28 05:23:09 +0000

    Fri, Jul 28, 2023, 11:23 AM +06:00

--- beecrowd/1255.c
+++ beecrowd/1255.c
@@ -1,11 +1,23 @@
 #include<stdio.h>
+#include<string.h>
 
 int main(void)
 {
     int n;
     scanf("%d", &n);
 
-    for(int i = 0; )
+    for(int i = 0; i < n; i++)
+    {
+        char freq[200] = '\0';
+        scanf("%[^\n]s", freq);
+        freq[strlen(freq)] = '\0';
+
+        int j = 0;
+        while(freq[j] != '\0')
+        {
+            freq[]
+        }
+    }
 
     return 0;
 }
