58f4d6c140ec9ef4f68b2d7b94969e867ba1a21a
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-02 12:36:25 +0000

    Wed, Aug 2, 2023, 6:36 PM +06:00

--- codeforces/new_commers_training/sheet1/b.c
+++ codeforces/new_commers_training/sheet1/b.c
@@ -2,7 +2,14 @@
 
 int main(void)
 {
+    int a;
+    long long int b;
+    char c;
+    float d;
+    double e;
 
+    scanf("%d %lld %c %f %lf", &a, &b, &c, &d, &e);
+    printf("%d\n%lld\n%c\n%.2f\n%.1lf\n", a, b, c, d, e);
 
     return 0;
 }
