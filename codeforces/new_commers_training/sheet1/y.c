aa4e806772cab29d7ebc70825227e6b955a8dbd8
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-03 09:25:54 +0000

    Thu, Aug 3, 2023, 3:25 PM +06:00

--- codeforces/new_commers_training/sheet1/y.c
+++ codeforces/new_commers_training/sheet1/y.c
@@ -4,7 +4,8 @@
 int main(void)
 {
     long long a, b, c, d;
-    scanf("%lld %lld %lld %lld", &a, &b, )
+    scanf("%lld %lld %lld %lld", &a, &b, &c, &d);
+    printf("%lld\n", ((a * b * c * d) % 100));
 
     return 0;
 }
