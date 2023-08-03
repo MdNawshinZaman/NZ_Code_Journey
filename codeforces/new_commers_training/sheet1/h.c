4466b133df622b40c9a2f5d6d66a1980cc4af033
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-03 09:04:07 +0000

    Thu, Aug 3, 2023, 3:04 PM +06:00

--- codeforces/new_commers_training/sheet1/h.c
+++ codeforces/new_commers_training/sheet1/h.c
@@ -6,9 +6,8 @@ int main(void)
     int a, b;
     scanf("%d %d", &a, &b);
     printf("floor %d / %d = %.lf\n", a, b, floor(a / (float)b));
-    printf("floor %d / %d = %.lf\n", a, b, floor(a / (float)b));
-    printf("floor %d / %d = %.lf\n", a, b, floor(a / (float)b));
-
+    printf("ceil %d / %d = %.lf\n", a, b, ceil(a / (float)b));
+    printf("round %d / %d = %.lf\n", a, b, round(a / (float)b));
 
     return 0;
 }
