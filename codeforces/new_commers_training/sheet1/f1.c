4b8020e16454ddc1d2ce212fe4105f481fc93826
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-03 08:33:48 +0000

    Thu, Aug 3, 2023, 2:33 PM +06:00

--- codeforces/new_commers_training/sheet1/f1.c
+++ codeforces/new_commers_training/sheet1/f1.c
@@ -7,10 +7,10 @@ int main(void)
     char a[25], b[25];
     scanf("%s %s", a, b);
 
-    long long x = strlen(a);
-    long long y = strlen(b);
+    int x = strlen(a);
+    int y = strlen(b);
 
-    printf("%lld\n", atoi(a[x-1]) + atoi(b[x-1]));
+    printf("%d\n", atoi(&a[x-1]) + atoi(&b[x-1]));
 
     return 0;
 }
