dc3a94c0fbaab1ecef32283c987e25a034d86853
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-03 09:28:03 +0000

    Thu, Aug 3, 2023, 3:28 PM +06:00

--- codeforces/new_commers_training/sheet1/y.c
+++ codeforces/new_commers_training/sheet1/y.c
@@ -3,9 +3,9 @@
 
 int main(void)
 {
-    long long a, b, c, d;
-    scanf("%lld %lld %lld %lld", &a, &b, &c, &d);
-    printf("%lld\n", ((a * b * c * d) % 100));
+    unsigned long long a, b, c, d;
+    scanf("%llu %llu %llu %llu", &a, &b, &c, &d);
+    printf("%llu\n", ((a * b * c * d) % 100));
 
     return 0;
 }
