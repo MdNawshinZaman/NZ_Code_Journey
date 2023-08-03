db939594022f6fbaa7aa257a2df02aaadfff8443
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-03 09:19:13 +0000

    Thu, Aug 3, 2023, 3:19 PM +06:00

--- codeforces/new_commers_training/sheet1/j.c
+++ codeforces/new_commers_training/sheet1/j.c
@@ -6,8 +6,14 @@ int main(void)
     int a, b;
     scanf("%d %d", &a, &b);
 
-    if(a >= b) printf("Yes\n");
-    else printf("No\n");
+   if((a % b == 0) || (b % a == 0))
+   {
+        printf("Multiples\n");
+   }
+   else
+   {
+        printf("No Multiples\n");
+   }
 
     return 0;
 }
