ee919b400623e3ae469f2df0d09c42a78e0a989d
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-03 08:27:50 +0000

    Thu, Aug 3, 2023, 2:27 PM +06:00

--- codeforces/new_commers_training/sheet1/f.c
+++ codeforces/new_commers_training/sheet1/f.c
@@ -2,7 +2,11 @@
 
 int main(void)
 {
-    
+    long long n, m, sum = 0;
+    scanf("%lld %lld", &n, &m);
+
+    sum = (n % 10) + (m % 10);
+    printf("%lld\n", sum);
 
     return 0;
 }
---
+++ codeforces/new_commers_training/sheet1/f1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(void)
+{
+    char a[25], b[25];
+    scanf("%s %s", a, b);
+
+    return 0;
+}
