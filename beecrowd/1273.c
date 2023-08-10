425b340f31dc8deb2dc15ce5a22394285bef8dce
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-10 14:03:58 +0000

    Thu, Aug 10, 2023, 8:03 PM +06:00

--- beecrowd/1273.c
+++ beecrowd/1273.c
@@ -4,9 +4,36 @@
 int main(void)
 {
     int n;
-    while(scanf("%d", &n) != 0)
+    while(1)
     {
-        cahr
+        scanf("%d", &n);
+        if(n == 0) break;
+
+        int big = 0;
+        char s_str[50][51];
+        for(int i = 0; i < n; i++)
+        {
+            fflush(stdin);
+            scanf("%s", s_str[i]);
+
+            int m = strlen(s_str[i]);
+            if(m > big)
+            {
+                big = m;
+            }
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            int x = strlen(s_str[i]);
+
+            for(int j = 0; j < (big - x); j++)
+            {
+                printf(" ");
+            }
+            printf("%s\n", s_str[i]);
+        }
+        printf("\n");
     }
 
     return 0;
