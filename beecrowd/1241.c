c35758d6f6d76713a509853ac326c76051b8a8dc
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-10 05:03:20 +0000

    Thu, Aug 10, 2023, 11:03 AM +06:00

--- beecrowd/1241.c
+++ beecrowd/1241.c
@@ -3,7 +3,34 @@
 
 int main(void)
 {
-    
+    int n;
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+    {
+        char a[1009], b[1009];
+        fflush(stdin);
+        scanf("%s", a);
+        fflush(stdin);
+        scanf("%s", b);
+
+        int p = strlen(a), q = strlen(b);
+        char comp1[1009], comp2[1009];
+
+        if(q > p) {printf("nao encaixa\n");}
+        else
+        {
+            int r = 0;
+            for(int j = p-1; j > p-q-1; j--)
+            {
+                comp1[r] = a[j];
+                r++;
+            }
+            comp1[r] = '\0';
+
+            printf("%s\n", comp1);
+        }
+    }
 
     return 0;
 }
