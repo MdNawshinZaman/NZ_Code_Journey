423d2e493645c76a4643f3ed3a79c368c86b536b
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-10 14:34:39 +0000

    Thu, Aug 10, 2023, 8:34 PM +06:00

--- beecrowd/1255.c
+++ beecrowd/1255.c
@@ -3,42 +3,6 @@
 
 int main(void)
 {
-    int n;
-    scanf("%d", &n);
-    int freq[200] = {0};
-
-    for(int i = 0; i < n; i++)
-    {
-
-        char text[209] = {'\0'};
-        getchar();
-        scanf("%[^\n]s", text);
-        text[strlen(text)] = '\0';
-
-        int j = 0;
-        while(text[j] != '\0')
-        {
-            if((text[j] >= 65) && (text[j] <= 90)) freq[text[j] + 32]++;
-            else freq[text[j]]++;
-            j++;
-        }
-
-        int max = 0;
-
-        for(int k = 97; k <= 122; k++)
-        {
-            if(max < freq[k]) max = freq[k];
-        }
-
-        for(int k = 97; k <= 122; k++)
-        {
-            if(max == freq[k]) printf("%c", k);
-        }
-
-        printf("\n");
-
-
-    }
 
     return 0;
 }
