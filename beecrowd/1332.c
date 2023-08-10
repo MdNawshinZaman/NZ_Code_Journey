13d514f66d3a3165c737a472fc89be363ee8ca85
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-10 04:39:53 +0000

    Thu, Aug 10, 2023, 10:39 AM +06:00

--- beecrowd/1332.c
+++ beecrowd/1332.c
@@ -2,7 +2,31 @@
 
 int main(void)
 {
-    
+    int n;
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+    {
+        char srt[6];
+        fflush(stdin);
+        scanf("%s", str);
+
+        if(strlen(str) == 5)
+        {
+            printf("3\n");
+        }
+        else
+        {
+            if(str[0] == 'o' || str[1] == 'n' || str[2] == 'e')
+            {
+                printf("1\n");
+            }
+            else
+            {
+                printf("2\n");
+            }
+        }
+    }
 
     return 0;
 }
