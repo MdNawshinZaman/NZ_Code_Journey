9690116669491cc6f6f48b29d94748aad605b745
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-16 13:52:06 +0000

    Wed, Aug 16, 2023, 7:52 PM +06:00

--- beecrowd/1309.c
+++ beecrowd/1309.c
@@ -6,14 +6,7 @@ int main(void)
     long long int n, m;
     while(scanf("%lld %lld", &n, &m) != EOF)
     {
-        double sum = 0;
-        sum = n + (m / 100.0);
 
-        char str[30];
-        sprintf(str, "%0,.2f", sum);
-        int len = strlen(str);
-
-        printf("%s\n", str);
 
     }
 
