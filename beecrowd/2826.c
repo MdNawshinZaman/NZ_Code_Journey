f69e21e492aede5c4d1bee556ebeff97252d4db6
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-16 13:44:01 +0000

    Wed, Aug 16, 2023, 7:44 PM +06:00

--- beecrowd/2826.c
+++ beecrowd/2826.c
@@ -6,7 +6,14 @@ int main(void)
     char str1[22], str2[22];
     scanf("%s %s", str1, str2);
 
-
+    if(strcmp(str1, str2) <= 0)
+    {
+        printf("%s\n%s\n", str1, str2);
+    }
+    else
+    {
+        printf("%s\n%s\n", str2, str1);
+    }
 
     return 0;
 }
