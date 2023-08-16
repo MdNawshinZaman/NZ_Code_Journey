d127813fb9b236befbe27bba5b3669a7c3959a66
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-16 17:47:05 +0000

    Wed, Aug 16, 2023, 11:47 PM +06:00

--- beecrowd/3088.c
+++ beecrowd/3088.c
@@ -3,9 +3,31 @@
 
 int main(void)
 {
-    char str[107];
+    char str[100007];
+    char final[1000007];
+
     scanf("%[^\n]", str);
 
+    int i = 0;
+    while(str[i] != '\0')
+    {
+        if ((str[i] == ' ') && ((str[i+1] == ',') || (str[i+1] == '.')))
+        str[i] = -1;
+    }
+
+    i = 0;
+    int j = 0;
+    while(str[i] != '\0')
+    {
+        if(str[i] != -1)
+        {
+            final[j] = str[i];
+            j++;
+        }
+        i++;
+    }
+
+    printf("%s\n", final);
 
     return 0;
 }
