be4e4fbe8ec482fe07eca6fd1eedf9b1096db75f
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-17 04:17:01 +0000

    Thu, Aug 17, 2023, 10:17 AM +06:00

--- beecrowd/1551.c
+++ beecrowd/1551.c
@@ -8,7 +8,31 @@ int main(void)
 
     for(int i = 0; i < n; i++)
     {
-        
+        char str[100];
+        int freq[150];
+        scanf("%[^\n]%*c", str);
+        int len = strlen[str];
+        str[len] = '\0';
+
+        int j = 0;
+        while(str[j] != '\0')
+        {
+            if(str[j] >= 'a' && str[j] <= 'z')
+            {
+                freq[str[j]] = 1;
+            }
+            j++;
+        }
+
+        int sum  = 0;
+        for(int k = 'a'; k <= 'z'; k++)
+        {
+            sum += freq[k];
+        }
+
+        if (sum == 26) printf("frase completa\n");
+        else if (sum >= 13) printf("frase quase completa\n");
+        else printf("frase mal elaborada\n");
     }
 
     return 0;
