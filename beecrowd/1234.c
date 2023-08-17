b25818a574a4b8a26004483f2119ecd94b202584
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-17 12:03:28 +0000

    Thu, Aug 17, 2023, 6:03 PM +06:00

--- beecrowd/1234.c
+++ beecrowd/1234.c
@@ -3,7 +3,53 @@
 
 int main(void)
 {
-    
+    char str[51];
+
+    while(scanf("%[^\n]%*c", str) != EOF)
+    {
+        int len = strlen(str);
+        str[len] = '\0';
+
+        int i = 0, flag = 1;
+        while(str[i] != '\0')
+        {
+            if(((str[i] >= 'A') && (str[i] <= 'Z')) || ((str[i] >= 'a') && (str[i] <= 'z')))
+            {
+                if(flag == 1)
+                {
+                    if ((str[i] >= 'A') && (str[i] <= 'Z'))
+                    {
+                        printf("%c", str[i]);
+                        flag = 0;
+                    }
+                    else
+                    {
+                        printf("%c", str[i] - 32);
+                        flag = 0;
+                    }
+                }
+                else
+                {
+                    if ((str[i] >= 'a') && (str[i] <= 'a'))
+                    {
+                        printf("%c", str[i]);
+                        flag = 1;
+                    }
+                    else
+                    {
+                        printf("%c", str[i] + 32);
+                        flag = 1;
+                    }
+                }
+            }
+            else
+            {
+                printf("%c", str[i]);
+            }
+            i++;
+        }
+        printf("\n");
+    }
 
     return 0;
 }
