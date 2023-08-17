449c3c0c9540389a6e84fb7df03443925627bfbc
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-17 02:55:18 +0000

    Thu, Aug 17, 2023, 8:55 AM +06:00

--- beecrowd/3088.c
+++ beecrowd/3088.c
@@ -1,20 +1,24 @@
 #include<stdio.h>
 #include<string.h>
 #define max 100007
+
 int main(void)
 {
     char str[max];
     char final[max];
 
     scanf("%[^\n]s", str);
+    int len = strlen(str);
+    str[len] = '\0';
 
-    printf("%s\n", str);
+    // printf("%s\n", str);
 
     int i = 0;
     while(str[i] != '\0')
     {
         if ((str[i] == ' ') && ((str[i+1] == ',') || (str[i+1] == '.')))
         str[i] = -1;
+        i++;
     }
 
     i = 0;
