fcb3625165f3d14a8fa473194472948c9c9d3b2b
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-17 07:06:13 +0000

    Thu, Aug 17, 2023, 1:06 PM +06:00

---
+++ beecrowd/1235.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    int n;
+    scanf("%d%*c", &n);
+
+    
+
+    return 0;
+}
--- beecrowd/1238.c
+++ beecrowd/1238.c
@@ -6,15 +6,29 @@ int main(void)
     int n;
     scanf("%d%*c", &n);
 
-    for(int i = 0; i <  n; i++)
+    char str1[51], str2[51];
+    for(int i = 0; i < n; i++)
     {
-        char str1[51], str2[51];
         scanf("%s%*c%s%*c", str1, str2);
-
         int len1 = strlen(str1);
         int len2 = strlen(str2);
 
-        
+        int big =  (len1 >  len2) ? len1:len2;
+
+        int j = 0;
+        while(j < big)
+        {
+            if(j < len1)
+            {
+                printf("%c", str1[j]);
+            }
+            if(j < len2)
+            {
+                printf("%c", str2[j]);
+            }
+            j++;
+        }
+        printf("\n");
     }
 
     return 0;
