8a0c77dcc75bebd972635b0a7e3e0b01cb29c5e0
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-18 08:22:40 +0000

    Fri, Aug 18, 2023, 2:22 PM +06:00

--- beecrowd/1140.c
+++ beecrowd/1140.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
+#include <string.h>
+
 
 int main(void)
 {
-    while(1);
+    char str[1107];
+    while(1)
+    {
+        scanf("%[^\n]%*c", str);
+        int len  = strlen(str);
+        str[len] = '\0';
+
+        if(strcmp(str, "*") == 0) break;
+
+        char ch = str[0];
+
+        int i = 0;
+
+        while(str[i] != '\0')
+        {
+            
+        }
+
+    }
 
     return 0;
 }
