9a96bffafed788405f9d87c25ee3120cee7c9ae5
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-18 12:12:30 +0000

    Fri, Aug 18, 2023, 6:12 PM +06:00

--- beecrowd/1028.c
+++ beecrowd/1028.c
@@ -1,27 +1,36 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
+
+int gcd(f1, f2)
+{
+    int temp;
+    if (f1 < f2)
+    {
+        temp = f1;
+        f1 = f2;
+        f2 = temp;
+    }
+
+    int rem;
+    while(f2 != 0)
+    {
+        rem = f1 % f2;
+        f1 = f2;
+        f2 = rem;
+    }
+    return f1;
+}
 
 int main(void)
 {
     int n;
     scanf("%d", &n);
 
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         int f1, f2;
         scanf("%d %d", &f1, &f2);
-        int small = (f1 < f2)? f1 : f2;
-
-        int stack = 0;
-        for(int j = small; j > 0; j--)
-        {
-            if((f1 % j == 0) && (f2 % j == 0))
-            {
-                stack = j;
-                break;
-            }
-        }
-        printf("%d\n", stack);
+        printf("%d",gcd(f1, f2));
     }
 
     return 0;
