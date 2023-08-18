8926f8f3fcd824676978501aa19c9c8515299027
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-18 15:32:52 +0000

    Fri, Aug 18, 2023, 9:32 PM +06:00

--- beecrowd/2874.c
+++ beecrowd/2874.c
@@ -10,7 +10,7 @@ int main(void)
         for (int i = 0; i < n; i++)
         {
             char str[9];
-            
+            scanf("")
 
             printf("%d\n", (char) strtoll(str, NULL, 2));
         }
