c82275a3c123c9f26d796a0109baa51110a51b23
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-18 05:30:53 +0000

    Fri, Aug 18, 2023, 11:30 AM +06:00

--- beecrowd/1837.c
+++ beecrowd/1837.c
@@ -5,8 +5,8 @@ int main(void)
     int a, b, r, q;
     scanf("%d %d", &a, &b);//-7 3
 
-    r = a % b;//(-3)
-    q = a / b;//(2)
+    q = round(a / b); // -3
+    r = 
 
     printf("%d %d\n", q, r);
 
