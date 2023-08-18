fa05e576fd549322c70ce2dacdcf310b37d495f3
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-18 06:17:19 +0000

    Fri, Aug 18, 2023, 12:17 PM +06:00

--- beecrowd/1573.c
+++ beecrowd/1573.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
-    
+    int a, b, c;
+    while(1)
+    {
+        scanf("%d %d %d", &a, &b, &c);
+        if(a == 0 && b == 0 && c == 0) break;
+
+        printf("%.lf\n", pow(a*b*c, 1/3.0));
+    }
 
     return 0;
 }
