7920deb7714734d22040470ac2de4b2047b8c321
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-18 05:39:01 +0000

    Fri, Aug 18, 2023, 11:39 AM +06:00

--- beecrowd/1837.c
+++ beecrowd/1837.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 int main(void)
 {
     int a, b, r, q;
     scanf("%d %d", &a, &b);//-7 3
 
-    q = ceil(a / (doublr)b); // -3
+    q = ceil(abs(a / b)) * (-1); // -3
     //divident = divisor * quotient + reminder
     // a = b * q + r
     r = a - (b * q); // 2
