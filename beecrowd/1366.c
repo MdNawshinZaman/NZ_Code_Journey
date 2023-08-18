2860d4d2837445c619e75d8ea32f098078f27bc4
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-18 16:24:33 +0000

    Fri, Aug 18, 2023, 10:24 PM +06:00

--- beecrowd/1366.c
+++ beecrowd/1366.c
@@ -4,7 +4,19 @@
 
 int main(void)
 {
-    
+    int n;
+    while(1)
+    {
+        scanf("%d", &n);
+        if (n == 0) break;
+
+        for(int i = 0; i < n; i++)
+        {
+            
+        }
+    }
+
+
 
     return 0;
 }
