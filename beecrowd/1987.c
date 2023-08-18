1e142ad1ffe484d6a008d011806eb4a571843a8e
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-18 04:49:08 +0000

    Fri, Aug 18, 2023, 10:49 AM +06:00

--- beecrowd/1987.c
+++ beecrowd/1987.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
     int n;
     char m[11];
+    int flag = 0;
     while(scanf("%d %s", &n, m) != EOF)
     {
+        if(flag == 1)printf("\n");
+        else flag = 1;
+
         int sum = 0;
         for(int i = 0; i <  n; i++)
         {
-
+            sum += atoi(m[i]);
         }
+
+        if(sum % 3 == 0) printf("%d sim", sum);
+        else printf("%d nao", sum);
+
     }
 
     return 0;
