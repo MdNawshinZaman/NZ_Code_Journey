73ed9403cf354f518826b384814f34afbe12f036
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-18 08:50:07 +0000

    Fri, Aug 18, 2023, 2:50 PM +06:00

--- beecrowd/2345.c
+++ beecrowd/2345.c
@@ -8,11 +8,17 @@ int main(void)
         scanf("%d", &arr[i]);
     }
 
+    int temp;
     for(int i = 0; i < 4; i++)
     {
         for(int j = 0; j < 3; j++)
         {
             if(arr[i] > arr[i+1])
+            {
+                temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1]  = temp;
+            }
         }
     }
     return 0;
