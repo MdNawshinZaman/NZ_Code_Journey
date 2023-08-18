cb3b8cbb07a28cc652ba2a56c1ae0ff23408c256
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-18 05:34:55 +0000

    Fri, Aug 18, 2023, 11:34 AM +06:00

--- beecrowd/1837.c
+++ beecrowd/1837.c
@@ -6,7 +6,7 @@ int main(void)
     int a, b, r, q;
     scanf("%d %d", &a, &b);//-7 3
 
-    q = ceil(a / b); // -3
+    q = floor(a / b); // -3
     //divident = divisor * quotient + reminder
     // a = b * q + r
     r = a - (b * q); // 2
