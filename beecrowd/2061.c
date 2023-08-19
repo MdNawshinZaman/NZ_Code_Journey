9f48e41f857988d2a0784727570d94a36f64d2cb
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-19 16:39:05 +0000

    Sat, Aug 19, 2023, 10:39 PM +06:00

--- beecrowd/2061.c
+++ beecrowd/2061.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
-#include <math.h>
+#include <string.h>
 
 
 int main(void)
 {
-    
+    int n, m;
+    scanf("%d%*c%d%*c", &n, &m);
+
+    for(int i =0 ; i < m; i++)
+    {
+        char action[10];
+        scanf("%s%*c", action);
+
+        if(strcmp(action, "fechou") == 0) n++;
+        else n--;
+    }
+    printf("%d\n", n);
 
     return 0;
 }
