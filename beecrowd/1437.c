b373527a98d2cdf0e15347248c1b0382e7d3af94
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-19 05:43:30 +0000

    Sat, Aug 19, 2023, 11:43 AM +06:00

--- beecrowd/1437.c
+++ beecrowd/1437.c
@@ -1,39 +1,53 @@
-#include <stdio.h>
 #include <math.h>
-
+#include <stdio.h>
 
 int main(void)
 {
     int n;
     char str[1001];
-    while(1)
+    while (1)
     {
         scanf("%d%*c", &n);
-        if(n == 0) break;
+        if (n == 0)
+            break;
 
         scanf("%s%*c", str);
 
         int face = 1;
-        for(int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
         {
-            if(str[i] == 'E')
+            if (str[i] == 'E')
             {
                 face--;
-                if(face < 1) face = 4;
+                if (face < 1)
+                    face = 4;
             }
-            else if(str[i] == 'D')
+            else if (str[i] == 'D')
             {
                 face++;
-                if (face > 4) face = 1;
+                if (face > 4)
+                    face = 1;
             }
         }
 
-        switch(face)
+        switch (face)
         {
             case 1:
-            printf("")
-        }
+                printf("N\n");
+                break;
 
+            case 2:
+                printf("L\n");
+                break;
+
+            case 3:
+                printf("S\n");
+                break;
+
+            case 4:
+                printf("O\n");
+                break;
+        }
     }
 
     return 0;
