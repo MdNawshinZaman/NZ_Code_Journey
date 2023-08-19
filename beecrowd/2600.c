3a03686825f7ed7af03e58fc793bf64e97cb7021
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-19 15:42:09 +0000

    Sat, Aug 19, 2023, 9:42 PM +06:00

--- beecrowd/2600.c
+++ beecrowd/2600.c
@@ -1,19 +1,53 @@
-#include <stdio.h>
 #include <math.h>
-
+#include <stdio.h>
 
 int main(void)
 {
     int n;
     scanf("%d", &n);
 
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        int arr[6]
-
-        if((a1 >= 1 && a1 <= 6) && (a2 >= 1 && a2 <= 6) && (b1 >= 1 && b1 <= 6) && (b2 >= 1 && b2 <= 6) && (c1 >= 1 && c1 <= 6) && (c2 >= 1 && c2 <= 6) && (a1+a2+b1+b2+c1+c2 == 21))
+        int arr[6];
+        for (int j = 0; j < 6; j++)
         {
-            if((a1 + a2 == 7) && (b1 + b2 == 7) && (c1 + c2 == 7))
+            scanf("%d", &arr[j]);
+        }
+        int a1, a2, b1, b2, c1, c2;
+        a1 = arr[0];
+        b1 = arr[1];
+        c1 = arr[2];
+        b2 = arr[3];
+        c2 = arr[4];
+        a2 = arr[5];
+
+        for (int j = 0; j < 6; j++)
+        {
+            int temp;
+            for (int k = 0; k < 5; k++)
+            {
+                if (arr[k] > arr[k + 1])
+                {
+                    temp = arr[k];
+                    arr[k] = arr[k + 1];
+                    arr[k + 1] = temp;
+                }
+            }
+        }
+
+        int unique = 1;
+        for (int j = 0; j < 6; j++)
+        {
+            if (arr[j] != j + 1)
+            {
+                unique = 0;
+                break;
+            }
+        }
+
+        if (unique)
+        {
+            if ((a1 + a2 == 7) && (b1 + b2 == 7) && (c1 + c2 == 7))
             {
                 printf("SIM\n");
             }
@@ -26,7 +60,6 @@ int main(void)
         {
             printf("NAO\n");
         }
-
     }
 
     return 0;
