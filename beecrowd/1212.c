980df1a05463413a77a95c2b27e622ecfdd49a86
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-19 12:03:33 +0000

    Sat, Aug 19, 2023, 6:03 PM +06:00

--- beecrowd/1212.c
+++ beecrowd/1212.c
@@ -8,15 +8,10 @@ int main(void)
     scanf("%lld %lld", &a, &b);
 
     int num_carry = 0, carry = 0;
-
-    int temp;
-    if (a < b)
-    {
-        temp = a;
-        a = b;
-        b = temp;
-    }
     
+
+
+
 
     return 0;
 }
