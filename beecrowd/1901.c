4a24df3f514b88f16ae8a90cf29ac5a2375565a8
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-19 17:06:43 +0000

    Sat, Aug 19, 2023, 11:06 PM +06:00

--- beecrowd/1901.c
+++ beecrowd/1901.c
@@ -4,8 +4,37 @@
 
 int main(void)
 {
-    
+    int n;
+    scanf("%d",&n);
 
+    int arr[n][n];
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
 
+    int freq[1009];
+    for(int i = 0; i <= 1009; i++)
+    {
+        freq[i] = 0;
+    }
+
+    for(int i = 0; i < (n*2); i++)
+    {
+        int y, z;
+        scanf("%d %d", &y, &z);
+        freq[arr[y-1][z-1]] = 1;
+    }
+
+    int species = 0;
+    for(int i = 0; i <= 1009; i++)
+    {
+        species += freq[i];
+    }
+
+    printf("%d\n", species);
     return 0;
 }
