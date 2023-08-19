5b6c62d5f550aaec99df2743665250fc8142a5b7
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-19 05:23:05 +0000

    Sat, Aug 19, 2023, 11:23 AM +06:00

--- beecrowd/1542.c
+++ beecrowd/1542.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
-#include <math.h>
-
 
 int main(void)
 {
-    
+    int q = 0, d = 0, p = 0;
+    while(1)
+    {
+        scanf("%d", &q);
+        if(q == 0) break;
+        scanf("%d %d", &d, &p);
+        int page = (d*p*q) / (p-q);
 
+        if(page > 1)
+        {
+            printf("%d paginas\n", page);
+        }
+        else
+        {
+            printf("%d pagina\n", page);
+        }
+    }
     return 0;
 }
