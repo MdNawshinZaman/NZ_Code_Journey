969df89800139f77b9c224f3c8867b02b0d624da
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-19 05:41:44 +0000

    Sat, Aug 19, 2023, 11:41 AM +06:00

--- beecrowd/1437.c
+++ beecrowd/1437.c
@@ -12,9 +12,26 @@ int main(void)
         if(n == 0) break;
 
         scanf("%s%*c", str);
+
+        int face = 1;
         for(int i = 0; i < n; i++)
         {
+            if(str[i] == 'E')
+            {
+                face--;
+                if(face < 1) face = 4;
+            }
+            else if(str[i] == 'D')
+            {
+                face++;
+                if (face > 4) face = 1;
+            }
+        }
 
+        switch(face)
+        {
+            case 1:
+            printf("")
         }
 
     }
