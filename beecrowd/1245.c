171e846f962e3851909323c256342533a95bce8f
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-19 16:22:55 +0000

    Sat, Aug 19, 2023, 10:22 PM +06:00

--- beecrowd/1245.c
+++ beecrowd/1245.c
@@ -1,13 +1,44 @@
-#include <stdio.h>
 #include <math.h>
-
+#include <stdio.h>
 
 int main(void)
 {
-    int n;
-    scanf("%d", &n);
+    while (1)
+    {
+        int n;
+        scanf("%d%*c", &n);
 
-    for
+        int D[61], E[61];
+
+        for(int i = 0; i < 62; i++)
+        {
+            D[i] = 0;
+            E[i] = 0;
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            int m;
+            char l;
+            scanf("%d%*c%c%*c", &m, &l);
+
+            if(l == 'D') D[m]++;
+            else if(l == 'E') E[m]++;
+        }
+
+        int pair = 0;
+
+        for(int i = 0; i <= 61; i++)
+        {
+            if((D[i] > 0) && (E[i] > 0))
+            {
+                int small = (D[i] < E[i])? D[i] : E[i];
+                pair += small;
+            }
+        }
+
+        printf("%d\n", pair);
+    }
 
     return 0;
 }
