04cdbd92c86744396ad2175ae30fe8f79152e6f2
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-19 06:03:59 +0000

    Sat, Aug 19, 2023, 12:03 PM +06:00

--- beecrowd/1849.c
+++ beecrowd/1849.c
@@ -5,7 +5,29 @@
 int main(void)
 {
    int l1, c1, l2, c2;
-   scanf("%d %d %d %d", )
+   scanf("%d %d %d %d", &l1, &c1, &l2, &c2);
+
+   int temp;
+   if(l1 < c1)
+   {
+        temp = l1;
+        l1 = c1;
+        c1 = temp;
+   }
+
+   if(l2 < c2)
+   {
+        temp = l2;
+        l2 = c2;
+        c2 = temp;
+   }
+
+   int c = c1 + c2;
+   int l = (l1 < l2)? l1 : l2;
+
+   int a = (l < c)? l : c;
+
+   printf("%lld\n", a*a);
 
 
     return 0;
