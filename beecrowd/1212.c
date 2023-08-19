024bbee9138ca3f6090f29fd19fdfb7d494c5458
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-19 12:12:24 +0000

    Sat, Aug 19, 2023, 6:12 PM +06:00

--- beecrowd/1212.c
+++ beecrowd/1212.c
@@ -1,27 +1,42 @@
-#include <stdio.h>
 #include <math.h>
-
+#include <stdio.h>
 
 int main(void)
 {
     long long a, b;
-    scanf("%lld %lld", &a, &b);
-
-    int num_carry = 0, carry = 0;
-
-    int x = a % 10;
-    int y = b % 10;
-
-    if (x + y + carry >= 10)
+    while (1)
     {
-        num_carry++;
-        carry = 1;
-    }
-    else
-    {
-        carry  = 0;
-    }
+        scanf("%lld %lld", &a, &b);
+        if(a == 0 && b == 0) break;
 
+        int num_carry = 0, carry = 0;
+
+        while (a == 0 && b == 0)
+        {
+            int x = a % 10;
+            int y = b % 10;
+
+            if (x + y + carry >= 10)
+            {
+                num_carry++;
+                carry = 1;
+            }
+            else
+            {
+                carry = 0;
+            }
+
+            a /= 10;
+            b /= 10;
+        }
+
+        if (num_carry == 0)
+            printf("No carry operation.\n");
+        else if (num_carry == 1)
+            printf("1 carry operation.\n");
+        else
+            printf("%d carry operations.\n", num_carry);
+    }
 
     return 0;
 }
