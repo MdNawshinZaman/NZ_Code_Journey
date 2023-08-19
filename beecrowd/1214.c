d88540b9e074de385b879753b35ef552a046b21a
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-19 17:23:17 +0000

    Sat, Aug 19, 2023, 11:23 PM +06:00

--- beecrowd/1214.c
+++ beecrowd/1214.c
@@ -4,7 +4,35 @@
 
 int main(void)
 {
-    
+    int c;
+    while(scanf("%d", &c) != EOF)
+    {
+        for(int i = 0; i < c; i++)
+        {
+            int n;
+            scanf("%d", &n);
+            int arr[n];
+
+            double avg = 0;
+            for(int j = 0; j < n; j++)
+            {
+                scanf("%d", &arr[j]);
+                avg += arr[i];
+            }
+            avg /= 3.0;
+
+            int a_avg = 0;
+
+            for(int j = 0; j < n; j++)
+            {
+                if(arr[j] > avg) a_avg++;
+            }
+
+            printf("%.3lf", (a_avg/avg) * 100.0);
+
+        }
+    }
+
 
     return 0;
 }
