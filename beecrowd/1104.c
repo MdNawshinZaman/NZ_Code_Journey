51ce1c9bcf5e94c03f0ee60d905df38cad4edb21
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-20 13:41:20 +0000

    Sun, Aug 20, 2023, 7:41 PM +06:00

--- beecrowd/1104.c
+++ beecrowd/1104.c
@@ -17,21 +17,37 @@ int main(void)
         int a, b;
         scanf("%d %d", &a, &b)
 
+        if(a == 0 && b == 0) break;
+
         int temp;
         for(int i = 0; i < a; i++)
         {
             scanf("%d", &temp);
-            A[temp]++;
+            A[temp] = 1;
         }
 
         for(int i = 0; i < b; i++)
         {
             scanf("%d", &temp);
-            B[temp]++;
+            B[temp] = 1;
         }
 
-        
+        int sum1 = 0, sum2 = 0;
+        for(int i = 0; i <= 10009; i++)
+        {
+            if(A[i] == B[i])
+            {
+                A[i] = 0;
+                B[i] = 0;
+            }
+            else
+            {
+                sum1 += A[i];
+                sum2 += B[i];
+            }
+        }
 
+        printf("%d\n", (sum1<sum2)?sum1:sum2);
 
     }
 
