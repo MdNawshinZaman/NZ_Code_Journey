a52dbb6011779c972fc04d70e6144f4180716058
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-20 12:06:27 +0000

    Sun, Aug 20, 2023, 6:06 PM +06:00

--- beecrowd/1512.c
+++ beecrowd/1512.c
@@ -1,21 +1,34 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 #include <string.h>
 
 int gcd(int a, int b)
 {
-    if(b == 0) return a;
+    if (b == 0)
+        return a;
     int r = a % b;
-    return gcd(b,r);
+    return gcd(b, r);
 }
 
 int main(void)
 {
     int n, a, b;
-    scanf("%d %d %d", &n, &a, &b);
+    while (1)
+    {
+        scanf("%d %d %d", &n, &a, &b);
+        if (n == 0 && a == 0 && b == 0)
+            break;
 
-    printf("%d", (n/a) + (n/b) - (gcd()))
+        int temp;
+        if (a < b)
+        {
+            temp = a;
+            a = b;
+            b = temp;
+        }
 
+        printf("%d\n", (n / a) + (n / b) - (gcd(a, b)));
+    }
 
     return 0;
 }
