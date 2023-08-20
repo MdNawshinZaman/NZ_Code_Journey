e4024551a40b39ba409adfdba109b949bb5fbf5c
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-20 02:25:52 +0000

    Sun, Aug 20, 2023, 8:25 AM +06:00

--- beecrowd/1104.c
+++ beecrowd/1104.c
@@ -5,7 +5,19 @@
 
 int main(void)
 {
+    int A[10009], B[10009];
+    for(;;)
+    {
+        for(int i = 0; i <= 10009; i++)
+        {
+            A[i] = 0;
+            B[i] = 0;
+        }
 
+        int a, b;
+        scanf("%d %d", &a, &b)
+        for(int i)
+    }
 
     return 0;
 }
