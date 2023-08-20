233c3503d46914d5da933b176fb9b575f0e798bb
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-20 14:09:16 +0000

    Sun, Aug 20, 2023, 8:09 PM +06:00

--- beecrowd/2137.c
+++ beecrowd/2137.c
@@ -5,10 +5,27 @@
 
 int main(void)
 {
-    int n;
+    int n, arr[10009], temp;
     while(scanf("%d", &n) != EOF)
     {
-        
+        for(int i = 0; i < 10009; i++)
+        {
+            arr[i] = 0;
+        }
+
+        for(int i= 0; i < n; i++)
+        {
+            scanf("%d", &temp);
+            arr[temp] = 1;
+        }
+
+        for(int i= 0; i < 10009; i++)
+        {
+            if(arr[i] == 1)
+            {
+                printf("%04d\n", i);
+            }
+        }
     }
 
     return 0;
