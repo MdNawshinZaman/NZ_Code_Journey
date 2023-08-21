d7c0c9028e2b48302a6c47a7d3aa7c71bf81058b
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-21 16:42:36 +0000

    Mon, Aug 21, 2023, 10:42 PM +06:00

--- beecrowd/1414.c
+++ beecrowd/1414.c
@@ -5,14 +5,22 @@
 
 int main(void)
 {
-    int t, n;
-
+    int t, n, temp;
+    char team[13];
     while(1)
     {
         scanf("%d %d", &t, &n);
         if(t == 0) break;
 
-        
+        int sum = 0;
+        for(int i = 0; i < t; i++)
+        {
+            scanf(" %s %d", team, &temp);
+            sum += temp % 3;
+        }
+
+        printf("%d\n", sum / 2);
+
     }
 
     return 0;
