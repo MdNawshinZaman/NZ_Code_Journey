2784db0d9b7014c3fd9d45185e9ffd9d0ebc3f0a
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-21 13:25:34 +0000

    Mon, Aug 21, 2023, 7:25 PM +06:00

--- beecrowd/1219.c
+++ beecrowd/1219.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
-
+#define PI 3.1415926535897
 
 int main(void)
 {
-
-
+    int a, b, c;
+    while(scanf("%d %d %d", &a, &b, &c) != EOF)
+    {
+        double sun_flwr, violt, rose;
+        sum_flwr = PI * c * c;
+    }
     return 0;
 }
