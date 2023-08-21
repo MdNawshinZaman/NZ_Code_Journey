58993b83dae3d6c85832faccafd8d2260bc1cae3
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-21 11:44:58 +0000

    Mon, Aug 21, 2023, 5:44 PM +06:00

--- beecrowd/1221.c
+++ beecrowd/1221.c
@@ -5,7 +5,25 @@
 
 int main(void)
 {
-    
+    int n;
+    scanf("%d", &n);
+
+    unsigned int temp;
+    for(int j = 0; j < n; j++)
+    {
+        scanf("%u", &temp);
+        int flag = 1;
+        for(int i = 2; i <= sqrt(temp); i++)
+        {
+            if(temp % i == 0)
+            {
+                flag = 0;
+                break;
+            }
+        }
+
+        printf("%s\n", (flag == 1)? "Prime":"Not Prime");
+    }
 
     return 0;
 }
