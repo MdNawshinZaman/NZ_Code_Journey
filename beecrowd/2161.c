11e327f77807d776e21cc7210e7d9ebc58149c55
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-23 15:28:51 +0000

    Wed, Aug 23, 2023, 9:28 PM +06:00

--- beecrowd/2161.c
+++ beecrowd/2161.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <math.h>
 
+double div(double n, double m)
+{
+    if(n == 0) return 0;
+    if(n == 1) return 1/m;
+    return 1/(m + div(n-1, m));
+}
 
 int main(void)
 {
-    
+    int n;
+    scanf("%d", &n);
+
+    printf("%.10lf\n", 3 + div(n , 6));
 
     return 0;
 }
