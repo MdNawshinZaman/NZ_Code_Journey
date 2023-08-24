0ea558830d31292abeb3606fdd4210bd1f42bf4e
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-24 11:47:32 +0000

    Thu, Aug 24, 2023, 5:47 PM +06:00

--- beecrowd/2028.c
+++ beecrowd/2028.c
@@ -1,7 +1,4 @@
 #include <stdio.h>
-#include <math.h>
-#include <string.h>
-
 
 int main(void)
 {
@@ -34,7 +31,7 @@ int main(void)
                 printf("%d", i);
                 if(j != i-1 ) printf(" ");
             }
-            
+            if(i != n) printf(" ");
         }
         printf("\n\n");
 
