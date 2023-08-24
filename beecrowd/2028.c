5ef4ad704b757e9e5f2200272ec8014d2d4e77dd
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-24 10:13:38 +0000

    Thu, Aug 24, 2023, 4:13 PM +06:00

--- beecrowd/2028.c
+++ beecrowd/2028.c
@@ -8,7 +8,16 @@ int main(void)
     int n, case = 1;
     while(scanf("%d", &n) != EOF)
     {
-        
+        int num;
+        num = (n * (n - 1)) / 2;
+        num++;
+
+        if(num == 1)
+        {
+            printf("Caso %d: %d numeros\n", case, num);
+        }
+        else
+
     }
 
     return 0;
