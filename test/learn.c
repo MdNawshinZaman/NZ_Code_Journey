ed2d11db2b1f62b092e8d25c8ffb3a94b08f2c69
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-24 12:11:16 +0000

    Thu, Aug 24, 2023, 6:11 PM +06:00

--- test/learn.c
+++ test/learn.c
@@ -4,9 +4,9 @@
 int main(void)
 {
 
-    for(float i = 0.2; i < 2; i+=0.2)
+    for(double i = 0.2; i <= 2.0; i+=0.2)
     {
-        printf("%.2f = %f\n", i, trunc(i));
+        printf("%.2f = %.f\n", i, trunc(i));
     }
 
     return 0;
