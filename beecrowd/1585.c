f066b417f91ca5d27134206e67854e523f18e658
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-24 12:02:33 +0000

    Thu, Aug 24, 2023, 6:02 PM +06:00

--- beecrowd/1585.c
+++ beecrowd/1585.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
-#include <math.h>
-#include <string.h>
-
 
 int main(void)
 {
-    
+    int n;
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++)
+    {
+        int a, b;
+        scanf("%d %d", &a, &b);
+
+        int temp;
+        if(a < b)
+        {
+            temp = a;
+            a = b;
+            b = temp;
+        }
+
+        printf("%d cm2\n", (a/2)*b);
+    }
 
     return 0;
 }
