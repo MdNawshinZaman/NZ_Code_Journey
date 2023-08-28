d23b914d9cfe7b4c5bcac791d81d7fb1db640823
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-28 16:49:32 +0000

    Mon, Aug 28, 2023, 10:49 PM +06:00

--- beecrowd/1641.c
+++ beecrowd/1641.c
@@ -5,7 +5,27 @@
 
 int main(void)
 {
-    double 
+    double r, w, l, q;
+    int count = 1;
+    while(1)
+    {
+        scanf("%lf", &r);
+        if(r == 0) break;
+
+        scanf("%lf %lf", &w, &l);
+        q = sqrt(pow(w,2) + pow(l, 2));
+
+        if(q <= 2*r)
+        {
+            printf("Pizza %d fits on the table.\n",count);
+            count++;
+        }
+        else
+        {
+            printf("Pizza %d does not fit on the table.\n", count);
+            count++;
+        }
+    }
 
 
     return 0;
