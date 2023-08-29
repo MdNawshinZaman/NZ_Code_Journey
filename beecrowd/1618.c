b5659d85c04498500d608f51833caf211dcd609d
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-29 11:59:06 +0000

    Tue, Aug 29, 2023, 5:59 PM +06:00

--- beecrowd/1618.c
+++ beecrowd/1618.c
@@ -2,7 +2,14 @@
 
 int main(void)
 {
-    
+    int ax, ay, bx, by, cx, cy, dx, dy, rx, ry, n;
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d %d %d %d %d %d %d %d %d %d", &ax, &ay, &bx, &by, &cx, &cy, &dx, &dy, &rx, &ry);
+        
+    }
 
     return 0;
 }
