978f0bd06e140dbeb21cf4b49f7c003cee00b9b3
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-29 15:38:38 +0000

    Tue, Aug 29, 2023, 9:38 PM +06:00

--- beecrowd/2846.c
+++ beecrowd/2846.c
@@ -2,10 +2,17 @@
 #include <math.h>
 #include <string.h>
 
+int arr[200009] = {0};
+int fibonot[100009] = {0};
 
 int main(void)
 {
-
+    int ini = 0, i = 1;
+    while(ini <= 2000009)
+    {
+        ini += i;
+        arr[ini] = 1;
+    }
 
     return 0;
 }
