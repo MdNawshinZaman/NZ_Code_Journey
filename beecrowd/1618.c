7695b8b64e4db6959cfa9ebf9b69d770966f1831
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-29 13:13:49 +0000

    Tue, Aug 29, 2023, 7:13 PM +06:00

--- beecrowd/1618.c
+++ beecrowd/1618.c
@@ -8,13 +8,34 @@ int main(void)
     int x[5], y[5];
     for (int i = 0; i < n; i++)
     {
-        for(int i = 0; i < 4; i++)
+        for(int j = 0; j < 4; j++)
         {
-            scanf("%d %d", &x[i], &y[i]);
+            scanf("%d %d", &x[j], &y[j]);
         }
 
         scanf("%d %d", &rx, &ry);
 
+        int temp;
+        for(int j = 0; j < 4; j++)
+        {
+            for(int k = 0; k < 3; k++)
+            {
+                if (x[k] > x[k+1])
+                {
+                    temp = x[k];
+                    x[k] = x[k+1];
+                    x[k+1] = temp;
+                }
+                if (y[k] > y[k+1])
+                {
+                    temp = y[k];
+                    y[k] = y[k+1];
+                    y[k+1] = temp;
+                }
+            }
+        }
+
+        printf("x: %d %d\ny: %d %d\n",  x[0], x[3], y[0], y[3]);
 
         // if ((rx >= minX && rx  <= maxX) && (ry >= minY && ry  <= maxY))
         // {
