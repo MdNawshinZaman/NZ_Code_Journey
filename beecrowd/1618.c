1870168d8fe56dc66a43f2f829454574d5ae98d1
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-29 12:20:56 +0000

    Tue, Aug 29, 2023, 6:20 PM +06:00

--- beecrowd/1618.c
+++ beecrowd/1618.c
@@ -1,20 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main(void)
 {
     int ax, ay, bx, by, cx, cy, dx, dy, rx, ry, n;
     scanf("%d", &n);
 
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         scanf("%d %d %d %d %d %d %d %d %d %d", &ax, &ay, &bx, &by, &cx, &cy, &dx, &dy, &rx, &ry);
         int minX = ax, maxX = ax, minY = ay, maxY = ay;
 
-        minX = (minX > bx) ? ((bx > cx)? ((cx > dx) ? dx : cx) : bx) : ax;
-        maxX = (maxX < bx) ? ((bx < cx)? ((cx < dx) ? dx : cx) : bx) : ax;
+        minX = (minX > bx) ? ((bx > cx) ? ((cx > dx) ? dx : cx) : bx) : ax;
+        maxX = (maxX < bx) ? ((bx < cx) ? ((cx < dx) ? dx : cx) : bx) : ax;
 
-        minY = (minY > by) ? ((by > cy)? ((cy > dy) ? dy : cy) : by) : ay;
-        maxY = (maxY < by) ? ((by < cy)? ((cy < dy) ? dy : cy) : by) : ay;
+        minY = (minY > by) ? ((by > cy) ? ((cy > dy) ? dy : cy) : by) : ay;
+        maxY = (maxY < by) ? ((by < cy) ? ((cy < dy) ? dy : cy) : by) : ay;
 
         printf("X: %d %d\nY: %d %d\n", minX, maxX, minY, maxY);
 
