308c5f1fe5a1882af1fb73ad6ebe12137818b0a9
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-29 12:28:48 +0000

    Tue, Aug 29, 2023, 6:28 PM +06:00

--- beecrowd/1618.c
+++ beecrowd/1618.c
@@ -2,21 +2,19 @@
 
 int main(void)
 {
-    int ax, ay, bx, by, cx, cy, dx, dy, rx, ry, n;
+    int rx, ry, n;
     scanf("%d", &n);
 
+    int x[5], y[5];
     for (int i = 0; i < n; i++)
     {
-        scanf("%d %d %d %d %d %d %d %d %d %d", &ax, &ay, &bx, &by, &cx, &cy, &dx, &dy, &rx, &ry);
-        int minX = ax, maxX = ax, minY = ay, maxY = ay;
+        for(int i = 0; i < 4; i++)
+        {
+            scanf("%d %d", &x[i], &y[i]);
+        }
 
-        minX = (minX > bx) ? ((bx > cx) ? ((cx > dx) ? dx : cx) : bx) : ax;
-        maxX = (maxX < bx) ? ((bx < cx) ? ((cx < dx) ? dx : cx) : bx) : ax;
+        scanf("%d %d", &rx, &ry);
 
-        minY = (minY > by) ? ((by > cy) ? ((cy > dy) ? dy : cy) : by) : ay;
-        maxY = (maxY < by) ? ((by < cy) ? ((cy < dy) ? dy : cy) : by) : ay;
-
-        printf("X: %d %d\nY: %d %d\n", minX, maxX, minY, maxY);
 
         // if ((rx >= minX && rx  <= maxX) && (ry >= minY && ry  <= maxY))
         // {
