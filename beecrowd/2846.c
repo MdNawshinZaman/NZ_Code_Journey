2b44a9bb6f0539874abd560e1dbd5ddb769f8ee6
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-29 15:45:41 +0000

    Tue, Aug 29, 2023, 9:45 PM +06:00

--- beecrowd/2846.c
+++ beecrowd/2846.c
@@ -4,8 +4,8 @@
 
 int main(void)
 {
-    int arr[200009] = {0};
-    int fibonot[200009] = {0};
+    int arr[200009];
+    int fibonot[200009];
     int ini = 0, k = 1;
     while (ini <= 2000009)
     {
@@ -13,12 +13,13 @@ int main(void)
         arr[ini] = 1;
     }
 
-    for (int i = 0, j = 1; i < 100009; i++, j++)
+    for (int i = 0, j = 1; i < 100009; i++)
     {
         if (arr[j] == 0)
         {
             fibonot[i] = j;
         }
+        j++;
     }
 
     for (int i = 0; i < 10; i++)
