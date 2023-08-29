4c8e0cd4953d0697411ba7bd2deb64107fe0dc6e
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-08-29 15:44:33 +0000

    Tue, Aug 29, 2023, 9:44 PM +06:00

--- beecrowd/2846.c
+++ beecrowd/2846.c
@@ -1,23 +1,31 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 #include <string.h>
-
-int arr[200009] = {0};
-int fibonot[100009] = {0};
 
 int main(void)
 {
-    int ini = 0, i = 1;
-    while(ini <= 2000009)
+    int arr[200009] = {0};
+    int fibonot[200009] = {0};
+    int ini = 0, k = 1;
+    while (ini <= 2000009)
     {
-        ini += i;
+        ini += k;
         arr[ini] = 1;
     }
 
-    for(int i = 0, j = 1; i < 100009; i++, j++)
+    for (int i = 0, j = 1; i < 100009; i++, j++)
     {
-        if()
+        if (arr[j] == 0)
+        {
+            fibonot[i] = j;
+        }
     }
+
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ", fibonot[i]);
+    }
+    printf("\n");
 
     return 0;
 }
