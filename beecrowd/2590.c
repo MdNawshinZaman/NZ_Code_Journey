4d3df63961c0799d749173573b6d7230d78f91f8
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-09-01 03:54:23 +0000

    Fri, Sep 1, 2023, 9:54 AM +06:00

--- beecrowd/2590.c
+++ beecrowd/2590.c
@@ -3,7 +3,33 @@
 
 int main(void)
 {
-    
+    int n;
+    scanf("%d", &n);
+
+    int temp;
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &temp);
+
+        switch(temp % 4)
+        {
+            case 0:
+                printf("1\n");
+                break;
+
+            case 1:
+                printf("7\n");
+                break;
+
+            case 2:
+                printf("9\n");
+                break;
+
+            case 3:
+                printf("3\n");
+                break;
+        }
+    }
 
     return 0;
 }
