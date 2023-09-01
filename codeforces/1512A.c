8dfe3559280177a9d02e22a04c3bf09fb3e69fe9
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-09-01 16:21:25 +0000

    Fri, Sep 1, 2023, 10:21 PM +06:00

--- codeforces/1512A.c
+++ codeforces/1512A.c
@@ -5,10 +5,15 @@ int main(void)
     int n;
     scnaf("%d", &n);
 
-    int freq[109] = {0}, arr[107] = {0};
+    int freq[107] = {0}, arr[107] = {0};
     for(int i = 0; i < n; i++)
     {
-        
+        for(int j = 0; j < 107; j++)
+        {
+            freq[j] = 0;
+            arr[j] = 0
+        }
+
     }
 
     return 0;
