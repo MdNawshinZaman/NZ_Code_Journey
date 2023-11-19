b9f5ad15c47bf76901c99a51e80de9df9be5e06f
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-11-19 16:26:13 +0000

    Sun, Nov 19, 2023, 10:26 PM +06:00

--- advance_Camp/c.cpp
+++ advance_Camp/c.cpp
@@ -14,10 +14,15 @@ int main(void)
 
     for(int i = 0;  i < n; i++)
     {
-        for(int j = i; j < n; j++)
+        for(int j = 0; j < ; j++)
+        {
+            cout << ' ';
+        }
+        for(int j = 0; j < i+1; j++)
         {
             cout << '*';
         }
+
         cout << '\n';
     }
 
