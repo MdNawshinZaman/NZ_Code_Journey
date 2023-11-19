246af89492d1eb00c4fe4e995746d46ec46b9ff4
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-11-19 17:20:28 +0000

    Sun, Nov 19, 2023, 11:20 PM +06:00

--- advance_Camp/d.cpp
+++ advance_Camp/d.cpp
@@ -1,9 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(void)
+void rec(n)
 {
     
+}
+
+void sum(n)
+{
+    if(n <= 0)
+    {
+        return;
+    }
+    sum(n-1);
+    rec(n);
+}
+
+int main(void)
+{
+    int n;
+    cin >> n;
+    rec_sum(n);
 
     return 0;
 }
