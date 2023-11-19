66a15362557549eeffad3d4a58512a3d74b61f41
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-11-19 16:47:53 +0000

    Sun, Nov 19, 2023, 10:47 PM +06:00

--- advance_Camp/c.cpp
+++ advance_Camp/c.cpp
@@ -1,27 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void row_printer()
+void row_printer(int n, int i)
 {
-    for (int i = 0; i < n; i++)
+    for (int j = 1; j < n - i; j++)
     {
-        for (int j = 1; j < n - i; j++)
-        {
-            cout << ' ';
-        }
-        for (int j = 0; j < ((i * 2) + 1); j++)
-        {
-            cout << '*';
-        }
-
-        cout << '\n';
+        cout << ' ';
     }
+    for (int j = 0; j < ((i * 2) + 1); j++)
+    {
+        cout << '*';
+    }
+
+    cout << '\n';
 }
 
 void pattern(int num, int n)
 {
     if (n == 1)
     {
+        
     }
 }
 
