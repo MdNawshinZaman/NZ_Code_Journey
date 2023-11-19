631a12831d45e8b2f512911f6f1296a1179fd364
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-11-19 17:48:33 +0000

    Sun, Nov 19, 2023, 11:48 PM +06:00

--- advance_Camp/e.cpp
+++ advance_Camp/e.cpp
@@ -1,15 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int fib_n(n)
+int fib_n(int n, int current, int prev, int count)
 {
-    
+    if(count == n)
+    {
+        return current;
+    }
+    fib_n(n, current += , count+=1)
 }
 
 int main(void)
 {
     int n;
     cin >> n;
-    cout << fib_n(n);
+    cout << fib_n(n,1, 0, 2);
     return 0;
 }
