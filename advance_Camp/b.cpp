3aaad0ace06f21836ea5df5d25689dd776d21436
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-11-19 12:59:13 +0000

    Sun, Nov 19, 2023, 6:59 PM +06:00

--- advance_Camp/b.cpp
+++ advance_Camp/b.cpp
@@ -1,11 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define ll long long int
 
-#define long long int ll
+void num(ll num)
+{
+    
+}
 
 int main(void)
 {
-    
+    int a;
+    cin >> a;
+
+    ll num = 0;
+    for(int i = 0; i < a; i++)
+    {
+        cin >> num;
+        print(num);
+        cout << '\n';
+    }
 
     return 0;
 }
