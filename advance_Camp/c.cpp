4550e12ab1bcd7bb15283c9d77a67e82fd6fb6c5
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-11-19 16:38:22 +0000

    Sun, Nov 19, 2023, 10:38 PM +06:00

--- advance_Camp/c.cpp
+++ advance_Camp/c.cpp
@@ -1,20 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void pattern(int n)
+void pattern(int num, int n)
 {
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 1; j < n - i; j++)
-        {
-            cout << ' ';
-        }
-        for (int j = 0; j < ((i * 2) + 1); j++)
-        {
-            cout << '*';
-        }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 1; j < n - i; j++)
+    //     {
+    //         cout << ' ';
+    //     }
+    //     for (int j = 0; j < ((i * 2) + 1); j++)
+    //     {
+    //         cout << '*';
+    //     }
 
-        cout << '\n';
+    //     cout << '\n';
+    // }
+
+    if (n == 0)
+    {
+        
     }
 }
 
