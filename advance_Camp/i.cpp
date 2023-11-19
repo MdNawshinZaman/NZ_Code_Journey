a3fb1f50ae84806c56cbc2f0c870d633291cd80e
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2023-11-19 12:44:02 +0000

    Sun, Nov 19, 2023, 6:44 PM +06:00

--- advance_Camp/i.cpp
+++ advance_Camp/i.cpp
@@ -1,8 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int max(int a, int b, int c, int d)
+{
+    return a > b? = a:b;
+}
+
+
 int main(void)
 {
     int a, b, c, d;
-    cin<<
+    cin >> a >> b >> c >>d;
+    //cout << a <<' ' << b << ' ' << c << ' ' << d << '\n';
+
+    int m = max(a, b, c, d);
+
+    cout << m << '\n';
+
+    return 0;
 }
