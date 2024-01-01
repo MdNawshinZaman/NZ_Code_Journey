17fb126c9cf29c202a209406d4c2e3ed59feaa9f
MdNawshinZaman <119166131+MdNawshinZaman@users.noreply.github.com>
2024-01-01 13:06:38 +0000

    Mon, Jan 1, 2024, 7:06 PM +06:00

--- cs50x-2024/week1/hello.c
+++ cs50x-2024/week1/hello.c
@@ -4,7 +4,7 @@ int main(void)
 {
     // printf("Hello Worlds!\n");
     char ans[20];
-    scanf("%[\n]s%*c");
-    printf("Hello, %s\n", ans);
+    scanf("%[^\n]s%*c", ans);
+    printf("Hello, %s!\n", ans);
     return 0;
 }
